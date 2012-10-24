6f5d7c205ac1a960e5a6e822e7e6eed239e7058b
Tom MacWright <tom@macwright.org>
2012-10-24 18:35:44 -0400

    Remove old tagging infrastructure

--- js/iD/actions/CreatePOIAction.js
+++ js/iD/actions/CreatePOIAction.js
@@ -3,47 +3,47 @@
 define(['dojo/_base/declare', 'iD/actions/UndoableAction'],
        function(declare) {
 
-// ----------------------------------------------------------------------
-// CreatePOIAction class
+           // ----------------------------------------------------------------------
+           // CreatePOIAction class
 
-declare("iD.actions.CreatePOIAction", [iD.actions.CompositeUndoableAction], {
+           declare("iD.actions.CreatePOIAction", [iD.actions.CompositeUndoableAction], {
 
-	newNode: null,
-	tags: null,
-	lat: NaN,
-	lon: NaN,
-	connection: null,
+               newNode: null,
+               tags: null,
+               lat: NaN,
+               lon: NaN,
+               connection: null,
 
-	constructor: function(connection, tags, lat, lon) {
-		// summary:		Create a new node and set it as a POI. Used by drag-and-drop. Note that the
-		//				node is remembered, so that on redo we can just reinstate it.
-		this.setName('Create POI: ' + iD.Util.friendlyName(tags));
-		this.connection = connection;
-		this.tags = tags;
-		this.lat = lat;
-		this.lon = lon;
-	},
+               constructor: function(connection, tags, lat, lon) {
+                   // summary:		Create a new node and set it as a POI. Used by drag-and-drop. Note that the
+                   //				node is remembered, so that on redo we can just reinstate it.
+                   this.setName('Create POI: ' + iD.Util.friendlyName(tags));
+                   this.connection = connection;
+                   this.tags = tags;
+                   this.lat = lat;
+                   this.lon = lon;
+               },
 
-	run: function() {
-        if (this.newNode === null) {
-            this.newNode = this.connection.doCreateNode(this.tags,
-                this.lat, this.lon,
-                _.bind(this.push, this));
-        }
-		this.connection.registerPOI(this.newNode);
-		return true;
-	},
+               run: function() {
+                   if (this.newNode === null) {
+                       this.newNode = this.connection.doCreateNode(this.tags,
+                                                                   this.lat, this.lon,
+                                                                   _.bind(this.push, this));
+                   }
+                   this.connection.registerPOI(this.newNode);
+                   return true;
+               },
 
-	undo: function() {
-		this.connection.unregisterPOI(this.newNode);
-		return true;
-	},
+               undo: function() {
+                   this.connection.unregisterPOI(this.newNode);
+                   return true;
+               },
 
-	getNode: function() {
-		return this.newNode;
-	}
-});
+               getNode: function() {
+                   return this.newNode;
+               }
+           });
 
-// ----------------------------------------------------------------------
-// End of module
-});
+           // ----------------------------------------------------------------------
+           // End of module
+       });
--- js/iD/actions/MoveNodeAction.js
+++ js/iD/actions/MoveNodeAction.js
@@ -3,60 +3,60 @@
 define(['dojo/_base/declare','iD/actions/UndoableAction'],
        function(declare) {
 
-// ----------------------------------------------------------------------
-// MoveNodeAction class
+           // ----------------------------------------------------------------------
+           // MoveNodeAction class
 
-declare("iD.actions.MoveNodeAction", [iD.actions.UndoableEntityAction], {
+           declare("iD.actions.MoveNodeAction", [iD.actions.UndoableEntityAction], {
 
-	createTime: NaN,
-	oldLat: NaN,
-	oldLon: NaN,
-	newLat: NaN,
-	newLon: NaN,       
-	setLatLon: null,
+               createTime: NaN,
+               oldLat: NaN,
+               oldLon: NaN,
+               newLat: NaN,
+               newLon: NaN,       
+               setLatLon: null,
 
-	constructor: function(node, newLat, newLon, setLatLon) {
-		// summary:		Move a node to a new position.
-		this.entity = node;
-		this.newLat = newLat;
-		this.newLon = newLon;
-		this.setLatLon = setLatLon;
-		this.createTime = new Date().getTime();
-	},
+               constructor: function(node, newLat, newLon, setLatLon) {
+                   // summary:		Move a node to a new position.
+                   this.entity = node;
+                   this.newLat = newLat;
+                   this.newLon = newLon;
+                   this.setLatLon = setLatLon;
+                   this.createTime = new Date().getTime();
+               },
 
-	run: function() {
-		var node = this.entity;
-		this.oldLat = node.lat;
-		this.oldLon = node.lon;
-		if (this.oldLat === this.newLat &&
-            this.oldLon === this.newLon) {
-            return NO_CHANGE;
-        }
-		this.setLatLon(this.newLat, this.newLon);
-		this.markDirty();
-		node.refresh();
-		return true;
-	},
+               run: function() {
+                   var node = this.entity;
+                   this.oldLat = node.lat;
+                   this.oldLon = node.lon;
+                   if (this.oldLat === this.newLat &&
+                       this.oldLon === this.newLon) {
+                       return NO_CHANGE;
+                   }
+                   this.setLatLon(this.newLat, this.newLon);
+                   this.markDirty();
+                   node.refresh();
+                   return true;
+               },
 
-	undo: function() {
-		this.setLatLon(this.oldLat, this.oldLon);
-		this.markClean();
-		this.refresh();
-		return true;
-	},
+               undo: function() {
+                   this.setLatLon(this.oldLat, this.oldLon);
+                   this.markClean();
+                   this.refresh();
+                   return true;
+               },
 
-	mergePrevious: function(prev) {
-		if (prev.declaredClass!=this.declaredClass) { return false; }
-		
-		if (prev.entity == this.entity && prev.createTime+1000>this.createTime) {
-			this.oldLat = prev.oldLat;
-			this.oldLon = prev.oldLon;
-			return true;
-		}
-		return false;
-	}
-});
+               mergePrevious: function(prev) {
+                   if (prev.declaredClass!=this.declaredClass) { return false; }
 
-// ----------------------------------------------------------------------
-// End of module
-});
+                   if (prev.entity == this.entity && prev.createTime+1000>this.createTime) {
+                       this.oldLat = prev.oldLat;
+                       this.oldLon = prev.oldLon;
+                       return true;
+                   }
+                   return false;
+               }
+           });
+
+           // ----------------------------------------------------------------------
+           // End of module
+       });
