68ee31ea29fa3f3e6ca7cf16031b2900787232f9
John Firebaugh <john.firebaugh@gmail.com>
2012-11-08 15:45:39 -0800

    Move controller to own file

--- js/iD/actions/actions.js
+++ js/iD/actions/actions.js
@@ -177,23 +177,3 @@ iD.actions.Move = {
     },
     exit: function() { }
 };
-
-// A controller holds a single action at a time and calls `.enter` and `.exit`
-// to bind and unbind actions.
-iD.controller = function(map) {
-    var controller = { action: null };
-
-    controller.go = function(x) {
-        x.controller = controller;
-        x.map = map;
-        if (controller.action) {
-            controller.action.exit();
-        }
-        x.enter();
-        controller.action = x;
-    };
-
-    controller.go(iD.actions.Move);
-
-    return controller;
-};
