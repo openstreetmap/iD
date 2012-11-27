89e135d3bf0d0e51112cf5646e9488edef871d14
Tom MacWright <tom@macwright.org>
2012-11-27 18:14:25 -0500

    Add loading

---
+++ js/id/ui/loading.js
@@ -0,0 +1,16 @@
+iD.loading = function(message) {
+    var loading = d3.select('div.loading');
+    if (loading.empty()) loading = d3.select(document.body)
+        .append('div').attr('class', 'loading shaded');
+    var modal = loading.append('div')
+        .attr('class', 'modal loading-pane')
+        .text(message || '');
+
+    var l = {};
+
+    l.remove = function() {
+        d3.select('div.loading').remove();
+    };
+
+    return l;
+};
