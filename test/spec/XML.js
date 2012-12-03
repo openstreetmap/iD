2d2778aea070e88f7b7e31cfe8cb0754399d49f9
Tom MacWright <tom@macwright.org>
2012-12-03 12:31:02 -0500

    Test out way rep as well

--- test/spec/XML.js
+++ test/spec/XML.js
@@ -1,20 +1,24 @@
 describe('XML', function() {
-  describe('#rep', function() {
-      it('converts a node to jxon', function() {
-          expect(iD.format.XML.rep({ id: 'n-1', type: 'node', lat: 38, lon: -77 }))
-              .toEqual({ node : { '@id': '-1', '@lat': 38, '@lon': -77, '@version': 0, tag: [ ] } });
-      });
-  });
+    describe('#rep', function() {
+        it('converts a node to jxon', function() {
+            expect(iD.format.XML.rep({ id: 'n-1', type: 'node', lat: 38, lon: -77 }))
+            .toEqual({ node : { '@id': '-1', '@lat': 38, '@lon': -77, '@version': 0, tag: [ ] } });
+        });
+        it('converts a way to jxon', function() {
+            expect(iD.format.XML.rep({ id: 'w-1', type: 'way', nodes: [] }))
+            .toEqual({ way : { '@id' : '-1', nd : [ ], '@version': 0, tag: [ ] } });
+        });
+    });
 
-  describe('#mapping', function() {
-      it('serializes a node to xml', function() {
-          expect(iD.format.XML.mapping({ id: 'n-1', type: 'node', lat: 38, lon: -77 }))
-              .toEqual('&lt;node id=&quot;-1&quot; lat=&quot;38&quot; lon=&quot;-77&quot; version=&quot;0&quot;/&gt;');
-      });
+    describe('#mapping', function() {
+        it('serializes a node to xml', function() {
+            expect(iD.format.XML.mapping({ id: 'n-1', type: 'node', lat: 38, lon: -77 }))
+            .toEqual('&lt;node id=&quot;-1&quot; lat=&quot;38&quot; lon=&quot;-77&quot; version=&quot;0&quot;/&gt;');
+        });
 
-      it('serializes a way to xml', function() {
-          expect(iD.format.XML.mapping({ type: 'way', nodes: [], id: 'w-1' }))
-              .toEqual('&lt;way id=&quot;-1&quot; version=&quot;0&quot;/&gt;');
-      });
-  });
+        it('serializes a way to xml', function() {
+            expect(iD.format.XML.mapping({ type: 'way', nodes: [], id: 'w-1' }))
+            .toEqual('&lt;way id=&quot;-1&quot; version=&quot;0&quot;/&gt;');
+        });
+    });
 });
