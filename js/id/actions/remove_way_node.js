23856cc1c53a632c008f9c0793a0f546fa7578c5
Tom MacWright <tom@macwright.org>
2012-12-07 10:25:09 -0500

    Reclose ways if you delete the repeated node. Fixes #221

--- js/id/actions/remove_way_node.js
+++ js/id/actions/remove_way_node.js
@@ -1,7 +1,17 @@
 iD.actions.RemoveWayNode = function(wayId, nodeId) {
     return function(graph) {
-        var way = graph.entity(wayId),
+        var way = graph.entity(wayId), nodes;
+        // If this is the connecting node in a closed area
+        if (way.nodes.length > 1 &&
+            _.indexOf(way.nodes, nodeId) === 0 &&
+            _.lastIndexOf(way.nodes, nodeId) === way.nodes.length - 1) {
+            // Remove the node
             nodes = _.without(way.nodes, nodeId);
+            // And reclose the way on the new first node.
+            nodes.push(nodes[0]);
+        } else {
+            nodes = _.without(way.nodes, nodeId);
+        }
         return graph.replace(way.update({nodes: nodes}), 'removed from a road');
     };
 };
