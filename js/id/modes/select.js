a405e4e059fef793789e1978504b54081d5bf6fd
Tom MacWright <tom@macwright.org>
2012-12-07 23:59:11 -0500

    Fix way dragging. Fixes #235

--- js/id/modes/select.js
+++ js/id/modes/select.js
@@ -19,9 +19,10 @@ iD.modes.Select = function (entity) {
                 mode.history.perform(iD.actions.Noop());
             }
 
-            entity.nodes.forEach(function(node) {
-                var start = mode.map.projection(node.loc);
-                var end = mode.map.projection.invert([
+            entity.nodes.forEach(function(n) {
+                var node = mode.history.graph().entity(n.id),
+                    start = mode.map.projection(node.loc),
+                    end = mode.map.projection.invert([
                     start[0] + d3.event.dx,
                     start[1] + d3.event.dy]);
                 mode.history.replace(iD.actions.Move(node.id, end));
