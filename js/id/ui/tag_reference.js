26c513a9f3898dde2d2072a9306ffdfd5d6f504c
Saman Bemel-Benrud <samanpwbb@gmail.com>
2012-12-20 19:05:03 -0500

    basic styling for tag reference modal.

--- js/id/ui/tag_reference.js
+++ js/id/ui/tag_reference.js
@@ -3,32 +3,35 @@ iD.tagReference = function(selection) {
         function g(x) { return function(d) { return d[x]; }; }
         var selection = d3.select(this);
         var header = selection.append('div')
-                        .attr('class','modal-section')
-                        .append('h2');
-            header.append('span').attr('class','icon big icon-pre-text big-inspect');
-            header.append('span').text(g('title'));
-
-        var icon_row = selection.append('div');
+            .attr('class','modal-section')
+            .append('h2');
+                header.append('span').attr('class','icon big icon-pre-text big-inspect');
+                header.append('span').text(g('title'));
+        var icon_row = selection.append('div')
+            .attr('class','modal-section');
         var icons = icon_row.selectAll('span.icon')
             .data(g('types'))
             .enter()
             .append('span')
             .attr('title', function(d) {
                 return 'used with ' + d;
-            })
-            .attr('class', function(d) {
-                return 'icon big icon-pre-text big-' + d;
             });
-        selection
+            // .attr('class', function(d) {
+            //     // return 'icon icon-pre-text ' + d;
+            // });
+        icon_row
+            .append('h5')
+            .text('Description')
+        icon_row
+            .append('p')
+            .text(g('description'));
+        icon_row
             .append('a')
             .attr('href', function(d) {
                 return 'http://wiki.openstreetmap.org/wiki/' + d.title;
             })
             .text(function(d) {
-                return '→ ' + d.title + ' on wiki.osm.org';
+                return d.title + ' on wiki.osm.org';
             });
-        selection
-            .append('p')
-            .text(g('description'));
     });
 };
