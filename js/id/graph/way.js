4304c2290cac4f715beb81b3dc19ed6bd13fc227
Tom MacWright <tom@macwright.org>
2013-01-11 16:18:37 -0500

    Use simple inheritance for entities, use less dynamic new

--- js/id/graph/way.js
+++ js/id/graph/way.js
@@ -1,4 +1,29 @@
-iD.Way = iD.Entity.extend({
+iD.Way = function(sources) {
+    if (!sources.length || sources.length === 0) sources = [sources];
+    for (var i = 0; i < sources.length; ++i) {
+        var source = sources[i];
+        for (var prop in source) {
+            if (Object.prototype.hasOwnProperty.call(source, prop)) {
+                this[prop] = source[prop];
+            }
+        }
+    }
+
+    if (!this.id && this.type) {
+        this.id = iD.Entity.id(this.type);
+        this._updated = true;
+    }
+
+    if (iD.debug) {
+        Object.freeze(this);
+        Object.freeze(this.tags);
+
+        if (this.nodes) Object.freeze(this.nodes);
+        if (this.members) Object.freeze(this.members);
+    }
+};
+
+iD.Way.prototype = {
     type: "way",
     nodes: [],
 
@@ -41,4 +66,7 @@ iD.Way = iD.Entity.extend({
     geometry: function() {
         return this.isArea() ? 'area' : 'line';
     }
-});
+};
+
+iD.Entity.simpleExtend(iD.Way, iD.Entity);
+iD.Entity.way = iD.Way;
