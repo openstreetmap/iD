165aa9d4887c9d23970b9c72dc54035a010455fb
Tom MacWright <tom@macwright.org>
2013-01-12 01:12:19 -0500

    Faster string building, do not change value if not different. Fixes #390

--- js/id/svg/tag_classes.js
+++ js/id/svg/tag_classes.js
@@ -12,16 +12,20 @@ iD.svg.TagClasses = function() {
 
             classes = value.trim().split(/\s+/).filter(function(name) {
                 return name.length && !tagClassRe.test(name);
-            });
+            }).join(' ');
 
             var tags = d.tags;
             for (var k in tags) {
                 if (!keys[k]) continue;
-                classes.push('tag-' + k);
-                classes.push('tag-' + k + '-' + tags[k]);
+                classes += ' tag-' + k + ' ' +
+                    'tag-' + k + '-' + tags[k];
             }
 
-            return d3.select(this).attr('class', classes.join(' '));
+            classes = classes.trim();
+
+            if (classes !== value) {
+                d3.select(this).attr('class', classes);
+            }
         });
     };
 };
