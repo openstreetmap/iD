2f1e219d64217eb9cd9ad38a2ecc3533d5a0abf6
Tom MacWright <tom@macwright.org>
2013-01-14 10:18:22 -0500

    jshint tweak

--- js/id/util.js
+++ js/id/util.js
@@ -126,8 +126,8 @@ iD.util.geo.pointInPolygon = function(point, polygon) {
         var xi = polygon[i][0], yi = polygon[i][1];
         var xj = polygon[j][0], yj = polygon[j][1];
 
-        var intersect = ((yi > y) != (yj > y))
-            && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+        var intersect = ((yi > y) != (yj > y)) &&
+            (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
         if (intersect) inside = !inside;
     }
 
