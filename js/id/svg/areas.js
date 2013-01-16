140e6991eda31f53ba820a158ef72d7b58b1de17
Tom MacWright <tom@macwright.org>
2013-01-16 14:21:20 -0500

    Stack areas as well as ways. Fixes #409

--- js/id/svg/areas.js
+++ js/id/svg/areas.js
@@ -1,4 +1,31 @@
 iD.svg.Areas = function() {
+
+    var area_stack = {
+        building: 0,
+        manmade: 1,
+        natural: 1,
+        boundary: 2
+    };
+
+    function findKey(a) {
+        var vals = Object.keys(a.tags).filter(function(k) {
+            return area_stack[k] !== undefined;
+        });
+        if (vals.length > 0) return area_stack[vals[0]];
+        else return -1;
+    }
+
+    function areastack(a, b) {
+        if (!a || !b || !a.tags || !b.tags) return 0;
+        if (a.tags.layer !== undefined && b.tags.layer !== undefined) {
+            return a.tags.layer - b.tags.layer;
+        }
+        var as = 0, bs = 0;
+        as -= findKey(a);
+        bs -= findKey(b);
+        return as - bs;
+    }
+
     return function drawAreas(surface, graph, entities, filter, projection) {
         var areas = [];
 
@@ -8,6 +35,8 @@ iD.svg.Areas = function() {
                 areas.push(entity);
             }
         }
+
+        areas.sort(areastack);
 
         var lineStrings = {};
 
