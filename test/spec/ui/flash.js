2b45a5b452540e4f533fe7b7ae1f9c0bcb2b9b43
John Firebaugh <john.firebaugh@gmail.com>
2013-01-18 15:00:50 -0800

    Use sinon's fake timers

--- test/spec/ui/flash.js
+++ test/spec/ui/flash.js
@@ -1,13 +1,22 @@
 describe("iD.ui.flash", function () {
+    var clock;
+
+    beforeEach(function () {
+        clock = sinon.useFakeTimers();
+    });
+
+    afterEach(function () {
+        clock.restore();
+    });
+
     it('can be instantiated', function () {
         var flash = iD.ui.flash();
         expect(flash).to.be.ok;
     });
-    it('leaves after 1000 ms', function (done) {
+
+    it('leaves after 1000 ms', function () {
         var flash = iD.ui.flash();
-        window.setTimeout(function() {
-            expect(flash.node().parentNode).to.be.null;
-            done();
-        }, 1200);
+        clock.tick(1010);
+        expect(flash.node().parentNode).to.be.null;
     });
 });
