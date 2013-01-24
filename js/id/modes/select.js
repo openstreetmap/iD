66a0d0bdbdc3e0a2150c0269923a3ddb2a7f4875
John Firebaugh <john.firebaugh@gmail.com>
2013-01-24 18:40:27 -0500

    Center the radial menu on entities (fixes #483)
    Also, don't show it when finishing draw operations.

--- js/id/modes/select.js
+++ js/id/modes/select.js
@@ -133,8 +133,16 @@ iD.modes.Select = function(entity, initial) {
 
         radialMenu = iD.ui.RadialMenu(entity, history);
 
-        if (d3.event) {
-            surface.call(radialMenu, d3.mouse(surface.node()));
+        if (d3.event && !initial) {
+            var loc = map.mouseCoordinates();
+
+            if (entity.type === 'node') {
+                loc = entity.loc;
+            } else if (entity.type === 'way') {
+                loc = iD.geo.chooseIndex(entity, d3.mouse(map.surface.node()), map).loc;
+            }
+
+            surface.call(radialMenu, map.projection(loc));
         }
     };
 
