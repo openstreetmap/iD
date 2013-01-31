578d4eebdfde86143e75b308d81db7f79077d052
Saman Bemel-Benrud <samanpwbb@gmail.com>
2013-01-30 19:40:41 -0500

    everything but logo.

--- js/id/ui/splash.js
+++ js/id/ui/splash.js
@@ -2,10 +2,15 @@ iD.ui.splash = function() {
     var modal = iD.ui.modal();
 
     modal.select('.modal')
-        .classed('modal-splash', true);
+        .attr('class', 'modal-splash modal')
 
-    modal.select('.content')
-        .html('Welcome to iD!');
+    var introModal = modal.select('.content')
+        .append('div')
+        .attr('class', 'header modal-section fillL');
+
+        introModal.append('div').attr('class','logo');
+
+        introModal.html("<h2>Welcome to the iD OpenStreetMap editor</h2><p>This is development version 0.0.0-alpha1. For more information see <a href='http://ideditor.com/'>ideditor.com</a> and report bugs at <a href='https://github.com'>github.com.systemed/iD</a></p>");
 
     return modal;
 };
