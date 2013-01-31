bd4c91c61228ddad6f089623249823a117cb4cdf
Saman Bemel-Benrud <samanpwbb@gmail.com>
2013-01-31 13:25:20 -0500

    modal touch-ups.

--- js/id/ui/splash.js
+++ js/id/ui/splash.js
@@ -6,11 +6,11 @@ iD.ui.splash = function() {
 
     var introModal = modal.select('.content')
         .append('div')
-        .attr('class', 'header modal-section fillL');
+        .attr('class', 'modal-section fillL');
 
         introModal.append('div').attr('class','logo');
 
-        introModal.append('div').html("<h2>Welcome to the iD OpenStreetMap editor</h2><p>This is development version 0.0.0-alpha1. For more information see <a href='http://ideditor.com/'>ideditor.com</a> and report bugs at <a href='https://github.com'>github.com.systemed/iD</a>.</p>");
+        introModal.append('div').html("<h2 class>Welcome to the iD OpenStreetMap editor</h2><p>This is development version 0.0.0-alpha1. For more information see <a href='http://ideditor.com/'>ideditor.com</a> and report bugs at <a href='https://github.com'>github.com.systemed/iD</a>.</p>");
 
     return modal;
 };
