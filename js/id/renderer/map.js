f4975447f89cf9c2eba368ac03658d5e8951e53a
Dr Ian <porjo38@yahoo.com.au>
2013-02-01 23:05:47 +0100

    Limit zoom to 19 if geocode bounding box too small

--- js/id/renderer/map.js
+++ js/id/renderer/map.js
@@ -346,10 +346,6 @@ iD.Map = function(context) {
                 vZoomDiff = Math.log(Math.abs(vFactor)) / Math.LN2,
                 newZoom = map.zoom() - Math.max(hZoomDiff, vZoomDiff);
 
-                if(newZoom > 19){
-                        newZoom = 19;
-                }
-
             map.centerZoom(extent.center(), newZoom);
         }
     };
