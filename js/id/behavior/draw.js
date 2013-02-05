4b76b136fab3a2bc0beb2aba6f21c23d7287b2c4
Tom MacWright <tom@macwright.org>
2013-02-05 15:58:54 -0500

    Fix tests for faux click

--- js/id/behavior/draw.js
+++ js/id/behavior/draw.js
@@ -3,7 +3,7 @@ iD.behavior.Draw = function(context) {
         'clickNode', 'undo', 'cancel', 'finish'),
         keybinding = d3.keybinding('draw'),
         hover = iD.behavior.Hover(),
-        closeTolerance = 4;
+        closeTolerance = 4,
         tolerance = 12;
 
     function datum() {
