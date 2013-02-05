d159b37ab60ebc5b82e53a191371aabb86f414b6
Tom MacWright <tom@macwright.org>
2013-02-05 12:10:05 -0500

    Fix tests

--- test/spec/ui/modal.js
+++ test/spec/ui/modal.js
@@ -1,6 +1,16 @@
 describe("iD.ui.modal", function () {
-    it('can be instantiated', function () {
-        var modal = iD.ui.modal()
+    var elem;
+
+    beforeEach(function() {
+        elem = d3.select('body').append('div');
+    });
+
+    afterEach(function() {
+        elem.remove();
+    });
+
+    it('can be instantiated', function() {
+        var modal = iD.ui.modal(elem)
             .select('.content')
             .text('foo');
         expect(modal).to.be.ok;
