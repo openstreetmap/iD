bd8c9d6a00f5deac4d947673948c81ceb7ab2cc6
John Firebaugh <john.firebaugh@gmail.com>
2013-02-06 13:31:06 -0800

    Support merging points into an area (#435)

--- js/id/operations/merge.js
+++ js/id/operations/merge.js
@@ -1,18 +1,28 @@
 iD.operations.Merge = function(selection, context) {
-    var action = iD.actions.Join(selection);
+    var join = iD.actions.Join(selection),
+        merge = iD.actions.Merge(selection);
 
     var operation = function() {
         var annotation = t('operations.merge.annotation', {n: selection.length}),
-            difference = context.perform(action, annotation);
+            action;
+
+        if (join.enabled(context.graph())) {
+            action = join;
+        } else {
+            action = merge;
+        }
+
+        var difference = context.perform(action, annotation);
         context.enter(iD.modes.Select(context, difference.extantIDs()));
     };
 
     operation.available = function() {
-        return selection.length > 1;
+        return selection.length >= 2;
     };
 
     operation.enabled = function() {
-        return action.enabled(context.graph());
+        return join.enabled(context.graph()) ||
+            merge.enabled(context.graph());
     };
 
     operation.id = "merge";
