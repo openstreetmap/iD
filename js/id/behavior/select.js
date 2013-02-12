0dbdd7c79766658353a2374933bbfd3a423f057b
John Firebaugh <john.firebaugh@gmail.com>
2013-02-12 14:20:07 -0800

    Shift-click deselects a selected entity

--- js/id/behavior/select.js
+++ js/id/behavior/select.js
@@ -2,14 +2,20 @@ iD.behavior.Select = function(context) {
     var behavior = function(selection) {
         function click() {
             var datum = d3.event.target.__data__;
-            if (datum instanceof iD.Entity) {
-                if (d3.event.shiftKey) {
-                    context.enter(iD.modes.Select(context, context.selection().concat([datum.id])));
-                } else {
-                    context.enter(iD.modes.Select(context, [datum.id]));
-                }
-            } else if (!d3.event.shiftKey) {
+            if (!(datum instanceof iD.Entity) && !d3.event.shiftKey) {
                 context.enter(iD.modes.Browse(context));
+
+            } else if (!d3.event.shiftKey) {
+                context.enter(iD.modes.Select(context, [datum.id]));
+
+            } else if (context.selection().indexOf(datum.id) >= 0) {
+                var selection = _.without(context.selection(), datum.id);
+                context.enter(selection.length ?
+                    iD.modes.Select(context, selection) :
+                    iD.modes.Browse(context));
+
+            } else {
+                context.enter(iD.modes.Select(context, context.selection().concat([datum.id])));
             }
         }
 
