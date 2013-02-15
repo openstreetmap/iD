5b9c6e2531bfc07432fc6f227be6d0f45ac2cf9c
John Firebaugh <john.firebaugh@gmail.com>
2013-02-14 16:23:59 -0800

    Update discarded tags to match P2

--- data/discarded.js
+++ data/discarded.js
@@ -1,10 +1,20 @@
 // entirely discarded tags
+// https://github.com/systemed/potlatch2/blob/master/net/systemeD/halcyon/connection/XMLConnection.as#L24
 iD.data.discarded = [
+    "created_by",
     'tiger:upload_uuid',
     'tiger:tlid',
     'tiger:source',
     'tiger:separated',
     'geobase:datasetName',
     'geobase:uuid',
-    'sub_sea:type'
+    'sub_sea:type',
+    "odbl",
+    "odbl:note",
+    "yh:LINE_NAME",
+    "yh:LINE_NUM",
+    "yh:STRUCTURE",
+    "yh:TOTYUMONO",
+    "yh:TYPE",
+    "yh:WIDTH_RANK"
 ];
