9732d8c65907d585aeb1523ca91761ba4a1e01c5
Ansis Brammanis <ansis.brammanis@gmail.com>
2013-02-22 16:31:49 -0500

    Use clientX instead of x, via @porjo

--- js/lib/d3.tail.js
+++ js/lib/d3.tail.js
@@ -31,11 +31,11 @@ d3.tail = function() {
 
         function mousemove() {
             if (text === false) return;
-            var xoffset = ((d3.event.x + tooltip_size[0] + xmargin) > selection_size[0]) ?
+            var xoffset = ((d3.event.clientX + tooltip_size[0] + xmargin) > selection_size[0]) ?
                 -tooltip_size[0] - xmargin : xmargin;
             container.style(transformProp, 'translate(' +
-                (~~d3.event.x + xoffset) + 'px,' +
-                ~~d3.event.y + 'px)');
+                (~~d3.event.clientX + xoffset) + 'px,' +
+                ~~d3.event.clientY + 'px)');
         }
 
         function mouseout() {
