d85313182bcf75573b354e55efc83153fd57e9da
Tom MacWright <tom@macwright.org>
2013-02-28 16:25:49 -0500

    Fix corner cases of simplify

--- js/id/actions/simplify.js
+++ js/id/actions/simplify.js
@@ -12,42 +12,58 @@ iD.actions.Simplify = function(wayId, projection) {
         return idx;
     }
 
+    // Handles two corner cases
+    // 
+    // 1. Reuse as many old nodes as possible
+    // 2. Don't move or eliminate any nodes that have more than one
+    //    parent way.
     var action = function(graph) {
         var way = graph.entity(wayId),
             nodes = graph.childNodes(way),
-            points = nodes.map(function(n) { return projection(n.loc); }),
-            best, i, j, ids = [];
-
-        var xys = points.map(function(p) {
-                return { x: p[0], y: p[1] };
-            }),
-            simplified = simplify(xys, 0.7),
-            multiNodes = nodes.filter(function(n) {
-                return graph.parentWays(n).length > 1;
-            });
-
-        for (i = 0; i < simplified.length; i++) {
-
-            var loc = projection.invert([simplified[i].x, simplified[i].y]);
-
-            if (nodes.length) {
-                var idx = closestIndex(nodes, loc);
-                node = nodes[idx];
-                nodes.splice(idx, 1);
-            } else {
-                node = iD.Node();
-            }
-
-            ids.push(node.id);
-            graph = graph.replace(node.move(loc));
-        }
+            node, i, j, ids = [], idx,
+            singleParent = [], multiParent = [];
 
         for (i = 0; i < nodes.length; i++) {
-            graph.parentWays(nodes[i]).forEach(function(parent) {
-                graph = graph.replace(parent.replaceNode(nodes[i].id,
-                ids[closestIndex(graph.childNodes(way), nodes[i].loc)]));
-            });
-            graph = iD.actions.DeleteNode(nodes[i].id)(graph);
+            if (graph.parentWays(nodes[i]).length === 1) singleParent.push(nodes[i]);
+            else multiParent.push(nodes[i]);
+        }
+
+        // all points that have a single way parent
+        var points = singleParent.map(function(n) {
+            // simplify.js accepts {x,y} objects.
+            var p = projection(n.loc);
+            return { x: p[0], y: p[1] };
+        });
+
+        // tolerance of 0.8 - adjust as needed. simplification is guaranteed
+        // to produce fewer points in output than input
+        var simplified = simplify(points, 0.8).map(function(p) {
+            // convert back to iD's 2-elem array pattern
+            return { loc: projection.invert([p.x, p.y]) };
+        });
+
+        // first reuse all singleParent nodes. the
+        // ones not reused will be deleted
+        for (i = 0; i < simplified.length; i++) {
+            // reuse nodes for single parent nodes
+            idx = closestIndex(singleParent, simplified[i].loc);
+            node = singleParent[idx];
+            singleParent.splice(idx, 1);
+            graph = graph.replace(node.move(simplified[i].loc));
+            ids.push(node.id);
+        }
+
+        // then insert all multiParent nodes
+        for (i = 0; i < multiParent.length; i++) {
+            idx = closestIndex(simplified, multiParent[i].loc);
+            ids.splice(idx, 0, multiParent[i].id);
+        }
+
+        graph = graph.replace(way.update({ nodes: ids }));
+
+        // remove all single parent nodes that were not reused earlier
+        for (i = 0; i < singleParent.length; i++) {
+            graph = iD.actions.DeleteNode(singleParent[i].id)(graph);
         }
 
         return graph;
