f0577100d90e948d0141b453d9d4c97d3677e20a
Tom MacWright <tom@macwright.org>
2013-02-27 19:39:06 -0500

    Fix translation and zoom-dependency of simplify
    * Use a fixed projection for simplification
    * Correctly translate simplify

---
+++ js/id/operations/simplify.js
@@ -0,0 +1,25 @@
+iD.operations.Simplify = function(selection, context) {
+    var entityId = selection[0],
+        action = iD.actions.Simplify(entityId, context.projection);
+
+    var operation = function() {
+        var annotation = t('operations.simplify.annotation.' + context.geometry(entityId));
+        context.perform(action, annotation);
+    };
+
+    operation.available = function() {
+        return selection.length === 1 &&
+            context.entity(entityId).type === 'way';
+    };
+
+    operation.enabled = function() {
+        return action.enabled(context.graph());
+    };
+
+    operation.id = "simplify";
+    operation.keys = [t('operations.simplify.key')];
+    operation.title = t('operations.simplify.title');
+    operation.description = t('operations.simplify.description');
+
+    return operation;
+};
