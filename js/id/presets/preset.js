d1d90ab691d2ea3f9215228b160cb058e363820f
Ansis Brammanis <ansis.brammanis@gmail.com>
2013-03-01 18:43:47 -0500

    misc preset

--- js/id/presets/preset.js
+++ js/id/presets/preset.js
@@ -16,7 +16,7 @@ iD.presets.Preset = function(preset, forms) {
         var tags = preset.match.tags;
         for (var t in tags) {
             if (entity.tags[t] !== tags[t] &&
-                (tags[t] !== '*' || t in entity.tags)) return -1;
+                !(tags[t] === '*' && t in entity.tags)) return -1;
         }
         return Object.keys(preset.match.tags).length;
     };
