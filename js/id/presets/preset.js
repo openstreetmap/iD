06a7f6a16c09547fa19a09f1223f9950e4e3ca2e
Ansis Brammanis <ansis.brammanis@gmail.com>
2013-03-04 19:16:46 -0500

    move preset tag application to presets/preset.js

--- js/id/presets/preset.js
+++ js/id/presets/preset.js
@@ -21,5 +21,31 @@ iD.presets.Preset = function(preset, forms) {
         return Object.keys(preset.match.tags).length;
     };
 
+    preset.removeTags = function(tags, geometry) {
+        tags = _.omit(tags, _.keys(preset.match.tags));
+
+        for (var i in preset.form) {
+            var field = preset.form[i];
+            if (field['default'] && field['default'][geometry] == tags[field.key]) {
+                delete tags[field.key];
+            }
+        }
+        return tags;
+
+    };
+
+    preset.applyTags = function(tags, geometry) {
+        for (var k in preset.match.tags) {
+            if (preset.match.tags[k] !== '*') tags[k] = preset.match.tags[k];
+        }
+
+        for (var f in preset.form) {
+            f = preset.form[f];
+            if (f.key && !tags[f.key] && f['default'] && f['default'][geometry]) {
+                tags[f.key] = f['default'][geometry];
+            }
+        }
+    };
+
     return preset;
 };
