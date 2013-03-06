7cdfa8daf3611142805a833c2692499931ed4466
John Firebaugh <john.firebaugh@gmail.com>
2013-03-06 14:49:19 -0800

    Remove duplicated code

--- js/id/renderer/layers.js
+++ js/id/renderer/layers.js
@@ -1,15 +1,2 @@
 iD.layers = iD.data.imagery.map(iD.BackgroundSource.template);
-
-iD.layers.push((function() {
-    function custom() {
-        var template = window.prompt('Enter a tile template. Valid tokens are {z}, {x}, {y} for Z/X/Y scheme and {u} for quadtile scheme.');
-        if (!template) return null;
-        if (template.match(/google/g)) return null;
-        return iD.BackgroundSource.template({
-            template: template,
-            name: 'Custom (customized)'
-        });
-    }
-    custom.data = { name: 'Custom' };
-    return custom;
-})());
+iD.layers.push(iD.BackgroundSource.Custom);
