66b9054e3865911d1b5ba2c8b6cdb655401afb35
John Firebaugh <john.firebaugh@gmail.com>
2013-03-18 14:41:01 -0700

    Generate preset translations from .json files
    This makes the .json files the one-stop-shop for adding/modifying
    presets. You don't have to remember to also update presets.yaml.

--- build.js
+++ build.js
@@ -1,6 +1,7 @@
 var fs = require('fs'),
     path = require('path'),
-    glob = require('glob');
+    glob = require('glob'),
+    YAML = require('js-yaml');
 
 function read(f) {
     return JSON.parse(fs.readFileSync(f));
@@ -14,17 +15,35 @@ function rp(f) {
     return r('presets/' + f);
 }
 
+var translations = {
+    fields: {},
+    presets: {}
+};
+
 var fields = {};
 glob.sync(__dirname + '/data/presets/fields/*.json').forEach(function(file) {
-    fields[path.basename(file, '.json')] = read(file);
+    var field = read(file),
+        id = path.basename(file, '.json');
+    translations.fields[id] = {label: field.label};
+    if (field.strings) {
+        for (var i in field.strings) {
+            translations.fields[id][i] = field.strings[i];
+        }
+    }
+    fields[id] = field;
 });
 fs.writeFileSync('data/presets/fields.json', JSON.stringify(fields, null, 4));
 
 var presets = {};
 glob.sync(__dirname + '/data/presets/presets/**/*.json').forEach(function(file) {
-    presets[file.match(/presets\/presets\/([^.]*)\.json/)[1]] = read(file);
+    var preset = read(file),
+        id = file.match(/presets\/presets\/([^.]*)\.json/)[1];
+    translations.presets[id] = {name: preset.name};
+    presets[id] = preset;
 });
 fs.writeFileSync('data/presets/presets.json', JSON.stringify(presets, null, 4));
+
+fs.writeFileSync('data/presets.yaml', YAML.dump({en: {presets: translations}}));
 
 fs.writeFileSync('data/data.js', 'iD.data = ' + JSON.stringify({
     deprecated: r('deprecated.json'),
