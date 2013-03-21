52e9259108950be4165fb4cf8175496c6649a246
John Firebaugh <john.firebaugh@gmail.com>
2013-03-20 17:02:24 -0700

    Show Bing logo (#1095)

--- js/id/ui/attribution.js
+++ js/id/ui/attribution.js
@@ -9,16 +9,19 @@ iD.ui.Attribution = function(context) {
 
         if (!d) return;
 
-        var desc = t('imagery.provided_by', {
-            source: (d.data.sourcetag || d.data.name)
-        });
+        var source = d.data.sourcetag || d.data.name;
+
+        if (d.data.logo) {
+            source = '<img src="img/' + d.data.logo + '">'
+        }
+
+        var desc = t('imagery.provided_by', {source: source});
 
         if (d.data.terms_url) {
             provided_by.append('a')
-                .attr('href', (d.data.terms_url || ''))
+                .attr('href', d.data.terms_url)
                 .attr('target', '_blank')
-                .classed('disabled', !d.data.terms_url)
-                .text(desc);
+                .html(desc);
         } else {
             provided_by.text(desc);
         }
