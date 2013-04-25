4eccd7015f8380dae44a85543c6321fe2dcff901
Ansis Brammanis <ansis.brammanis@gmail.com>
2013-04-25 18:41:49 -0400

    corner destroying resampling for oneway markers

--- js/id/svg/surface.js
+++ js/id/svg/surface.js
@@ -34,12 +34,13 @@ iD.svg.Surface = function(context) {
                 id: 'oneway-marker',
                 viewBox: '0 0 10 10',
                 refY: 2.5,
+                refX: 5,
                 markerWidth: 2,
                 markerHeight: 2,
                 orient: 'auto'
             })
             .append('path')
-            .attr('d', 'M 0 0 L 5 2.5 L 0 5 z');
+            .attr('d', 'M 5 3 L 0 3 L 0 2 L 5 2 L 5 0 L 10 2.5 L 5 5 z');
 
         var patterns = defs.selectAll('pattern')
             .data([
@@ -109,7 +110,7 @@ iD.svg.Surface = function(context) {
             maki));
 
         var layers = selection.selectAll('.layer')
-            .data(['fill', 'shadow', 'casing', 'stroke', 'text', 'hit', 'halo', 'label']);
+            .data(['fill', 'shadow', 'casing', 'stroke', 'oneway', 'hit', 'halo', 'label']);
 
         layers.enter().append('g')
             .attr('class', function(d) { return 'layer layer-' + d; });
