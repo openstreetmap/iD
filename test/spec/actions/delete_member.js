cac654e8ab0a6906b3bee0814275627b1389522e
John Firebaugh <john.firebaugh@gmail.com>
2013-05-16 12:44:06 -0700

    Add iD.actions.ChangeMember

--- test/spec/actions/delete_member.js
+++ test/spec/actions/delete_member.js
@@ -1,7 +1,7 @@
 describe("iD.actions.DeleteMember", function () {
     it("removes the member at the specified index", function () {
         var node     = iD.Node(),
-            relation = iD.Relation({members: [node.id]}),
+            relation = iD.Relation({members: [{id: node.id}]}),
             action   = iD.actions.DeleteMember(relation.id, 0),
             graph    = action(iD.Graph([node, relation]));
         expect(graph.entity(relation.id).members).to.eql([]);
