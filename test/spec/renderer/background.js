5a8bf0fada75903f4f4c6b0a97060d7e7f099f39
John Firebaugh <john.firebaugh@gmail.com>
2013-05-28 19:50:40 -0700

    Fix tests

--- test/spec/renderer/background.js
+++ test/spec/renderer/background.js
@@ -21,27 +21,4 @@ describe('iD.Background', function() {
             expect(c.size()).to.eql([100,100]);
         });
     });
-
-    describe('iD.BackgroundSource.Template', function() {
-        it('does not error with blank template', function() {
-            var source = iD.BackgroundSource.template({ template: '' });
-            expect(source([0,1,2])).to.equal('');
-        });
-        it('generates a tile-generating source', function() {
-            var source = iD.BackgroundSource.template({ template: '{z}/{x}/{y}' });
-            expect(source([0,1,2])).to.equal('2/0/1');
-        });
-        it('supports subdomains', function() {
-            var source = iD.BackgroundSource.template({ template: '{t}/{z}/{x}/{y}', subdomains: ['apples', 'oranges'] });
-            expect(source([0,1,2])).to.equal('apples/2/0/1');
-        });
-        it('distributes requests between subdomains', function() {
-            var source = iD.BackgroundSource.template({ template: '{t}/{z}/{x}/{y}', subdomains: ['apples', 'oranges'] });
-            expect(source([0,1,1])).to.equal('oranges/1/0/1');
-        });
-        it('supports josm style templates', function() {
-            var source = iD.BackgroundSource.template({ template: '{switch:foo,bar}/{zoom}/{x}/{y}' });
-            expect(source([0,1,1])).to.equal('bar/1/0/1');
-        });
-    });
 });
