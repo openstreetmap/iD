6ad14892a5c72845aceec0e854af7ae11956d49f
Bryan Housel <bryan@7thposition.com>
2014-03-02 13:37:21 -0500

    revert autogenerated area_keys too

--- js/id/core/area_keys.js
+++ js/id/core/area_keys.js
@@ -38,35 +38,6 @@ iD.areaKeys = {
     "golf": {
         "hole": true
     },
-    "highway": {
-        "bridleway": true,
-        "bus_stop": true,
-        "crossing": true,
-        "cycleway": true,
-        "living_street": true,
-        "mini_roundabout": true,
-        "motorway": true,
-        "motorway_junction": true,
-        "motorway_link": true,
-        "path": true,
-        "primary": true,
-        "primary_link": true,
-        "residential": true,
-        "road": true,
-        "secondary": true,
-        "secondary_link": true,
-        "service": true,
-        "steps": true,
-        "stop": true,
-        "tertiary": true,
-        "tertiary_link": true,
-        "track": true,
-        "traffic_signals": true,
-        "trunk": true,
-        "trunk_link": true,
-        "turning_circle": true,
-        "unclassified": true
-    },
     "historic": {
         "boundary_stone": true
     },
@@ -99,19 +70,6 @@ iD.areaKeys = {
     },
     "public_transport": {
         "stop_position": true
-    },
-    "railway": {
-        "abandoned": true,
-        "disused": true,
-        "funicular": true,
-        "halt": true,
-        "level_crossing": true,
-        "monorail": true,
-        "narrow_gauge": true,
-        "rail": true,
-        "subway": true,
-        "subway_entrance": true,
-        "tram": true
     },
     "shop": {},
     "tourism": {
