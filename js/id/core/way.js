0c5f563a262edbea4911da0c4f65f380975e6276
Bryan Housel <bryan@7thposition.com>
2014-06-09 23:39:16 -0400

    add test for iD.Entity.layer(), fix spelling of man_made.

--- js/id/core/way.js
+++ js/id/core/way.js
@@ -60,7 +60,7 @@ _.extend(iD.Way.prototype, {
         if (this.tags.cutting) return -1;
         if (this.tags.tunnel) return -1;
         if (this.tags.waterway) return -1;
-        if (this.tags.manmade === 'pipeline') return -10;
+        if (this.tags.man_made === 'pipeline') return -10;
         if (this.tags.boundary) return -10;
         return 0;
     },
