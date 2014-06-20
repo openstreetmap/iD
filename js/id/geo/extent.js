c316f87c6f714db9321f56f39016af5c9c095765
Bryan Housel <bryan@7thposition.com>
2014-06-20 18:01:53 -0400

    Fix buggy padByMeters (PI/180 is needed here)

--- js/id/geo/extent.js
+++ js/id/geo/extent.js
@@ -58,8 +58,8 @@ _.extend(iD.geo.Extent.prototype, {
     },
 
     padByMeters: function(meters) {
-        var dLat = meters / 111200,
-            dLon = meters / 111200 / Math.abs(Math.cos(this.center()[1]));
+        var dLat = meters / 111132.954,
+            dLon = meters / 111132.954 / Math.abs(Math.cos(this.center()[1] * (Math.PI / 180)));
         return iD.geo.Extent(
                 [this[0][0] - dLon, this[0][1] - dLat],
                 [this[1][0] + dLon, this[1][1] + dLat]);
