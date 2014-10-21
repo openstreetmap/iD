4d2d24bbaa13c2e58d8499332f336a14b710cb89
John Firebaugh <john.firebaugh@gmail.com>
2014-10-20 18:18:48 -0700

    Need to encode = as well as & (fixes #2406)

--- js/id/util.js
+++ js/id/util.js
@@ -41,7 +41,7 @@ iD.util.stringQs = function(str) {
 };
 
 iD.util.qsString = function(obj, noencode) {
-    function softEncode(s) { return s.replace('&', '%26'); }
+    function softEncode(s) { return s.replace('&', '%26').replace('=', '%3D'); }
     return Object.keys(obj).sort().map(function(key) {
         return encodeURIComponent(key) + '=' + (
             noencode ? softEncode(obj[key]) : encodeURIComponent(obj[key]));
