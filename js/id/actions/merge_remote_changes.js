977e29cfdb2b622a7032ba6d19e287bae0a40ad6
Bryan Housel <bryan@7thposition.com>
2014-12-11 23:52:53 -0500

    Most iD.actions.MergeRemoteChanges features complete (todo: merging ways where nodelist has not been reordered)

--- js/id/actions/merge_remote_changes.js
+++ js/id/actions/merge_remote_changes.js
@@ -1,29 +1,57 @@
 iD.actions.MergeRemoteChanges = function(id, localGraph, remoteGraph) {
     var base = localGraph.base().entities[id],
-        local = localGraph.entities[id],
-        remote = remoteGraph.entities[id],
-        option = 'safe',  // 'safe', 'force_local', 'force_remote'
-        target;
+        local = localGraph.entity(id),
+        remote = remoteGraph.entity(id),
+        option = 'safe';  // 'safe', 'force_local', 'force_remote'
 
-    function sameLocation() {
-        var epsilon = 1e-6;
-        return (Math.abs(remote.loc[0] - local.loc[0]) < epsilon) &&
-                (Math.abs(remote.loc[1] - local.loc[1]) < epsilon);
+
+    function mergeLocation(target) {
+        function pointEqual(a, b) {
+            var epsilon = 1e-6;
+            return (Math.abs(a[0] - b[0]) < epsilon) && (Math.abs(a[1] - b[1]) < epsilon);
+        }
+
+        if (!pointEqual(remote.loc, local.loc)) {
+            return (option === 'force_remote') ? target.update({loc: remote.loc}) : undefined;
+        }
+        return target;
     }
 
-    function mergeChildren() {
+    function mergeRemoteChildren(target) {
+        if (option === 'force_remote') {
+            return target.update({nodes: remote.nodes});
+        }
+
         // todo, support non-destructive merging
-        return _.isEqual(local.nodes, remote.nodes);
+        // for now fail on any change..
+        if (!_.isEqual(local.nodes, remote.nodes)) {
+            return;
+        }
+        return target;
     }
 
-    function mergeMembers() {
+    function mergeRemoteMembers(target) {
+        if (option === 'force_remote') {
+            return target.update({members: remote.members});
+        }
+
         // todo, support non-destructive merging
-        return _.isEqual(local.members, remote.members);
+        // for now fail on any change..
+        if (!_.isEqual(local.members, remote.members)) {
+            return;
+        }
+        return target;
     }
 
-    function mergeTags() {
+    function mergeRemoteTags(target) {
+        if (!target) { return; }
+        if (option === 'force_remote') {
+            return target.update({tags: remote.tags});
+        }
+
         var keys = _.reject(_.union(_.keys(base.tags), _.keys(remote.tags)), ignoreKey),
-            tags = _.cloneDeep(target.tags);
+            tags = _.cloneDeep(target.tags),
+            changed = false;
 
         function ignoreKey(k) {
             return k.indexOf('tiger:') === 0 || _.contains(iD.data.discarded, k);
@@ -33,34 +61,35 @@ iD.actions.MergeRemoteChanges = function(id, localGraph, remoteGraph) {
             var k = keys[i];
             if (remote.tags[k] !== base.tags[k]) {  // tag modified remotely..
                 if (local.tags[k] && local.tags[k] !== remote.tags[k]) {
-                    return false;
+                    return;
                 } else {
                     tags[k] = remote.tags[k];
+                    changed = true;
                 }
             }
         }
 
-        target = target.update({tags: tags});
-        return true;
+        return changed ? target.update({tags: tags}) : target;
     }
 
     var action = function(graph) {
+        var target = iD.Entity(local, {version: remote.version});
 
-        target = iD.Entity(local, {version: remote.version});
-        if (option === 'force_remote') { return graph.replace(remote); }
-        if (option === 'force_local') { return graph.replace(target); }
-
-        // otherwise, safe mode: only permit non-destructive merges..
-        var doMerge;
-        if (target.type === 'node') {
-            doMerge = (sameLocation() && mergeTags());
-        } else if (target.type === 'way') {
-            doMerge = (mergeChildren() && mergeTags());
-        } else if (target.type === 'relation') {
-            doMerge = (mergeMembers() && mergeTags());
+        if (option === 'force_local') {
+            return graph.replace(target);
         }
 
-        return doMerge ? graph.replace(target) : graph;
+        if (target.type === 'node') {
+            target = mergeLocation(target);
+        } else if (target.type === 'way') {
+            graph.rebase(remoteGraph.childNodes(remote), [graph], false);
+            target = mergeRemoteChildren(target);
+        } else if (target.type === 'relation') {
+            target = mergeRemoteMembers(target);
+        }
+
+        target = mergeRemoteTags(target);
+        return target ? graph.replace(target) : graph;
     };
 
     action.withOption = function(opt) {
