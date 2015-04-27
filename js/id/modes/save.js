a565b72f7da030bbe5ca798a38f1926ec626223b
Bryan Housel <bryan@7thposition.com>
2015-04-27 10:18:07 -0400

    Before saving, revert entities where user chose "keep theirs"

--- js/id/modes/save.js
+++ js/id/modes/save.js
@@ -175,6 +175,20 @@ iD.modes.Save = function(context) {
                     selection.remove();
                 })
                 .on('save', function() {
+                    for (var i = 0; i < conflicts.length; i++) {
+                        if (conflicts[i].chosen === 1) {  // user chose "keep theirs"
+                            var entity = context.entity(conflicts[i].id);
+                            if (entity.type === 'way') {
+                                var children = _.uniq(entity.nodes);
+                                for (var j = 0; j < children.length; j++) {
+                                    var child = context.entity(children[j]);
+                                    history.replace(iD.actions.Revert(child));
+                                }
+                            }
+                            history.replace(iD.actions.Revert(entity));
+                        }
+                    }
+
                     selection.remove();
                     save(e, true);
                 })
