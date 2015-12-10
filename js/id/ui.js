6caad1bcea767877fad5113849f1c604a7f83370
Bryan Housel <bryan@mapbox.com>
2015-12-10 14:02:47 -0800

    Set default map view to 2/0/0 (zoomed out world) (should cut down on errant edits in Washington DC)

--- js/id/ui.js
+++ js/id/ui.js
@@ -9,7 +9,7 @@ iD.ui = function(context) {
         hash();
 
         if (!hash.hadHash) {
-            map.centerZoom([-77.02271, 38.90085], 20);
+            map.centerZoom([0, 0], 2);
         }
 
         container.append('svg')
