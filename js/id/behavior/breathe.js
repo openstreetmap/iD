c185189512a24024f2b79917c22316584c36f7a4
Bryan Housel <bryan@mapbox.com>
2015-12-18 20:39:45 -0500

    Adjust interpolation params, DRY code, perf improvements

--- js/id/behavior/breathe.js
+++ js/id/behavior/breathe.js
@@ -1,99 +1,104 @@
 iD.behavior.Breathe = function() {
-    var duration = 1000,
+    var duration = 800,
         selector = '.selected.shadow, .selected .shadow',
+        selected = d3.select(null),
+        classed = [],
         params = {},
-        done, selection;
+        done;
 
     function reset(selection) {
-        return selection
-            .style('fill-opacity', null)
-            .style('r', null)
+        selection
             .style('stroke-opacity', null)
-            .style('stroke-width', null);
+            .style('stroke-width', null)
+            .style('fill-opacity', null)
+            .style('r', null);
     }
 
-    function reselect(surface) {
-        return function() {
-            if (done) return true;
-            var currSelection = surface.selectAll(selector);
-            if (_.isEqual(currSelection, selection)) return false;  // no change
-
-            selection = currSelection;
-            if (selection.empty()) return false;
-
-            // reset styles, calculate animation params
-            selection
-                .call(reset)
-                .each(function calcAnimationParams(d) {
-                    if (params.hasOwnProperty(d.id)) return;
-
-                    // determine default opacity and width
-                    var s = d3.select(this),
-                        tag = s.node().tagName,
-                        p = {},
-                        opacity, width;
-
-                    if (tag === 'circle') {
-                        opacity = parseFloat(s.style('fill-opacity') || 0.5);
-                        width = parseFloat(s.style('r') || 15.5);
-                    } else {
-                        opacity = parseFloat(s.style('stroke-opacity') || 0.7);
-                        width = parseFloat(s.style('stroke-width') || 10);
-                    }
-
-                    // calculate min/max interpolation params based on defaults..
-                    p.tag = tag;
-                    p.opacity0 = Math.max(opacity - 0.4, 0.1);
-                    p.opacity1 = Math.min(opacity + 0.2, 1.0);
-                    p.width0 = Math.max(width - (tag === 'circle' ? 1 : 2), 4);
-                    p.width1 = width + 2;
-                    params[d.id] = p;
-                });
-
-            inhale();
-        };
+    function setAnimationParams(transition, fromTo) {
+        transition
+            .style('stroke-opacity', function(d) { return params[d.id][fromTo].opacity; })
+            .style('stroke-width', function(d) { return params[d.id][fromTo].width; })
+            .style('fill-opacity', function(d) { return params[d.id][fromTo].opacity; })
+            .style('r', function(d) { return params[d.id][fromTo].width; });
     }
 
-    function inhale() {
-        if (done || selection.empty()) {
-            selection.call(reset);
+    function calcAnimationParams(selection) {
+        selection
+            .call(reset)
+            .each(function(d) {
+                var s = d3.select(this),
+                    tag = s.node().tagName,
+                    p = {'from': {}, 'to': {}},
+                    opacity, width;
+
+                // determine base opacity and width
+                if (tag === 'circle') {
+                    opacity = parseFloat(s.style('fill-opacity') || 0.5);
+                    width = parseFloat(s.style('r') || 15.5);
+                } else {
+                    opacity = parseFloat(s.style('stroke-opacity') || 0.7);
+                    width = parseFloat(s.style('stroke-width') || 10);
+                }
+
+                // calculate from/to interpolation params..
+                p.tag = tag;
+                p.from.opacity = opacity * 0.6;
+                p.to.opacity = opacity * 1.25;
+                p.from.width = width * 0.9;
+                p.to.width = width * (tag === 'circle' ? 1.5 : 1.25);
+                params[d.id] = p;
+            });
+    }
+
+    function run(surface, fromTo) {
+        var toFrom = (fromTo === 'from' ? 'to': 'from'),
+            currSelected = surface.selectAll(selector),
+            currClassed = Array.prototype.slice.call(surface.node().classList),
+            n = 0;
+
+        if (done || currSelected.empty()) {
+            selected.call(reset);
             return;
         }
-        selection
-            .transition()
-            .style('stroke-opacity', function(d) { return params[d.id].opacity1; })
-            .style('stroke-width', function(d) { return params[d.id].width1; })
-            .style('fill-opacity', function(d) { return params[d.id].opacity1; })
-            .style('r', function(d) { return params[d.id].width1; })
-            .duration(duration)
-            .each('end', exhale);
-    }
 
-    function exhale() {
-        if (done || selection.empty()) {
-            selection.call(reset);
-            return;
+        if (!_.isEqual(currSelected, selected) || !_.isEqual(currClassed, classed)) {
+            selected.call(reset);
+            classed = _.clone(currClassed);
+            selected = currSelected.call(calcAnimationParams);
         }
-        selection
+
+        selected
             .transition()
-            .style('stroke-opacity', function(d) { return params[d.id].opacity0; })
-            .style('stroke-width', function(d) { return params[d.id].width0; })
-            .style('fill-opacity', function(d) { return params[d.id].opacity0; })
-            .style('r', function(d) { return params[d.id].width0; })
+            .call(setAnimationParams, fromTo)
             .duration(duration)
-            .each('end', inhale);
+            .each(function() { ++n; })
+            .each('end', function() {
+                if (!--n) {  // call once
+                    surface.call(run, toFrom);
+                }
+            });
     }
 
     var breathe = function(surface) {
         done = false;
-        d3.timer(reselect(surface), 200);
+        d3.timer(function() {
+            if (done) return true;
+
+            var currSelected = surface.selectAll(selector);
+            if (currSelected.empty()) return false;
+
+            surface.call(run, 'from');
+            return true;
+        }, 200);
     };
 
-    breathe.off = function() {
+    breathe.off = function(surface) {
         done = true;
-        if (selection) {
-            selection.call(reset);
-        }
+        d3.timer.flush();
+        selected
+            .transition()
+            .call(reset)
+            .duration(0);
     };
 
     return breathe;
