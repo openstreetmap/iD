b9e24fb96bbf6cd0fab09f34899262433ea0f8c2
Bryan Housel <bryan@mapbox.com>
2016-04-27 22:46:01 -0400

    Exclude name tag when validating whether a feature has tags (closes #3091)

--- js/id/validations/missing_tag.js
+++ js/id/validations/missing_tag.js
@@ -1,12 +1,18 @@
 iD.validations.MissingTag = function() {
 
+    // Slightly stricter check than Entity#isUsed (#3091)
+    function hasTags(entity, graph) {
+        return _.without(Object.keys(entity.tags), 'area', 'name').length > 0 ||
+            graph.parentRelations(entity).length > 0;
+    }
+
     var validation = function(changes, graph) {
         var warnings = [];
         for (var i = 0; i < changes.created.length; i++) {
             var change = changes.created[i],
                 geometry = change.geometry(graph);
 
-            if ((geometry === 'point' || geometry === 'line' || geometry === 'area') && !change.isUsed(graph)) {
+            if ((geometry === 'point' || geometry === 'line' || geometry === 'area') && !hasTags(change, graph)) {
                 warnings.push({
                     id: 'missing_tag',
                     message: t('validations.untagged_' + geometry),
