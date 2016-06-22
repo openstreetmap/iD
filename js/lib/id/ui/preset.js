304b3b35799877868cc0ff04ce06577dd8e59cc0
Bryan Housel <bryan@mapbox.com>
2016-06-22 16:21:51 -0400

    Restore numeric lanes preset, but keep code for new WIP lanes field

--- js/lib/id/ui/preset.js
+++ js/lib/id/ui/preset.js
@@ -1238,6 +1238,51 @@
       return d3.rebind(localized, dispatch, 'on');
   }
 
+  function lanes(field, context) {
+      var dispatch = d3.dispatch('change'),
+          wayID,
+          laneData;
+
+      function lanes(selection) {
+          // if form field is hidden or has detached from dom, clean up.
+          if (!d3.select('.inspector-wrap.inspector-hidden').empty() || !selection.node().parentNode) {
+              selection.call(lanes.off);
+              return;
+          }
+
+          laneData = context.entity(wayID).lanes();
+
+          var wrap = selection.selectAll('.preset-input-wrap')
+              .data([0]);
+
+          var enter = wrap.enter()
+              .append('div')
+              .attr('class', 'preset-input-wrap');
+
+          enter
+              .append('div')
+              .attr('class', 'lane-count')
+              .append('span');
+
+          selection.selectAll('.lane-count')
+            .text(laneData.tagged.lanes.count || laneData.defaults.lanes.count);
+
+      }
+
+
+      lanes.entity = function(_) {
+          if (!wayID || wayID !== _.id) {
+              wayID = _.id;
+          }
+      };
+
+      lanes.tags = function() {};
+      lanes.focus = function() {};
+      lanes.off = function() {};
+
+      return d3.rebind(lanes, dispatch, 'on');
+  }
+
   function maxspeed(field, context) {
       var dispatch = d3.dispatch('change'),
           entity,
@@ -1851,6 +1896,7 @@
   exports.email = url;
   exports.tel = url;
   exports.localized = localized;
+  exports.lanes = lanes;
   exports.maxspeed = maxspeed;
   exports.radio = radio;
   exports.restrictions = restrictions;
