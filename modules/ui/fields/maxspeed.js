1c3a2505762e3a8470ff14195ac0cd5d75c09b71
Bryan Housel <bryan@mapbox.com>
2016-08-01 23:16:05 -0400

    Add missing 60 mph maxspeed option

--- modules/ui/fields/maxspeed.js
+++ modules/ui/fields/maxspeed.js
@@ -10,7 +10,7 @@ export function maxspeed(field, context) {
         input;
 
     var metricValues = [20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120],
-        imperialValues = [20, 25, 30, 35, 40, 45, 50, 55, 65, 70];
+        imperialValues = [20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70];
 
     function maxspeed(selection) {
         combobox = d3.combobox();
