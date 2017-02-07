openstreetmap/iD

commit 919234d2df7aa89cc067c6ec564de339296e0330

Make sure to select `vertex-persistent` when looking for siblings (closes #3824)
When hovering, there is also a companion `vertex-hover` element that might
sometimes get selected, and doesn't have the sibling class that enables drag.

modules/modes/drag_node.js
@@ -102,7 +102,7 @@ export function modeDragNode(context) {
         // vertices classed "sibling" include:  (see svg/vertices.js)
         // - children of selected ways or multipolygons
         // - vertices sharing a way with selected vertices
-        var selection = d3.selectAll('g.' + entity.id),
+        var selection = d3.selectAll('g.vertex-persistent.' + entity.id),
             isSelected = !selection.empty() &&
                 (selection.classed('selected') || selection.classed('sibling'));
 

commit 86118ece4e744af0333d1443836765b2645c392f

Fix node drag regression introduced in 919234d (closes #3829)

modules/modes/drag_node.js
@@ -99,14 +99,20 @@ export function modeDragNode(context) {
     function start(entity) {
         wasMidpoint = entity.type === 'midpoint';
 
-        // vertices classed "sibling" include:  (see svg/vertices.js)
-        // - children of selected ways or multipolygons
-        // - vertices sharing a way with selected vertices
-        var selection = d3.selectAll('g.vertex-persistent.' + entity.id),
-            isSelected = !selection.empty() &&
-                (selection.classed('selected') || selection.classed('sibling'));
+        // Things allowed to be dragged include:
+        // - midpoints
+        // - nodes that are selected
+        // - vertices that are selected
+        // - vertices classed 'sibling' which includes (see svg/vertices.js)
+        //   - children of selected ways or multipolygons
+        //   - vertices sharing a way with selected vertices
+        var selector = 'g.node.point.selected.' + entity.id +
+            ', g.vertex-persistent.selected.' + entity.id +
+            ', g.vertex-persistent.sibling.' + entity.id;
 
-        isCancelled = d3.event.sourceEvent.shiftKey || !(wasMidpoint || isSelected) ||
+        var isDraggable = wasMidpoint || !d3.select(selector).empty();
+
+        isCancelled = d3.event.sourceEvent.shiftKey || !isDraggable ||
             context.features().hasHiddenConnections(entity, context.graph());
 
         if (isCancelled) {