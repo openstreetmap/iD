1db4ea86f728323e8df7950a6841a52ea754491d
Bryan Housel <bryan@mapbox.com>
2017-02-16 10:20:37 -0500

    Add icons to operation flash messages

--- modules/behavior/operation.js
+++ modules/behavior/operation.js
@@ -1,11 +1,35 @@
 import * as d3 from 'd3';
 import { d3keybinding } from '../lib/d3.keybinding.js';
-import { uiFlash } from '../ui/index';
+import { uiFlash } from '../ui';
 
 
 /* Creates a keybinding behavior for an operation */
 export function behaviorOperation(context) {
     var which, keybinding;
+
+
+    function drawIcon(selection) {
+        var button = selection
+            .append('svg')
+            .attr('class', 'operation-icon')
+            .append('g')
+            .attr('class', 'radial-menu-item radial-menu-item-' + which.id)
+            .attr('transform', 'translate(18,18)')
+            .classed('disabled', which.disabled());
+
+        button
+            .append('circle')
+            .attr('r', 15);
+
+        button
+            .append('use')
+            .attr('transform', 'translate(-10,-10)')
+            .attr('width', '20')
+            .attr('height', '20')
+            .attr('xlink:href', '#operation-' + which.id);
+
+        return selection;
+    }
 
 
     var behavior = function () {
@@ -14,11 +38,23 @@ export function behaviorOperation(context) {
             keybinding.on(which.keys, function() {
                 d3.event.preventDefault();
                 var disabled = which.disabled();
+
                 if (disabled) {
-                    uiFlash(2500, 500).text(which.tooltip);
+                    uiFlash(2500, 500)
+                        .html('')
+                        .call(drawIcon)
+                        .append('div')
+                        .attr('class', 'operation-tip')
+                        .text(which.tooltip);
+
                 } else {
-                    var annotation = which.annotation() || which.title;
-                    uiFlash(1500, 250).text(annotation);
+                    uiFlash(1500, 250)
+                        .html('')
+                        .call(drawIcon)
+                        .append('div')
+                        .attr('class', 'operation-tip')
+                        .text(which.annotation() || which.title);
+
                     which();
                 }
             });
