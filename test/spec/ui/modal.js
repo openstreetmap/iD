b803730a4b4199f841976fae7110a597f4858f58
Bryan Housel <bryan@mapbox.com>
2017-04-24 14:57:47 -0400

    More modal and confirm tests, d3.timerFlush to avoid false failures

--- test/spec/ui/modal.js
+++ test/spec/ui/modal.js
@@ -10,10 +10,55 @@ describe('iD.uiModal', function () {
     });
 
     it('can be instantiated', function() {
-        var modal = iD.uiModal(elem)
-            .select('.content')
-            .text('foo');
-        expect(modal).to.be.ok;
-        happen.keydown(document, { keyCode: 27 }); // dismiss
+        var selection = iD.uiModal(elem);
+        expect(selection).to.be.ok;
     });
+
+    it('has a content section', function () {
+        var selection = iD.uiModal(elem);
+        expect(selection.selectAll('div.content').size()).to.equal(1);
+    });
+
+    it('can be dismissed by calling close function', function (done) {
+        var selection = iD.uiModal(elem);
+        selection.close();
+        window.setTimeout(function() {
+            d3.timerFlush();
+            expect(selection.node().parentNode).to.be.null;
+            done();
+        }, 275);
+    });
+
+    it('can be dismissed by clicking the close button', function (done) {
+        var selection = iD.uiModal(elem);
+        happen.click(selection.select('button.close').node());
+        window.setTimeout(function() {
+            d3.timerFlush();
+            expect(selection.node().parentNode).to.be.null;
+            done();
+        }, 275);
+    });
+
+    it('can be dismissed by pressing escape', function (done) {
+        var selection = iD.uiModal(elem);
+        happen.keydown(document, {keyCode: 27});
+        happen.keyup(document, {keyCode: 27});
+        window.setTimeout(function() {
+            d3.timerFlush();
+            expect(selection.node().parentNode).to.be.null;
+            done();
+        }, 275);
+    });
+
+    it('can be dismissed by pressing backspace', function (done) {
+        var selection = iD.uiModal(elem);
+        happen.keydown(document, {keyCode: 8});
+        happen.keyup(document, {keyCode: 8});
+        window.setTimeout(function() {
+            d3.timerFlush();
+            expect(selection.node().parentNode).to.be.null;
+            done();
+        }, 275);
+    });
+
 });
