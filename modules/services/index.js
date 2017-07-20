6a6e3dabf1c6814a0e5942411753a6cda39a6c38
Kushan Joshi <0o3ko0@gmail.com>
2017-07-20 13:10:38 +0530

    setup imagery offset

--- modules/services/index.js
+++ modules/services/index.js
@@ -1,15 +1,17 @@
-import serviceMapillary from './mapillary';
-import serviceNominatim from './nominatim';
-import serviceOsm from './osm';
-import serviceTaginfo from './taginfo';
-import serviceWikidata from './wikidata';
-import serviceWikipedia from './wikipedia';
+import serviceMapillary from "./mapillary";
+import serviceNominatim from "./nominatim";
+import serviceOsm from "./osm";
+import serviceTaginfo from "./taginfo";
+import serviceWikidata from "./wikidata";
+import serviceWikipedia from "./wikipedia";
 
+import serviceImageryOffset from "./imagery_offset";
+window.offset = serviceImageryOffset;
 export var services = {
-    mapillary: serviceMapillary,
-    geocoder: serviceNominatim,
-    osm: serviceOsm,
-    taginfo: serviceTaginfo,
-    wikidata: serviceWikidata,
-    wikipedia: serviceWikipedia
+  mapillary: serviceMapillary,
+  geocoder: serviceNominatim,
+  osm: serviceOsm,
+  taginfo: serviceTaginfo,
+  wikidata: serviceWikidata,
+  wikipedia: serviceWikipedia
 };
