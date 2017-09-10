openstreetmap/iD

commit af776501d3e8445c94d8d5f4198df5a06ccf1ba2

Require locale, second pass (#3259)
* Require locale, second pass

* Remove t global, remove t where it isn't needed

modules/presets/preset.js
@@ -1,3 +1,4 @@
+import { t } from '../util/locale';
 import _ from 'lodash';
 export function Preset(id, preset, fields) {
     preset = _.clone(preset);

modules/ui/preset.js
@@ -1,3 +1,4 @@
+import { t } from '../util/locale';
 import _ from 'lodash';
 import { Browse } from '../modes/index';
 import { Disclosure } from './disclosure';

commit 9c738cb5b719511142c51a6f99d11b88658dab19

Restore preset ordering test with leading terms (regressed in af776501)
The ordering did not actually change, but the test was failing because
it was throwing away the preset terms.. preset.terms() actually returns
translated terms not original terms, and should default to returning the
original terms when translations are not available (like in testing).

modules/presets/preset.js
@@ -47,19 +47,19 @@ export function presetPreset(id, preset, fields) {
     };
 
 
-    var name = preset.name || '';
+    var origName = preset.name || '';
     preset.name = function() {
         if (preset.suggestion) {
             id = id.split('/');
             id = id[0] + '/' + id[1];
-            return name + ' - ' + t('presets.presets.' + id + '.name');
+            return origName + ' - ' + t('presets.presets.' + id + '.name');
         }
-        return preset.t('name', {'default': name});
+        return preset.t('name', { 'default': origName });
     };
 
-
+    var origTerms = (preset.terms || []).join();
     preset.terms = function() {
-        return preset.t('terms', {'default': ''}).toLowerCase().trim().split(/\s*,+\s*/);
+        return preset.t('terms', { 'default': origTerms }).toLowerCase().trim().split(/\s*,+\s*/);
     };
 
 