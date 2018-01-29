8831ac18c97571f2209147a715301740e26a017d
Benoit Costamagna <benoi@BEN-LAPTOP.norecrin.geopixel.com>
2018-01-29 12:50:50 +0100

    Added more details to the warning when many elements are deleted

--- modules/validations/many_deletions.js
+++ modules/validations/many_deletions.js
@@ -6,10 +6,18 @@ export function validationManyDeletions() {
 
     var validation = function(changes) {
         var warnings = [];
+        var nodes=0, ways=0, relations=0;
+
+        changes.deleted.forEach(function(c) {
+            if (c.type == 'relation') {relations++}
+            if (c.type == 'way') {ways++}
+            if (c.type == 'node') {nodes++}
+        });
         if (changes.deleted.length > threshold) {
             warnings.push({
                 id: 'many_deletions',
-                message: t('validations.many_deletions', { n: changes.deleted.length })
+                message: t('validations.many_deletions',
+                    { n: changes.deleted.length, p: nodes, l: ways, r: relations })
             });
         }
 
