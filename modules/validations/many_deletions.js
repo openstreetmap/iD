45b74d87308dc959622253ee48af80b7f9a8626a
Benoit Costamagna <benoi@BEN-LAPTOP.norecrin.geopixel.com>
2018-01-29 13:10:39 +0100

    Added difference between ways and areas

--- modules/validations/many_deletions.js
+++ modules/validations/many_deletions.js
@@ -4,20 +4,21 @@ import { t } from '../util/locale';
 export function validationManyDeletions() {
     var threshold = 100;
 
-    var validation = function(changes) {
+    var validation = function(changes, graph) {
         var warnings = [];
-        var nodes=0, ways=0, relations=0;
+        var nodes=0, ways=0, areas=0, relations=0;
 
         changes.deleted.forEach(function(c) {
             if (c.type == 'node') {nodes++}
-            else if (c.type == 'way') {ways++}
+            else if (c.type == 'way' && c.geometry(graph) == 'line') {ways++}
+            else if (c.type == 'way' && c.geometry(graph) == 'area') {areas++}
             else if (c.type == 'relation') {relations++}
         });
         if (changes.deleted.length > threshold) {
             warnings.push({
                 id: 'many_deletions',
                 message: t('validations.many_deletions',
-                    { n: changes.deleted.length, p: nodes, l: ways, r: relations })
+                    { n: changes.deleted.length, p: nodes, l: ways, a:areas, r: relations })
             });
         }
 
