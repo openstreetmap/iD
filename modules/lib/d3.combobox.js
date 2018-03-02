3917f02067fe8bcc8a70ff16d2748203b6c41535
Bryan Housel <bryan@mapbox.com>
2018-03-02 00:42:24 -0500

    When combobox is attached to textarea, let user up/down arrow (closes #4750)

--- modules/lib/d3.combobox.js
+++ modules/lib/d3.combobox.js
@@ -36,6 +36,7 @@ export function d3combobox() {
             .selectAll('div.combobox')
             .filter(function(d) { return d === input.node(); });
         var shown = !wrapper.empty();
+        var tagName = input.node() ? input.node().tagName.toLowerCase() : '';
 
         input
             .classed('combobox-input', true)
@@ -139,11 +140,13 @@ export function d3combobox() {
                    break;
                // up arrow
                case 38:
+                   if (tagName === 'textarea' && !shown) return;
                    nav(-1);
                    d3_event.preventDefault();
                    break;
                // down arrow
                case 40:
+                   if (tagName === 'textarea' && !shown) return;
                    nav(+1);
                    d3_event.preventDefault();
                    break;
