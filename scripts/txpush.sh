0ba8aab93a95d70379fb7927ce08c89517d914b6
Bryan Housel <bryan@mapbox.com>
2018-05-07 12:24:23 -0400

    Test Node 10 on travis matrix build (closes #5028)

--- scripts/txpush.sh
+++ scripts/txpush.sh
@@ -10,7 +10,7 @@ echo "TRAVIS_NODE_VERSION=$TRAVIS_NODE_VERSION"
 if [[ "$TRAVIS" != "true" ]]; then exit 0; fi
 if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then exit 0; fi
 if [[ "$TRAVIS_BRANCH" != "master" ]]; then exit 0; fi
-if [[ "$TRAVIS_NODE_VERSION" != "6" ]]; then exit 0; fi
+if [[ "$TRAVIS_NODE_VERSION" != "10" ]]; then exit 0; fi
 
 echo "Pushing source strings to Transifex..."
 pip install virtualenv
