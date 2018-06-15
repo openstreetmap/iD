eaa9d8d246173b63c3293183743d53d24a9ee08f
john gravois <jgravois@esri.com>
2018-06-15 08:21:53 -0700

    fix recomposed tilemap url

--- modules/renderer/background_source.js
+++ modules/renderer/background_source.js
@@ -310,7 +310,7 @@ rendererBackgroundSource.Esri = function(data) {
         var y = (Math.floor((1 - Math.log(Math.tan(center[1] * Math.PI / 180) + 1 / Math.cos(center[1] * Math.PI / 180)) / Math.PI) / 2 * Math.pow(2, z)));
 
         // fetch an 8x8 grid because responses to leverage cache
-        var tilemapUrl = dummyUrl.replace(/tile\/[0-9]+\/[0-9]+\/[0-9]+/, 'tilemap') + '/' + z + '/' + y + ' /' + '/8/8';
+        var tilemapUrl = dummyUrl.replace(/tile\/[0-9]+\/[0-9]+\/[0-9]+/, 'tilemap') + '/' + z + '/' + y + ' /' + x + '/8/8';
 
         // make the request and introspect the response from the tilemap server
         d3_json(tilemapUrl, function (err, tilemap) {
