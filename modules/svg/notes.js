3de1fd8e6f94f01ce3ea133168a9cdf839ddeef7
Thomas Hervey <thomasahervey@gmail.com>
2018-06-19 10:45:58 -0400

    reformatted svg/notes

--- modules/svg/notes.js
+++ modules/svg/notes.js
@@ -1,30 +1,31 @@
 export function svgNotes(projection, context, dispatch) {
-  var enabled = false;
+    var enabled = false;
 
+    function drawNotes() {
 
-  function drawNotes() {
-  }
-
-  function showLayer() {
-  }
-
-
-  function hideLayer() {
-  }
-
-  drawNotes.enabled = function(_) {
-    if (!arguments.length) return enabled;
-    enabled = _;
-
-    if (enabled) {
-      showLayer();
-    } else {
-      hideLayer();
     }
 
-    dispatch.call('change');
-    return this;
-  };
+    function showLayer() {
 
-  return drawNotes;
+    }
+
+    function hideLayer() {
+
+    }
+
+    drawNotes.enabled = function(_) {
+        if (!arguments.length) return enabled;
+        enabled = _;
+
+        if (enabled) {
+            showLayer();
+        } else {
+            hideLayer();
+        }
+
+        dispatch('change');
+        return this;
+    };
+
+    return drawNotes;
 }
