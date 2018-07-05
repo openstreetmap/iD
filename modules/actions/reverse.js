416ae66f7121c6293ad3d196ac90782a0f9d9d34
Bryan Housel <bryan@mapbox.com>
2018-07-05 14:47:56 -0400

    Support more direction tags for reversal in Reverse action (closes #5121)
    This commit also refactors some of the logic to make it simpler and more efficient
    (removes lodash _transform too) and updates the comment that was out of date.

--- modules/actions/reverse.js
+++ modules/actions/reverse.js
@@ -1,67 +1,61 @@
-import _transform from 'lodash-es/transform';
-
-
 /*
-  Order the nodes of a way in reverse order and reverse any direction dependent tags
-  other than `oneway`. (We assume that correcting a backwards oneway is the primary
-  reason for reversing a way.)
+Order the nodes of a way in reverse order and reverse any direction dependent tags
+other than `oneway`. (We assume that correcting a backwards oneway is the primary
+reason for reversing a way.)
 
-  The following transforms are performed:
+In addition, numeric-valued `incline` tags are negated.
 
-    Keys:
-          *:right=* ⟺ *:left=*
-        *:forward=* ⟺ *:backward=*
-       direction=up ⟺ direction=down
-         incline=up ⟺ incline=down
-            *=right ⟺ *=left
+The JOSM implementation was used as a guide, but transformations that were of unclear benefit
+or adjusted tags that don't seem to be used in practice were omitted.
 
-    Relation members:
-       role=forward ⟺ role=backward
-         role=north ⟺ role=south
-          role=east ⟺ role=west
-
-   In addition, numeric-valued `incline` tags are negated.
-
-   The JOSM implementation was used as a guide, but transformations that were of unclear benefit
-   or adjusted tags that don't seem to be used in practice were omitted.
-
-   Also, each node on the way is examined for its own tags and the following transformations are performed
-   in order to ensure associated nodes (eg a Stop Sign) is also reversed
-
-    Node Keys:
-        *direction=forward ⟺ *direction=backward
-        *direction=left ⟺ *direction=right
-        *:forward=* ⟺ *:backward=*
-        *:left=* ⟺ *:right=*
-
-   References:
-      http://wiki.openstreetmap.org/wiki/Forward_%26_backward,_left_%26_right
-      http://wiki.openstreetmap.org/wiki/Key:direction#Steps
-      http://wiki.openstreetmap.org/wiki/Key:incline
-      http://wiki.openstreetmap.org/wiki/Route#Members
-      http://josm.openstreetmap.de/browser/josm/trunk/src/org/openstreetmap/josm/corrector/ReverseWayTagCorrector.java
-      http://wiki.openstreetmap.org/wiki/Tag:highway%3Dstop
-      http://wiki.openstreetmap.org/wiki/Key:traffic_sign#On_a_way_or_area
- */
-export function actionReverse(wayId, options) {
-    var replacements = [
-            [/:right$/, ':left'], [/:left$/, ':right'],
-            [/:forward$/, ':backward'], [/:backward$/, ':forward']
-        ],
-        numeric = /^([+\-]?)(?=[\d.])/,
-        roleReversals = {
-            forward: 'backward',
-            backward: 'forward',
-            north: 'south',
-            south: 'north',
-            east: 'west',
-            west: 'east'
-        };
+References:
+    http://wiki.openstreetmap.org/wiki/Forward_%26_backward,_left_%26_right
+    http://wiki.openstreetmap.org/wiki/Key:direction#Steps
+    http://wiki.openstreetmap.org/wiki/Key:incline
+    http://wiki.openstreetmap.org/wiki/Route#Members
+    http://josm.openstreetmap.de/browser/josm/trunk/src/org/openstreetmap/josm/corrector/ReverseWayTagCorrector.java
+    http://wiki.openstreetmap.org/wiki/Tag:highway%3Dstop
+    http://wiki.openstreetmap.org/wiki/Key:traffic_sign#On_a_way_or_area
+*/
+export function actionReverse(wayID, options) {
+    var ignoreKey = /^.*(_|:)?(description|name|note|website|ref|source|comment|watch|attribution)(_|:)?/;
+    var numeric = /^([+\-]?)(?=[\d.])/;
+    var keyReplacements = [
+        [/:right$/, ':left'],
+        [/:left$/, ':right'],
+        [/:forward$/, ':backward'],
+        [/:backward$/, ':forward']
+    ];
+    var valueReplacements = {
+        left: 'right',
+        right: 'left',
+        up: 'down',
+        down: 'up',
+        forward: 'backward',
+        backward: 'forward',
+        forwards: 'backward',
+        backwards: 'forward',
+    };
+    var roleReplacements = {
+        forward: 'backward',
+        backward: 'forward',
+        forwards: 'backward',
+        backwards: 'forward',
+        north: 'south',
+        south: 'north',
+        east: 'west',
+        west: 'east'
+    };
+    var onewayReplacements = {
+        yes: '-1',
+        '1': '-1',
+        '-1': 'yes'
+    };
 
 
     function reverseKey(key) {
-        for (var i = 0; i < replacements.length; ++i) {
-            var replacement = replacements[i];
+        for (var i = 0; i < keyReplacements.length; ++i) {
+            var replacement = keyReplacements[i];
             if (replacement[0].test(key)) {
                 return key.replace(replacement[0], replacement[1]);
             }
@@ -71,61 +65,47 @@ export function actionReverse(wayId, options) {
 
 
     function reverseValue(key, value) {
+        if (ignoreKey.test(key)) return value;
+
         if (key === 'incline' && numeric.test(value)) {
             return value.replace(numeric, function(_, sign) { return sign === '-' ? '' : '-'; });
-        } else if (key === 'incline' || key === 'direction') {
-            return {up: 'down', down: 'up'}[value] || value;
         } else if (options && options.reverseOneway && key === 'oneway') {
-            return {yes: '-1', '1': '-1', '-1': 'yes'}[value] || value;
+            return onewayReplacements[value] || value;
         } else {
-            return {left: 'right', right: 'left'}[value] || value;
+            return valueReplacements[value] || value;
         }
     }
 
 
-    function reverseDirectionTags(node) {
-        // Update the direction based tags as appropriate then return an updated node
-        return node.update({tags: _transform(node.tags, function(acc, tagValue, tagKey) {
-            // See if this is a direction tag and reverse (or use existing value if not recognised)
-            var re = /direction$/;
-            if (re.test(tagKey)) {
-                acc[tagKey] = {forward: 'backward', backward: 'forward', left: 'right', right: 'left'}[tagValue] || tagValue;
-            } else {
-                // Use the reverseKey method to cater for situations such as traffic_sign:forward=stop
-                // This will pass through other tags unchanged
-                acc[reverseKey(tagKey)] = tagValue;
+    // Reverse the direction of tags attached to the nodes - #3076
+    function reverseNodeTags(graph, nodeIDs) {
+        for (var i = 0; i < nodeIDs.length; i++) {
+            var node = graph.hasEntity(nodeIDs[i]);
+            if (!node || !Object.keys(node.tags).length) continue;
+
+            var tags = {};
+            for (var key in node.tags) {
+                tags[reverseKey(key)] = reverseValue(key, node.tags[key]);
             }
-            return acc;
-        }, {})});
-    }
-
-
-    function reverseTagsOnNodes(graph, nodeIds) {
-        // Reverse the direction of appropriate tags attached to the nodes (#3076)
-        return nodeIds
-            // Get each node from the graph
-            .map(function(nodeId) { return graph.entity(nodeId);})
-            // Check tags on the node, if there aren't any, we can skip
-            .filter(function(existingNode) { return existingNode.tags !== undefined;})
-            // Get a new version of each node with the appropriate tags reversed
-            .map(function(existingNode) { return reverseDirectionTags(existingNode);})
-            // Chain together consecutive updates to the graph for each updated node and return
-            .reduce(function (accGraph, value) { return accGraph.replace(value); }, graph);
+            graph = graph.replace(node.update({tags: tags}));
+        }
+        return graph;
     }
 
 
     return function(graph) {
-        var way = graph.entity(wayId),
-            nodes = way.nodes.slice().reverse(),
-            tags = {}, key, role;
+        var way = graph.entity(wayID);
+        var nodes = way.nodes.slice().reverse();
+        var tags = {};
+        var role;
 
-        for (key in way.tags) {
+        for (var key in way.tags) {
             tags[reverseKey(key)] = reverseValue(key, way.tags[key]);
         }
 
         graph.parentRelations(way).forEach(function(relation) {
             relation.members.forEach(function(member, index) {
-                if (member.id === way.id && (role = roleReversals[member.role])) {
+                if (member.id === way.id && (role = roleReplacements[member.role])) {
                     relation = relation.updateMember({role: role}, index);
                     graph = graph.replace(relation);
                 }
@@ -134,6 +114,7 @@ export function actionReverse(wayId, options) {
 
         // Reverse any associated directions on nodes on the way and then replace
         // the way itself with the reversed node ids and updated way tags
-        return reverseTagsOnNodes(graph, nodes).replace(way.update({nodes: nodes, tags: tags}));
+        return reverseNodeTags(graph, nodes)
+            .replace(way.update({nodes: nodes, tags: tags}));
     };
 }
