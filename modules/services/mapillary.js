836517a9fe336385689f3a70ad7bb13f971cef66
Matias Volpe <matias.volpe@gmail.com>
2018-07-12 09:11:18 -0300

    Add ability to resize to PhotoViewer

--- modules/services/mapillary.js
+++ modules/services/mapillary.js
@@ -431,6 +431,13 @@ export default {
         // load mapillary signs sprite
         var defs = context.container().select('defs');
         defs.call(svgDefs(context).addSprites, ['mapillary-sprite']);
+
+        // Register viewer resize handler
+        context.ui().on('photoviewerResize', function() {
+            if (_mlyViewer) {
+                _mlyViewer.resize();
+            }
+        });
     },
 
 
