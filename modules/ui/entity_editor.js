822917466cbcce752e09bd40446765d92b02f37f
Bryan Housel <bryan@mapbox.com>
2018-11-26 21:55:13 -0500

    Display name suggestion presets on 2 lines (closes #5514)

--- modules/ui/entity_editor.js
+++ modules/ui/entity_editor.js
@@ -95,7 +95,9 @@ export function uiEntityEditor(context) {
             .attr('class', 'preset-list-button preset-reset')
             .call(tooltip().title(t('inspector.back_tooltip')).placement('bottom'))
             .append('div')
-            .attr('class', 'label');
+            .attr('class', 'label')
+            .append('div')
+            .attr('class', 'label-inner');
 
         enter
             .append('div')
@@ -142,8 +144,20 @@ export function uiEntityEditor(context) {
                 .preset(_activePreset)
             );
 
-        body.select('.preset-list-item .label')
-            .text(_activePreset.name());
+
+        var label = body.select('.label-inner');
+        var nameparts = label.selectAll('.namepart')
+            .data(_activePreset.name().split(' - '), function(d) { return d; });
+
+        nameparts.exit()
+            .remove();
+
+        nameparts
+            .enter()
+            .append('div')
+            .attr('class', 'namepart')
+            .text(function(d) { return d; });
+
 
         body.select('.preset-editor')
             .call(presetEditor
