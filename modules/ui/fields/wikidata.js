afcc474d3c12e842cc735477917c56aa923a003c
Quincy Morgan <quincy.morgan@icloud.com>
2018-12-01 17:39:16 -0800

    Wikidata entity labels and descriptions are now fetched and displayed in the field, if present

--- modules/ui/fields/wikidata.js
+++ modules/ui/fields/wikidata.js
@@ -5,6 +5,8 @@ import {
     event as d3_event
 } from 'd3-selection';
 
+import { services } from '../../services/index';
+
 import { svgIcon } from '../../svg/index';
 import {
     utilGetSetValue,
@@ -16,6 +18,7 @@ import { t } from '../../util/locale';
 
 
 export function uiFieldWikidata(field) {
+    var wikidata = services.wikidata;
     var dispatch = d3_dispatch('change'),
         link = d3_select(null),
         title = d3_select(null),
@@ -42,29 +45,32 @@ export function uiFieldWikidata(field) {
             .attr('class', 'labeled-inputs')
             .merge(list);
 
+        var wikidataProperties = ['identifier', 'label', 'description'];
 
         var items = list.selectAll('li')
-            .data(field.keys);
+            .data(wikidataProperties);
 
         // Enter
         var enter = items.enter()
             .append('li')
-            .attr('class', function(d) { return 'preset-access-' + d; });
+            .attr('class', function(d) { return 'preset-wikidata-' + d; });
 
         enter
             .append('span')
-            .attr('class', 'label preset-label-access')
+            .attr('class', 'label')
             .attr('for', function(d) { return 'preset-input-wikidata-' + d; })
-            .text(t('wikidata.identifier'));
+            .text(function(d) { return t('wikidata.'+d); });
 
         var inputWrap = enter
             .append('div')
-            .attr('class', 'preset-input-wikidata-wrap');
+            .attr('class', 'input-wrap');
 
-        title = inputWrap.append('input')
+        inputWrap.append('input')
             .attr('type', 'text')
             .attr('class', 'preset-input-wikidata')
-            .attr('id', function(d) { return 'preset-input-wikidata-' + d; })
+            .attr('id', function(d) { return 'preset-input-wikidata-' + d; });
+
+        title = wrap.select('.preset-wikidata-identifier input')
             .call(utilNoAuto)
             .merge(title);
 
@@ -72,9 +78,14 @@ export function uiFieldWikidata(field) {
             .on('blur', blur)
             .on('change', change);
 
-        link = enter
-            .append('button')
+        var idItem = wrap.select('.preset-wikidata-identifier');
+
+        idItem.select('button')
+            .remove();
+
+        link = idItem.append('button')
             .attr('class', 'form-field-button wiki-link')
+            .attr('title', t('icons.open_wikidata'))
             .attr('tabindex', -1)
             .call(svgIcon('#iD-icon-out-link'))
             .merge(link);
@@ -83,6 +94,29 @@ export function uiFieldWikidata(field) {
             .on('click', function() {
                 d3_event.preventDefault();
                 if (wikiURL) window.open(wikiURL, '_blank');
+            });
+
+        var readOnlyItems = wrap.selectAll('li:not(.preset-wikidata-identifier)');
+
+        readOnlyItems.select('input')
+            .classed('disabled', 'true')
+            .attr('readonly', 'true');
+
+        readOnlyItems.select('button')
+            .remove();
+
+        readOnlyItems.append('button')
+            .attr('class', 'form-field-button wiki-link')
+            .attr('title', t('icons.copy'))
+            .attr('tabindex', -1)
+            .call(svgIcon('#iD-operation-copy'))
+            .on('click', function() {
+                d3_event.preventDefault();
+                d3_select(this.parentNode)
+                    .select('input')
+                    .node()
+                    .select();
+                document.execCommand("copy");
             });
     }
 
@@ -109,8 +143,36 @@ export function uiFieldWikidata(field) {
         // value in correct format
         if (matches) {
             wikiURL = 'https://wikidata.org/wiki/' + value;
+            wikidata.entityByQID(value, function(qid, entity) {
+                var label = '', description = '';
+
+                if (entity.labels && Object.keys(entity.labels).length > 0) {
+                    var lang = Object.keys(entity.labels)[0];
+                    label = entity.labels[lang].value;
+                }
+                if (entity.descriptions && Object.keys(entity.descriptions).length > 0) {
+                    var lang = Object.keys(entity.descriptions)[0];
+                    description = entity.descriptions[lang].value;
+                }
+
+                d3_select('.preset-wikidata-label')
+                    .style('display', function(){
+                        return label.length > 0 ? 'flex' : 'none';
+                    })
+                    .select('input')
+                    .attr('value', label);
+
+                d3_select('.preset-wikidata-description')
+                    .style('display', function(){
+                        return description.length > 0 ? 'flex' : 'none';
+                    })
+                    .select('input')
+                    .attr('value', description);
+            });
         // unrecognized value format
         } else {
+            d3_select('.preset-wikidata-label').style('display', 'none');
+            d3_select('.preset-wikidata-description').style('display', 'none');
             if (value && value !== '') {
                 wikiURL = 'https://wikidata.org/wiki/Special:Search?search=' + value;
             } else {
