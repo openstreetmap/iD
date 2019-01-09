8cb41fa0033e3f7d05fb8754e28f9ee22a360c7a
Clarisse Abalos <samantha.abalos@digitalglobe.com>
2019-01-09 18:16:16 -0500

    remove additional deprecated references

--- data/mapcss_rules/index.js
+++ data/mapcss_rules/index.js
@@ -1,10 +1,8 @@
-import * as deprecated from './deprecated.json';
 import * as unnecessary from './unnecessary.json';
 import * as power from './power.json';
 
 export var mapcss = {
 	rules : {
-		deprecated: deprecated,
 		unnecessary: unnecessary,
 		power: power
 	}
