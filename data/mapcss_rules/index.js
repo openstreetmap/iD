18243b8a7bed9d1ab88338723ff9968007f68a45
Clarisse Abalos <samantha.abalos@digitalglobe.com>
2019-02-01 13:19:51 -0500

    add missing track type check
    ref #5835

--- data/mapcss_rules/index.js
+++ data/mapcss_rules/index.js
@@ -1,9 +1,11 @@
 import * as unnecessary from './unnecessary.json';
+import * as road from './road.json';
 import * as power from './power.json';
 
 export var mapcss = {
 	rules : {
 		unnecessary: unnecessary,
+		road: road,
 		power: power
 	}
 };
