34792d7fbf818b9eefcce8c92e432328ea1200a9
Quincy Morgan <quincy.morgan@icloud.com>
2019-04-03 10:57:42 -0700

    Don't add field defaults when upgrading tags to a specific replacement

--- modules/actions/change_preset.js
+++ modules/actions/change_preset.js
@@ -1,11 +1,11 @@
-export function actionChangePreset(entityID, oldPreset, newPreset) {
+export function actionChangePreset(entityID, oldPreset, newPreset, skipFieldDefaults) {
     return function action(graph) {
         var entity = graph.entity(entityID);
         var geometry = entity.geometry(graph);
         var tags = entity.tags;
 
         if (oldPreset) tags = oldPreset.unsetTags(tags, geometry);
-        if (newPreset) tags = newPreset.setTags(tags, geometry);
+        if (newPreset) tags = newPreset.setTags(tags, geometry, skipFieldDefaults);
 
         return graph.replace(entity.update({tags: tags}));
     };
