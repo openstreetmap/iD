6e6ed7fad56df271fb951fe02cdb156c123562a8
J Guthrie <jamie.guthrie@gmail.com>
2019-04-05 19:01:12 +0100

    eslint fixes

--- modules/validations/incompatible_source.js
+++ modules/validations/incompatible_source.js
@@ -6,7 +6,7 @@ import { validationIssue, validationIssueFix } from '../core/validator';
 export function validationIncompatibleSource() {
     var type = 'incompatible_source';
 
-    var invalidSources = [{id:"google", regex:"google"}];
+    var invalidSources = [{id:'google', regex:'google'}];
 
     var validation = function(entity, context) {
         var issues = [];
@@ -14,7 +14,7 @@ export function validationIncompatibleSource() {
         if (entity.tags && entity.tags.source) {
             
             invalidSources.forEach(function(invalidSource) {
-                var pattern = new RegExp(invalidSource.regex, "i");
+                var pattern = new RegExp(invalidSource.regex, 'i');
 
                 if (entity.tags.source.match(pattern)) {
                     issues.push(new validationIssue({
