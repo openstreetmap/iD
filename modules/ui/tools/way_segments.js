9a29587f0ae3db706bf6092d6d3acca746ff5957
Quincy Morgan <quincy.morgan@icloud.com>
2019-05-29 15:51:48 -0400

    Add tooltips and keyboard shortcut for drawing segment toggle (re: #6453)

--- modules/ui/tools/way_segments.js
+++ modules/ui/tools/way_segments.js
@@ -1,17 +1,34 @@
 import {
-    select as d3_select
+    select as d3_select,
+    selectAll as d3_selectAll,
 } from 'd3-selection';
 
-import { svgIcon } from '../../svg';
+import { svgIcon } from '../../svg/icon';
 import { t } from '../../util/locale';
+import { uiTooltipHtml } from '../tooltipHtml';
+import { tooltip } from '../../util/tooltip';
 
 export function uiToolWaySegments(context) {
+
+    var key = t('toolbar.segments.shortcut');
 
     var tool = {
         id: 'way_segments',
         contentClass: 'joined',
         label: t('toolbar.segments.title')
     };
+
+    function storedSegmentType() {
+        return context.storage('line-segments') || 'straight';
+    }
+
+    function setStoredSegmentType(type) {
+        context.storage('line-segments', type);
+
+        d3_selectAll('.way-segments .bar-button.active')
+            .classed('active', false);
+        d3_selectAll('.way-segments .bar-button.' + type).classed('active', true);
+    }
 
     tool.render = function(selection) {
 
@@ -31,30 +48,37 @@ export function uiToolWaySegments(context) {
         buttons
             .append('button')
             .attr('class', function(d) {
-                var segmentType = context.storage('line-segments') || 'straight';
-                return 'bar-button ' + (segmentType === d.id ? 'active' : '');
+                return 'bar-button ' + d.id + ' ' + (storedSegmentType() === d.id ? 'active' : '');
             })
             .attr('tabindex', -1)
             .on('click', function(d) {
                 if (d3_select(this).classed('active')) return;
 
-                context.storage('line-segments', d.id);
-
-                selection.selectAll('.bar-button')
-                    .classed('active', false);
-                d3_select(this).classed('active', true);
+                setStoredSegmentType(d.id);
             })
             .each(function(d) {
-                d3_select(this).call(svgIcon('#iD-segment-' + d.id, 'icon-30'));
+                var tooltipBehavior = tooltip()
+                    .placement('bottom')
+                    .html(true)
+                    .title(uiTooltipHtml(t('toolbar.segments.' + d.id + '.title'), key));
+
+                d3_select(this)
+                    .call(tooltipBehavior)
+                    .call(svgIcon('#iD-segment-' + d.id, 'icon-30'));
             });
+
+        context.keybinding()
+            .on(key, toggleMode, true);
     };
 
-    tool.update = function() {
-
-    };
+    function toggleMode() {
+        var type = storedSegmentType() === 'orthogonal' ? 'straight' : 'orthogonal';
+        setStoredSegmentType(type);
+    }
 
     tool.uninstall = function() {
-
+        context.keybinding()
+            .off(key, true);
     };
 
     return tool;
