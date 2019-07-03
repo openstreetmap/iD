eef84ea30cfee5fcc59fce8ac117cfa27c0773bf
Richez Antoine <richez.antoine.richez@gmail.com>
2019-07-03 16:11:24 +0200

    Issue #3 changed OSM backend to OHM backend resolved

--- modules/services/osm.js
+++ modules/services/osm.js
@@ -14,7 +14,7 @@ import { utilArrayChunk, utilArrayGroupBy, utilArrayUniq, utilRebind, utilTiler,
 
 var tiler = utilTiler();
 var dispatch = d3_dispatch('authLoading', 'authDone', 'change', 'loading', 'loaded', 'loadedNotes');
-var urlroot = 'https://www.openstreetmap.org';
+var urlroot = 'http://www.openhistoricalmap.org';
 var oauth = osmAuth({
     url: urlroot,
     oauth_consumer_key: '5A043yRSEugj4DJ5TljuapfnrflWDte8jTOcWLlT',
