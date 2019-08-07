d5c9312f0a724d8e769f671995337f5ea1e69cef
Quincy Morgan <quincy.morgan@icloud.com>
2019-08-07 16:37:17 -0500

    Enable undoing zoom to center (close #6611)

--- modules/ui/tools/center_zoom.js
+++ modules/ui/tools/center_zoom.js
@@ -4,23 +4,47 @@ import { t } from '../../util/locale';
 
 export function uiToolCenterZoom(context) {
 
+    var originTransform;
+
     var tool = uiToolSimpleButton({
         id: 'center_zoom',
-        label: t('toolbar.center_zoom.title'),
-        iconName: 'iD-icon-frame-pin',
+        label: function() {
+            if (!originTransform) {
+                return t('toolbar.center_zoom.title');
+            } else {
+                return t('toolbar.return');
+            }
+        },
+        iconName: function() {
+            if (!originTransform) {
+                return 'iD-icon-frame-pin';
+            } else {
+                return 'iD-icon-frame-back';
+            }
+        },
         onClick: function() {
-            context.mode().zoomToSelected();
+            if (!originTransform) {
+                context.mode().zoomToSelected();
+                originTransform = context.projection.transform();
+            } else {
+                context.map().transformEase(originTransform);
+                originTransform = null;
+            }
         },
         tooltipText: function() {
-            var mode = context.mode();
-            if (mode.id === 'select') {
-                return t('inspector.zoom_to.tooltip_feature');
-            } else if (mode.id === 'select-note') {
-                return t('inspector.zoom_to.tooltip_note');
-            } else if (mode.id === 'select-data') {
-                return t('inspector.zoom_to.tooltip_data');
-            } else if (mode.id === 'select-error') {
-                return t('inspector.zoom_to.tooltip_issue');
+            if (!originTransform) {
+                var mode = context.mode();
+                if (mode.id === 'select') {
+                    return t('inspector.zoom_to.tooltip_feature');
+                } else if (mode.id === 'select-note') {
+                    return t('inspector.zoom_to.tooltip_note');
+                } else if (mode.id === 'select-data') {
+                    return t('inspector.zoom_to.tooltip_data');
+                } else if (mode.id === 'select-error') {
+                    return t('inspector.zoom_to.tooltip_issue');
+                }
+            } else {
+                return t('toolbar.center_zoom.return_tooltip');
             }
         },
         tooltipKey: t('inspector.zoom_to.key'),
@@ -31,6 +55,17 @@ export function uiToolCenterZoom(context) {
         var modeID = context.mode().id;
         return (modeID === 'select' && !context.mode().newFeature()) || modeID === 'select-note' ||
             modeID === 'select-data' || modeID === 'select-error';
+    };
+
+    tool.install = function() {
+        context.on('enter.uiToolCenterZoom', function() {
+            originTransform = null;
+        });
+    };
+
+    tool.uninstall = function() {
+        context.on('enter.uiToolCenterZoom', null);
+        originTransform = null;
     };
 
     return tool;
