b1eaa2c29dbf53e6dfa662fad5d6540d0eaa51c3
Quincy Morgan <quincy.morgan@icloud.com>
2019-09-19 09:06:42 -0400

    Allow values in addTags to blacklist tags from implying a way should be an area (close #6800)

--- modules/presets/index.js
+++ modules/presets/index.js
@@ -134,15 +134,14 @@ export function presetIndex(context) {
 
         // blacklist
         presets.forEach(function(d) {
-            for (var key in d.tags) break;
-            if (!key) return;
-            if (ignore.indexOf(key) !== -1) return;
-
-            var value = d.tags[key];
-            if (key in areaKeys &&                      // probably an area...
-                d.geometry.indexOf('line') !== -1 &&    // but sometimes a line
-                value !== '*') {
-                areaKeys[key][value] = true;
+            for (var key in d.addTags) {
+                // examine all addTags to get a better sense of what can be tagged on lines - #6800
+                var value = d.addTags[key];
+                if (key in areaKeys &&                      // probably an area...
+                    d.geometry.indexOf('line') !== -1 &&    // but sometimes a line
+                    value !== '*') {
+                    areaKeys[key][value] = true;
+                }
             }
         });
 
