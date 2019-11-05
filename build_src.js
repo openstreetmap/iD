263c3ad08118ee234422c2c7a3c44c6027f03ed8
Bryan Housel <bryan@mapbox.com>
2019-11-05 15:40:17 -0500

    Modernize build scripts, working towards multiple rollup targets

--- build_src.js
+++ build_src.js
@@ -8,65 +8,65 @@ const rollup = require('rollup');
 const shell = require('shelljs');
 //const visualizer = require('rollup-plugin-visualizer');
 
+let _isBuilding = false;
+
 
 module.exports = function buildSrc() {
-    var isBuilding = false;
+  return function () {
+    if (_isBuilding) return;
 
-    return function () {
-        if (isBuilding) return;
+    // Start clean
+    shell.rm('-f', [
+      //'docs/statistics.html',
+      'dist/iD.js',
+      'dist/iD.js.map'
+    ]);
 
-        // Start clean
-        shell.rm('-f', [
-            //'docs/statistics.html',
-            'dist/iD.js',
-            'dist/iD.js.map'
-        ]);
+    console.log('building src');
+    console.time(colors.green('src built'));
 
-        console.log('building src');
-        console.time(colors.green('src built'));
+    _isBuilding = true;
 
-        isBuilding = true;
-
-        return rollup
-            .rollup({
-                input: './modules/id.js',
-                plugins: [
-                    includePaths( {
-                        paths: ['node_modules/d3/node_modules'],  // npm2 or windows
-                        include: {
-                            'martinez-polygon-clipping': 'node_modules/martinez-polygon-clipping/dist/martinez.umd.js'
-                        }
-                    }),
-                    nodeResolve({
-                        mainFields: ['module', 'main'],
-                        browser: false,
-                        dedupe: ['object-inspect']
-                    }),
-                    commonjs(),
-                    json({ indent: '' }),
-                    // viz causes src build to take about 3x longer; skip
-                    // visualizer({
-                    //     filename: 'docs/statistics.html',
-                    //     sourcemap: true
-                    // })
-                ]
-            })
-            .then(function (bundle) {
-                return bundle.write({
-                    format: 'iife',
-                    file: 'dist/iD.js',
-                    sourcemap: true,
-                    strict: false
-                });
-            })
-            .then(function () {
-                isBuilding = false;
-                console.timeEnd(colors.green('src built'));
-            })
-            .catch(function (err) {
-                isBuilding = false;
-                console.error(err);
-                process.exit(1);
-            });
-    };
+    return rollup
+      .rollup({
+        input: './modules/id.js',
+        plugins: [
+          includePaths({
+            paths: ['node_modules/d3/node_modules'],  // npm2 or windows
+            include: {
+              'martinez-polygon-clipping': 'node_modules/martinez-polygon-clipping/dist/martinez.umd.js'
+            }
+          }),
+          nodeResolve({
+            mainFields: ['module', 'main'],
+            browser: false,
+            dedupe: ['object-inspect']
+          }),
+          commonjs(),
+          json({ indent: '' }),
+          // viz causes src build to take about 3x longer; skip
+          // visualizer({
+          //   filename: 'docs/statistics.html',
+          //   sourcemap: true
+          // })
+        ]
+      })
+      .then((bundle) => {
+        return bundle.write({
+          format: 'iife',
+          file: 'dist/iD.js',
+          sourcemap: true,
+          strict: false
+        });
+      })
+      .then(() => {
+        _isBuilding = false;
+        console.timeEnd(colors.green('src built'));
+      })
+      .catch((err) => {
+        _isBuilding = false;
+        console.error(err);
+        process.exit(1);
+      });
+  };
 };
