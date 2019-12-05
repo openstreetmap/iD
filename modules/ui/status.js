c1f77211974dd3a03b218160755fe2b3d65a077c
Quincy Morgan <quincy.morgan@icloud.com>
2019-12-05 17:17:57 -0500

    Reload API status automatically when a response indicates a probable change in status (close #6650) Make API status messages more informative (close #7021) Add manual Retry button to "unable to connect" API status message (close #5864)

--- modules/ui/status.js
+++ modules/ui/status.js
@@ -1,3 +1,4 @@
+import _throttle from 'lodash-es/throttle';
 import { event as d3_event } from 'd3-selection';
 
 import { t } from '../util/locale';
@@ -11,48 +12,68 @@ export function uiStatus(context) {
     return function(selection) {
         if (!osm) return;
 
-        function update() {
-            osm.status(function(err, apiStatus) {
-                selection.html('');
+        function update(err, apiStatus) {
+            selection.html('');
 
-                if (err) {
-                    if (apiStatus === 'connectionSwitched') {
-                        // if the connection was just switched, we can't rely on
-                        // the status (we're getting the status of the previous api)
-                        return;
+            if (err) {
+                if (apiStatus === 'connectionSwitched') {
+                    // if the connection was just switched, we can't rely on
+                    // the status (we're getting the status of the previous api)
+                    return;
 
-                    } else if (apiStatus === 'rateLimited') {
-                        selection
-                            .text(t('status.rateLimit'))
-                            .append('a')
-                            .attr('class', 'api-status-login')
-                            .attr('target', '_blank')
-                            .call(svgIcon('#iD-icon-out-link', 'inline'))
-                            .append('span')
-                            .text(t('login'))
-                            .on('click.login', function() {
-                                d3_event.preventDefault();
-                                osm.authenticate();
-                            });
-                    } else {
-                        // eslint-disable-next-line no-warning-comments
-                        // TODO: nice messages for different error types
-                        selection.text(t('status.error'));
-                    }
+                } else if (apiStatus === 'rateLimited') {
+                    selection
+                        .text(t('osm_api_status.message.rateLimit'))
+                        .append('a')
+                        .attr('class', 'api-status-login')
+                        .attr('target', '_blank')
+                        .call(svgIcon('#iD-icon-out-link', 'inline'))
+                        .append('span')
+                        .text(t('login'))
+                        .on('click.login', function() {
+                            d3_event.preventDefault();
+                            osm.authenticate();
+                        });
+                } else {
 
-                } else if (apiStatus === 'readonly') {
-                    selection.text(t('status.readonly'));
-                } else if (apiStatus === 'offline') {
-                    selection.text(t('status.offline'));
+                    // don't allow retrying too rapidly
+                    var throttledRetry = _throttle(function() {
+                        // try loading the visible tiles
+                        context.loadTiles(context.projection);
+                        // manually reload the status too in case all visible tiles were already loaded
+                        osm.reloadApiStatus();
+                    }, 2000);
+
+                    // eslint-disable-next-line no-warning-comments
+                    // TODO: nice messages for different error types
+                    selection
+                        .text(t('osm_api_status.message.error') + ' ')
+                        .append('a')
+                        // let the user manually retry their connection directly
+                        .text(t('osm_api_status.retry'))
+                        .on('click.retry', function() {
+                            d3_event.preventDefault();
+                            throttledRetry();
+                        });
                 }
 
-                selection.attr('class', 'api-status ' + (err ? 'error' : apiStatus));
-            });
+            } else if (apiStatus === 'readonly') {
+                selection.text(t('osm_api_status.message.readonly'));
+            } else if (apiStatus === 'offline') {
+                selection.text(t('osm_api_status.message.offline'));
+            }
+
+            selection.attr('class', 'api-status ' + (err ? 'error' : apiStatus));
         }
 
-        osm.on('change', function() { update(selection); });
+        osm.on('apiStatusChange.uiStatus', update);
 
-        window.setInterval(update, 90000);
-        update(selection);
+        // reload the status periodically regardless of other factors
+        window.setInterval(function() {
+            osm.reloadApiStatus();
+        }, 90000);
+
+        // load the initial status in case no OSM data was loaded yet
+        osm.reloadApiStatus();
     };
 }
