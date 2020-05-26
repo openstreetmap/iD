ccbe0021bd23d69eeb0ff4845ac3a590b65cde57
Kyle Hensel <k.y.l.e@outlook.co.nz>
2020-05-26 21:16:57 +1200

    localise the last modified message

--- modules/ui/view_on_osm.js
+++ modules/ui/view_on_osm.js
@@ -1,17 +1,35 @@
+import { t } from '../core/localizer';
+import { utilDetect } from '../util/detect';
 import { osmEntity, osmNote } from '../osm';
 import { svgIcon } from '../svg/icon';
 
-/** @param {Date} date @returns {string} */
+const preferredLanguage = utilDetect().browserLocales[0];
+
+/** @param {Date} date @returns {[number, string]} */
 function timeSince(date) {
     const seconds = Math.floor((new Date() - date) / 1000);
     const s = n => Math.floor(seconds / n);
 
-    if (s(60*60*24*365) > 1) return s(60*60*24*365) + ' years';
-    if (s(60*60*24*30) > 1) return s(60*60*24*30) + ' months';
-    if (s(60*60*24) > 1) return s(60*60*24) + ' days';
-    if (s(60*60) > 1) return s(60*60) + ' hours';
-    if (s(60) > 1) return s(60) + ' minutes';
-    return s(1) + ' seconds';
+    if (s(60*60*24*365) > 1) return [s(60*60*24*365), 'years'];
+    if (s(60*60*24*30) > 1) return [s(60*60*24*30), 'months'];
+    if (s(60*60*24) > 1) return [s(60*60*24), 'days'];
+    if (s(60*60) > 1) return [s(60*60), 'hours'];
+    if (s(60) > 1) return [s(60), 'minutes'];
+    return [s(1), 'seconds'];
+}
+
+/**
+ * Show the relative time if `Intl.RelativeTimeFormat` is supported
+ * Oterwise fallback to the current date
+ * @param {Date} date
+ * @returns {string}
+ */
+function getRelativeDate(date) {
+    if (typeof Intl === 'undefined' || typeof Intl.RelativeTimeFormat === 'undefined') {
+        return `on ${date.toLocaleDateString(preferredLanguage)}`;
+    }
+    const [number, units] = timeSince(date);
+    return new Intl.RelativeTimeFormat(preferredLanguage).format(-number, units);
 }
 
 export function uiViewOnOSM(context) {
@@ -42,9 +60,12 @@ export function uiViewOnOSM(context) {
             .attr('href', url)
             .call(svgIcon('#iD-icon-out-link', 'inline'));
 
+
+        const timeago = getRelativeDate(new Date(_what.timestamp));
+
         linkEnter
             .append('span')
-            .text(`Last modified ${timeSince(new Date(_what.timestamp))} ago by ${_what.user}`);
+            .text(t('inspector.last_modified', { timeago, user: _what.user }));
     }
 
 
