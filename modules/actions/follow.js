9aa5e344c9c847f0c8510dfc18a42796c9ff4c46
Pierre-Léo Bourbonnais <leo.bourbonnais@polymtl.ca>
2020-06-02 14:14:51 -0400

    follow

---
+++ modules/actions/follow.js
@@ -0,0 +1,157 @@
+export function actionFollow(selectedIDs, projection) {
+
+  // tgt: target
+  // src: source
+  // cnt: count
+  // idx: index
+
+  var getStartNodeId = function(startNodeId, tgtNodes, srcNodes, graph) {
+      if (startNodeId) {
+          return startNodeId;
+      } else {
+          for (var tgtI = 0, tgtNodesCnt = tgtNodes.length; tgtI < tgtNodesCnt; tgtI++) {
+              var tgtNodeIdxInSrc = srcNodes.indexOf(tgtNodes[tgtI]);
+              if (tgtNodeIdxInSrc >= 0)
+              {
+                  return tgtNodes[tgtI];
+              }
+          }
+      }
+      return null;
+  }
+
+  var getEndNodeId = function(startNodeId, endNodeId, tgtNodes, srcNodes, graph) {
+      if (endNodeId) {
+          return endNodeId;
+      } else {
+          for (var tgtI = 0, tgtNodesCnt = tgtNodes.length; tgtI < tgtNodesCnt; tgtI++) {
+              var tgtNodeIdxInSrc = srcNodes.indexOf(tgtNodes[tgtI]);
+              if (tgtNodeIdxInSrc >= 0 && tgtNodes[tgtI] !== startNodeId)
+              {
+                  return tgtNodes[tgtI];
+              }
+          }
+      }
+      return null;
+
+  }
+
+  var action = function(graph, t) {
+
+      var tgtWay         = graph.entity(selectedIDs[0]);
+      var tgtWayIsClosed = tgtWay.isClosed();
+      var tgtNodes       = tgtWay.nodes.slice();
+      var tgtNodesCnt    = tgtNodes.length;
+      var srcWay         = graph.entity(selectedIDs[1]);
+      var srcWayIsClosed = srcWay.isClosed();
+      var srcNodes       = srcWay.nodes.slice();
+      var srcNodesCnt    = srcNodes.length;
+
+      var startNodeId = getStartNodeId(selectedIDs[2], tgtNodes, srcNodes);
+      var endNodeId   = getEndNodeId(startNodeId, selectedIDs[3], tgtNodes, srcNodes);
+
+      var startNodeIdxInSrc = srcNodes.indexOf(startNodeId);
+      var endNodeIdxInSrc   = srcNodes.indexOf(endNodeId);
+
+      var startNodeIdxInTgt = tgtNodes.indexOf(startNodeId);
+      var endNodeIdxInTgt   = tgtNodes.indexOf(endNodeId);
+
+      if (!srcWayIsClosed && startNodeIdxInSrc > endNodeIdxInSrc) { // reverse direction for src if backward
+          var tmpNodeIdx        = startNodeIdxInSrc;
+              startNodeIdxInSrc = endNodeIdxInSrc;
+              endNodeIdxInSrc   = tmpNodeIdx;
+      }
+
+      if (!tgtWayIsClosed && !srcWayIsClosed && startNodeIdxInTgt > endNodeIdxInTgt) { // reverse direction for src if backward
+          var tmpNodeIdx        = startNodeIdxInTgt;
+              startNodeIdxInTgt = endNodeIdxInTgt;
+              endNodeIdxInTgt   = tmpNodeIdx;
+      }
+
+      // check if target is closed and if start and end target nodes are the last segment of the loop :
+      var tgtIsLastSegmentOfClosedWay = tgtWayIsClosed && ((startNodeIdxInTgt === 0 && endNodeIdxInTgt === tgtNodesCnt - 2) || (endNodeIdxInTgt === 0 && startNodeIdxInTgt === tgtNodesCnt - 2));
+      var sameDirection               = srcNodes[startNodeIdxInSrc] === tgtNodes[startNodeIdxInTgt];
+
+      var newTgtNodes = tgtNodes;
+
+      var insertIdx  = endNodeIdxInTgt;
+      var srcNodeIdx = srcWayIsClosed && startNodeIdxInSrc === srcNodesCnt - 2 ? 0 : startNodeIdxInSrc + 1;
+
+      var srcIndexIncrement = sameDirection ? 1 : 0;
+      var insertIdx         = endNodeIdxInTgt;
+
+      if (srcWayIsClosed)
+      {
+          var tgtDirectionAsc   = endNodeIdxInTgt > startNodeIdxInTgt;
+              srcIndexIncrement = tgtDirectionAsc ? 1 : 0;
+              insertIdx         = tgtDirectionAsc ? endNodeIdxInTgt : startNodeIdxInTgt;
+
+          if (tgtIsLastSegmentOfClosedWay) {
+              insertIdx = tgtNodesCnt - 1;
+              srcIndexIncrement = tgtDirectionAsc ? 0 : 1;
+          }
+      }
+      else
+      {
+          if (tgtIsLastSegmentOfClosedWay) {
+              insertIdx = tgtNodesCnt - 1;
+              srcIndexIncrement = 0;
+          }
+      }
+
+      while (srcNodeIdx !== endNodeIdxInSrc)
+      {
+          newTgtNodes.splice(insertIdx, 0, srcNodes[srcNodeIdx]);
+          insertIdx = insertIdx + srcIndexIncrement;
+           // jump to the first node if source is closed:
+          srcNodeIdx = srcWayIsClosed && srcNodeIdx + 1 === srcNodesCnt - 1 ? 0 : srcNodeIdx + 1; 
+      }
+     
+      tgtWay = tgtWay.update({nodes: newTgtNodes});
+      graph  = graph.replace(tgtWay);
+
+      return graph;
+  };
+
+  action.disabled = function(graph) {
+
+      var tgtWay         = graph.entity(selectedIDs[0]);
+      var tgtWayIsClosed = tgtWay.isClosed();
+      var tgtNodes       = tgtWay.nodes.slice();
+      var tgtNodesCnt    = tgtNodes.length;
+      var srcWay         = graph.entity(selectedIDs[1]);
+      var srcNodes       = srcWay.nodes.slice();
+
+      var startNodeId = getStartNodeId(selectedIDs[2], tgtNodes, srcNodes);
+      var endNodeId   = getEndNodeId(startNodeId, selectedIDs[3], tgtNodes, srcNodes);
+
+      var startNodeIdxInTgt = tgtNodes.indexOf(startNodeId);
+      var endNodeIdxInTgt   = tgtNodes.indexOf(endNodeId);
+      var startNodeIdxInSrc = srcNodes.indexOf(startNodeId);
+      var endNodeIdxInSrc   = srcNodes.indexOf(endNodeId);
+
+      var tgtNodesCnt = tgtNodes.length;
+
+      var tgtIsLastSegmentOfClosedWay = tgtWayIsClosed && ((startNodeIdxInTgt === 0 && endNodeIdxInTgt === tgtNodesCnt - 2) || (endNodeIdxInTgt === 0 && startNodeIdxInTgt === tgtNodesCnt - 2));
+
+      // make sure the nodes are shared by source and target ways, and that are consecutive in target way
+      if (startNodeIdxInTgt === -1 || endNodeIdxInTgt === -1 || startNodeIdxInSrc === -1 || endNodeIdxInSrc === -1) {
+          return 'nodes_are_not_shared_by_both_ways';
+      }
+
+      if (tgtIsLastSegmentOfClosedWay) {
+        return false;
+      }
+
+      if (Math.abs(startNodeIdxInTgt - endNodeIdxInTgt) !== 1 && (endNodeIdxInTgt !== tgtNodesCnt - 1 || startNodeIdxInTgt !== 0)) {
+          return 'nodes_are_not_consecutive_in_target';
+      }
+      return false;
+
+  };
+
+  action.transitionable = true;
+
+
+  return action;
+}
---
+++ modules/operations/follow.js
@@ -0,0 +1,95 @@
+import { t } from '../core/localizer';
+import { actionFollow } from '../actions/follow';
+import { behaviorOperation } from '../behavior/operation';
+import { utilGetAllNodes } from '../util';
+
+
+export function operationFollow(context, selectedIDs) {
+
+    var action = actionFollow(selectedIDs, context.projection);
+    var nodes = utilGetAllNodes(selectedIDs, context.graph());
+    var coords = nodes.map(function(n) { return n.loc; });
+
+    var operation = function() {
+        context.perform(action, operation.annotation());
+
+        window.setTimeout(function() {
+            context.validator().validate();
+        }, 300);  // after any transition
+    };
+
+
+    operation.available = function() {
+
+        if (selectedIDs.length < 2 || selectedIDs.length > 4) {
+            return false;
+        }
+
+        var entities = selectedIDs.map(function(selectedID) {
+            return context.entity(selectedID);
+        });
+
+        if (
+            entities[0].type === 'way' && entities[1].type === 'way' && 
+            (
+                (entities[2] && entities[2].type === 'node' && entities[3] && entities[3].type === 'node') 
+             || (entities[2] && entities[2].type === 'node' && !entities[3]) 
+             || (!entities[2] && !entities[3])
+            )
+        ) {
+            return true;
+        }
+        return false;
+
+    };
+
+
+    // don't cache this because the visible extent could change
+    operation.disabled = function() {
+        var actionDisabled = action.disabled(context.graph());
+        if (actionDisabled) {
+            return actionDisabled;
+        } else if (someMissing()) {
+            return 'not_downloaded';
+        } else if (selectedIDs.some(context.hasHiddenConnections)) {
+            return 'connected_to_hidden';
+        }
+
+        return false;
+
+
+        function someMissing() {
+            if (context.inIntro()) return false;
+            var osm = context.connection();
+            if (osm) {
+                var missing = coords.filter(function(loc) { return !osm.isDataLoaded(loc); });
+                if (missing.length) {
+                    missing.forEach(function(loc) { context.loadTileAtLoc(loc); });
+                    return true;
+                }
+            }
+            return false;
+        }
+    };
+
+
+    operation.tooltip = function() {
+        var disable = operation.disabled();
+        return disable ?
+            t('operations.follow.' + disable) :
+            t('operations.follow.description.points');
+    };
+
+
+    operation.annotation = function() {
+        return t('operations.follow.annotation.points');
+    };
+
+
+    operation.id = 'follow';
+    operation.keys = [t('operations.follow.key')];
+    operation.title = t('operations.follow.title');
+    operation.behavior = behaviorOperation(context).which(operation);
+
+    return operation;
+}
