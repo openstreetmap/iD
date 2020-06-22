2b4a5d9e6667f3c6ef717e246c880fee8f4df9cf
Quincy Morgan <quincy.morgan@icloud.com>
2020-06-22 15:36:34 -0400

    Fix lint error

--- modules/core/difference.js
+++ modules/core/difference.js
@@ -57,8 +57,8 @@ export function coreDifference(base, head) {
         }
     }
 
-    for (var id in head.entities) checkEntityID(id);
-    for (var id in base.entities) checkEntityID(id);
+    for (var headId in head.entities) checkEntityID(headId);
+    for (var baseId in base.entities) checkEntityID(baseId);
 
 
     _diff.length = function length() {
