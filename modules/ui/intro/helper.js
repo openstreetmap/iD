f5d280682e4590953bca11ac758cbfc44984ab0d
Quincy Morgan <2046746+quincylvania@users.noreply.github.com>
2020-07-13 10:57:59 -0400

    Add mousewheel icon to help and walkthrough

--- modules/ui/intro/helper.js
+++ modules/ui/intro/helper.js
@@ -75,6 +75,7 @@ export function helpString(id, replacements) {
         save_icon: icon('#iD-icon-save', 'pre-text'),
         leftclick: icon('#iD-walkthrough-mouse-left', 'pre-text operation'),
         rightclick: icon('#iD-walkthrough-mouse-right', 'pre-text operation'),
+        mousewheel_icon: icon('#iD-walkthrough-mousewheel', 'pre-text operation'),
         tap_icon: icon('#iD-walkthrough-tap', 'pre-text operation'),
         doubletap_icon: icon('#iD-walkthrough-doubletap', 'pre-text operation'),
         longpress_icon: icon('#iD-walkthrough-longpress', 'pre-text operation'),
