4059ee51187c3371833bfe0fd40ec6a46e4c2e94
Quincy Morgan <2046746+quincylvania@users.noreply.github.com>
2020-10-23 13:38:36 -0400

    Enable block-scoped-var eslint rule

--- modules/util/util.js
+++ modules/util/util.js
@@ -418,8 +418,9 @@ export function utilEditDistance(a, b) {
     if (a.length === 0) return b.length;
     if (b.length === 0) return a.length;
     var matrix = [];
-    for (var i = 0; i <= b.length; i++) { matrix[i] = [i]; }
-    for (var j = 0; j <= a.length; j++) { matrix[0][j] = j; }
+    var i, j;
+    for (i = 0; i <= b.length; i++) { matrix[i] = [i]; }
+    for (j = 0; j <= a.length; j++) { matrix[0][j] = j; }
     for (i = 1; i <= b.length; i++) {
         for (j = 1; j <= a.length; j++) {
             if (b.charAt(i-1) === a.charAt(j-1)) {
