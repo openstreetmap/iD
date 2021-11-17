0c4447045bed2550ff0cbd311cd03007d1f5c6d3
Martin Raifer <martin@raifer.tech>
2021-11-17 17:53:46 +0100

    try to choose better splitting locations for "simple" cases

--- modules/validations/osm_api_limits.js
+++ modules/validations/osm_api_limits.js
@@ -49,8 +49,36 @@ export function validationOsmApiLimits(context) {
                 var entityId = this.entityIds[0];
                 var entity = context.graph().entities[entityId];
                 var numberOfParts = Math.ceil(entity.nodes.length / maxWayNodes);
-                var splitVertices = [...Array(numberOfParts - 1)].map((_, i) =>
-                    entity.nodes[Math.floor(entity.nodes.length * (i + 1) / numberOfParts)]);
+                var splitVertices;
+
+                if (numberOfParts === 2) {
+                    // simple case: try to split at the an intersection vertex
+                    var splitIntersection = entity.nodes
+                        .map(nid => g.entity(nid))
+                        .filter(n => g.parentWays(n).length > 1)
+                        .map(n => n.id)
+                        .find(nid => {
+                            var splitIndex = entity.nodes.indexOf(nid);
+                            return splitIndex < maxWayNodes &&
+                                entity.nodes.length - splitIndex < maxWayNodes;
+                        });
+                    if (splitIntersection !== undefined) {
+                        splitVertices = [splitIntersection];
+                    }
+                }
+
+                if (splitVertices === undefined) {
+                    // generial case: either more than one split is needed or no possible
+                    // intersection split point was found -> just split at regular intervals
+                    splitVertices = [...Array(numberOfParts - 1)].map((_, i) =>
+                        entity.nodes[Math.floor(entity.nodes.length * (i + 1) / numberOfParts)]);
+                }
+
+                if (entity.isClosed()) {
+                    // add extra split for closed ways at start of way
+                    splitVertices.push(entity.nodes[0]);
+                }
+
                 var operation = operationSplit(context, splitVertices.concat(entityId));
                 if (!operation.disabled()) {
                     operation();
