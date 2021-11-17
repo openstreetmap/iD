35c88d8d2bcd26181cf38f48de1ed748faee8d86
Martin Raifer <martin@raifer.tech>
2021-11-17 17:51:44 +0100

    load maximum way nodes during validation instead of when initializing
    that's because the actual (non placeholder) value for the maxWayNode setting will be only populated after the OSM API's capabilities call is finished

--- modules/validations/osm_api_limits.js
+++ modules/validations/osm_api_limits.js
@@ -4,15 +4,18 @@ import { operationSplit } from '../operations/split';
 
 export function validationOsmApiLimits(context) {
     var type = 'osm_api_limits';
-    var maxWayNodes = context.connection().maxWayNodes();
 
-    var validation = function checkOsmapiLimits(entity, graph) {
+    var validation = function checkOsmApiLimits(entity) {
         var issues = [];
+        var osm = context.connection();
+        if (!osm) return issues; // cannot check if there is no connection to the osm api, e.g. during unit tests
+        var maxWayNodes = osm.maxWayNodes();
 
         if (entity.type === 'way') {
             if (entity.nodes.length > maxWayNodes) {
                 issues.push(new validationIssue({
                     type: type,
+                    subtype: 'exceededMaxWayNodes',
                     severity: 'error',
                     message: function() {
                         return t.html('issues.osm_api_limits.max_way_nodes.message');
@@ -34,12 +37,15 @@ export function validationOsmApiLimits(context) {
         return issues;
     };
 
-    function splitWayIntoSmallChunks(context) {
+    function splitWayIntoSmallChunks() {
         var fix = new validationIssueFix({
             icon: 'iD-operation-split',
             title: t.html('issues.fix.split_way.title'),
             entityIds: this.entityIds,
             onClick: function(context) {
+                var maxWayNodes = context.connection().maxWayNodes();
+                var g = context.graph();
+
                 var entityId = this.entityIds[0];
                 var entity = context.graph().entities[entityId];
                 var numberOfParts = Math.ceil(entity.nodes.length / maxWayNodes);
