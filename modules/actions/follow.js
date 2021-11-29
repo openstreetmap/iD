581f6013c90e2d641aa22aa93ca356306b1c3fe6
Pierre-Léo Bourbonnais <leo.bourbonnais@polymtl.a>
2021-11-28 20:10:55 -0500

    follow v0.3

--- modules/actions/follow.js
+++ modules/actions/follow.js
@@ -48,44 +48,47 @@ export function actionFollow(selectedIDs, projection, reverse = false, customGra
         window.graph = graph;
         window.entities = graph.entities;
 
-        var tgtWay = graph.entity(selectedIDs[0]);
-        var tgtWayIsClosed = tgtWay.isClosed();
-        var tgtNodes = tgtWay.nodes.slice();
-        var tgtNodesCnt = tgtNodes.length;
-        var srcWay = graph.entity(selectedIDs[1]);
-        var srcWayIsClosed = srcWay.isClosed();
-        var srcNodes = srcWay.nodes.slice();
-        var srcNodesCnt = srcNodes.length;
+        let tgtWay = graph.entity(selectedIDs[0]);
+        let tgtWayIsClosed = tgtWay.isClosed();
+        let tgtNodes = tgtWay.nodes.slice();
+        let tgtNodesCnt = tgtNodes.length;
+        let srcWay = graph.entity(selectedIDs[1]);
+        let srcWayIsClosed = srcWay.isClosed();
+        let srcNodes = srcWay.nodes.slice();
+        let srcNodesCnt = srcNodes.length;
 
 
-        var startNodeId = getStartNodeId(selectedIDs[2], tgtNodes, srcNodes);
-        var endNodeId = getEndNodeId(startNodeId, selectedIDs[3], tgtNodes, srcNodes);
+        let startNodeId = getStartNodeId(selectedIDs[2], tgtNodes, srcNodes);
+        let endNodeId = getEndNodeId(startNodeId, selectedIDs[3], tgtNodes, srcNodes);
 
-        var startNodeIdxInSrc = srcNodes.indexOf(startNodeId);
-        var endNodeIdxInSrc = srcNodes.indexOf(endNodeId);
-        var startNodeIdxInTgt = tgtNodes.indexOf(startNodeId);
-        var endNodeIdxInTgt = tgtNodes.indexOf(endNodeId);
+        let startNodeIdxInSrc = srcNodes.indexOf(startNodeId);
+        let endNodeIdxInSrc = srcNodes.indexOf(endNodeId);
+        let startNodeIdxInTgt = tgtNodes.indexOf(startNodeId);
+        let endNodeIdxInTgt = tgtNodes.indexOf(endNodeId);
 
+        /*if (startNodeIdxInSrc > endNodeIdxInSrc) { // make sure the start index in source is before the end so we can use the next node index in the loop
+            [startNodeIdxInSrc, endNodeIdxInSrc] = [endNodeIdxInSrc, startNodeIdxInSrc];
+        }*/
+
+        // if target way is closed, create two unclosed lines from both side of the start and end nodes:
+        let tgtSideToUpdate = 1;
+        let tgtSide1Nodes = [];
+        let tgtSide2Nodes = [];
+        if (tgtWayIsClosed) {
+            tgtSide1Nodes = tgtWay.getNodesBetween(startNodeIdxInTgt, endNodeIdxInTgt);
+            tgtSide2Nodes = tgtWay.getNodesBetween(endNodeIdxInTgt, startNodeIdxInTgt);
+            //console.log('tgtNodes closed', JSON.parse(JSON.stringify(tgtNodes)));
+            tgtNodes = reverse ? tgtSide2Nodes : tgtSide1Nodes;
+            tgtSideToUpdate = reverse ? 2 : 1;
+            console.log(tgtSide1Nodes, tgtSide2Nodes);
+        }
+
+        startNodeIdxInTgt = tgtNodes.indexOf(startNodeId);
+        endNodeIdxInTgt = tgtNodes.indexOf(endNodeId);
         if (startNodeIdxInTgt > endNodeIdxInTgt) { // make sure the start index in target is before the end so we can use the next node index in the loop
             [startNodeIdxInTgt, endNodeIdxInTgt] = [endNodeIdxInTgt, startNodeIdxInTgt];
         }
 
-        if (startNodeIdxInSrc > endNodeIdxInSrc) { // make sure the start index in source is before the end so we can use the next node index in the loop
-            [startNodeIdxInSrc, endNodeIdxInSrc] = [endNodeIdxInSrc, startNodeIdxInSrc];
-        }
-
-        //console.log('srcIsClosed', srcWay.isClosed());
-        //console.log('tgtIsClosed', tgtWay.isClosed());
-        //console.log('startNodeIdxInSrc', startNodeIdxInSrc);
-        //console.log('endNodeIdxInSrc', endNodeIdxInSrc);
-        //console.log('startNodeIdxInTgt', startNodeIdxInTgt);
-        //console.log('endNodeIdxInTgt', endNodeIdxInTgt);
-        //console.log('srcNodesBetweenF', srcWay.getNodesBetween(startNodeIdxInSrc, endNodeIdxInSrc));
-        //console.log('srcNodesBetweenB', srcWay.getNodesBetween(endNodeIdxInSrc, startNodeIdxInSrc));
-        //const tgtNodesBetweenForward = tgtWay.getNodesBetween(startNodeIdxInTgt, endNodeIdxInTgt);
-        //const tgtNodesBetweenBackward = tgtWay.getNodesBetween(endNodeIdxInTgt, endNodeIdxInTgt);
-        //console.log('tgtNodesBetweenF', tgtNodesBetweenForward);
-        //console.log('tgtNodesBetweenB', tgtNodesBetweenBackward);
         let srcNodesUsed = srcWay.getNodesBetween(startNodeIdxInSrc, endNodeIdxInSrc);
         if (srcNodesUsed.length === 0) {
             console.error('no suitable nodes in source');
@@ -93,12 +96,12 @@ export function actionFollow(selectedIDs, projection, reverse = false, customGra
         }
         const srcNodesUsedReversed = [...srcNodesUsed].reverse(); // need to clone because reverse modifies the original array
 
-        console.log('srcNodesUsed and reversed', srcNodesUsed, srcNodesUsedReversed);
+        //console.log('srcNodesUsed and reversed', srcNodesUsed, srcNodesUsedReversed);
 
         const updatedTgtWayNodes = [];
-        if (!tgtWayIsClosed && !srcWayIsClosed) {
+        //if (!srcWayIsClosed) {
             let nodeIdx = 0;
-            while(nodeIdx < startNodeIdxInTgt) {
+            while (nodeIdx < startNodeIdxInTgt) {
                 updatedTgtWayNodes.push(tgtNodes[nodeIdx]);
                 nodeIdx++;
             }
@@ -108,29 +111,52 @@ export function actionFollow(selectedIDs, projection, reverse = false, customGra
                 updatedTgtWayNodes.push(...srcNodesUsedReversed);
             }
             nodeIdx = endNodeIdxInTgt + 1;
-            while(nodeIdx < tgtNodes.length) {
+            while (nodeIdx < tgtNodes.length) {
                 updatedTgtWayNodes.push(tgtNodes[nodeIdx]);
                 nodeIdx++;
             }
             // update target way:
-            tgtWay = tgtWay.update({nodes: updatedTgtWayNodes});
-            graph  = graph.replace(tgtWay);
+            if (!tgtWayIsClosed) {
+                tgtWay = tgtWay.update({
+                    nodes: updatedTgtWayNodes
+                });
+            } else {
+                let tgtSideToUpdate = 1;
+                if (tgtWayIsClosed) {
+                    if (tgtSideToUpdate === 1) {
+                        tgtSide2Nodes.shift(); // remove first node from tgt side 2, so it is not repeated. Only the last node will repeat and will be the looping node
+                        const firstNodes = [...updatedTgtWayNodes];
+                        const closedTgtWayNodes = firstNodes.concat(...tgtSide2Nodes);
+                        tgtWay = tgtWay.update({
+                            nodes: closedTgtWayNodes
+                        });
+                    } else {
+                        tgtSide1Nodes.shift(); // remove first node from tgt side 2, so it is not repeated. Only the last node will repeat and will be the looping node
+                        const firstNodes = [...updatedTgtWayNodes];
+                        const closedTgtWayNodes = firstNodes.concat(...tgtSide1Nodes);
+                        tgtWay = tgtWay.update({
+                            nodes: closedTgtWayNodes
+                        });
+                    }
+                }
+            }
+            graph = graph.replace(tgtWay);
             // remove unconnected tagless nodes in between:
             nodeIdx = startNodeIdxInTgt + 1;
-            while(nodeIdx < endNodeIdxInTgt) {
+            while (nodeIdx < endNodeIdxInTgt) {
                 const node = graph.entity(tgtNodes[nodeIdx]);
                 //console.log('checking node: ', node.id, graph.isShared(node), node.hasNonGeometryTags(), graph.parentWays(node).length);
                 if (!node.hasNonGeometryTags() && !graph.isShared(node) && graph.parentWays(node).length === 0) {
-                    console.log('removing node: ', node.id);
+                    //console.log('removing node: ', node.id);
                     const deleteAction = iD.actionDeleteNode(node.id);
                     graph = deleteAction(graph);
                 }
                 nodeIdx++;
             }
-        }
+        //}
 
         return graph;
-        
+
     };
 
     action.disabled = function (graph) {
