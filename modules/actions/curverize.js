dbc20637569816ebe7456ac6ca639a7b2ae94c73
Pierre-Léo Bourbonnais <leo.bourbonnais@polymtl.a>
2021-12-01 12:52:11 -0500

    curverize 0.9

--- modules/actions/curverize.js
+++ modules/actions/curverize.js
@@ -6,13 +6,13 @@ export function actionCurverize(selectedIds, projection) {
 
     var action = function (graph) {
 
-        console.log('graph before', graph.entities);
+        //console.log('graph before', graph.entities);
 
         const entities = selectedIds.map(function (selectedID) {
             return graph.entity(selectedID);
         });
 
-        console.log('entities', entities);
+        //console.log('entities', entities);
 
         // get the way and its last node to curverize:
         let way = null;
@@ -30,7 +30,7 @@ export function actionCurverize(selectedIds, projection) {
         }
         const lastNodeIdx = way.nodes.indexOf(lastNode.id);
 
-        console.log('lastNodeIdx', lastNodeIdx);
+        //console.log('lastNodeIdx', lastNodeIdx);
 
         // get four last nodes of way (the nodes to use for the curve):
         let lastFourNodesIds = [];
@@ -48,7 +48,7 @@ export function actionCurverize(selectedIds, projection) {
         }
         const lastFourPoints = lastFourNodes.map(function(n) { return projection(n.loc); });
 
-        console.log('lastFourNodesIds', lastFourNodesIds);
+        //console.log('lastFourNodesIds', lastFourNodesIds);
 
 
 
@@ -56,7 +56,7 @@ export function actionCurverize(selectedIds, projection) {
         const angle1 = geoVecAngle(lastFourPoints[0], lastFourPoints[1]);
         const angle2 = geoVecAngle(lastFourPoints[2], lastFourPoints[3]);
         const distanceBetweenTangents = geoVecLength(lastFourPoints[1], lastFourPoints[2]);
-        console.log('angle1', angle1, 'angle2', angle2, 'distanceBetweenTangents', distanceBetweenTangents);
+        //console.log('angle1', angle1, 'angle2', angle2, 'distanceBetweenTangents', distanceBetweenTangents);
         //const tangent1Length = geoVecLength(lastFourPoints[0], lastFourPoints[1]);
         //const tangent2Length = geoVecLength(lastFourPoints[2], lastFourPoints[3]);
         //console.log('tangent1Length', tangent1Length, 'tangent2Length', tangent2Length);
@@ -92,12 +92,6 @@ export function actionCurverize(selectedIds, projection) {
 
         const tangent1MinifiedLine = [tangentsIntersection, geoVecSubtract(tangentsIntersection, geoVecScale(tangent1UnitVector, minLengthToIntersection))];
         const tangent2MinifiedLine = [tangentsIntersection, geoVecSubtract(tangentsIntersection, geoVecScale(tangent2UnitVector, minLengthToIntersection))];
-        console.log('tangent1Line', tangent1Line);
-        console.log('tangent1Length', tangent1LengthToIntersection);
-        console.log('tangent1Vector', tangent1Vector);
-        console.log('tangent1UnitVector', tangent1UnitVector);
-        console.log('tangent1MinifiedLine', tangent1MinifiedLine);
-        console.log('tangent2MinifiedLine', tangent2MinifiedLine);
 
         const tangent1PerpendicularMinifiedLine = geoRotate(tangent1MinifiedLine, -Math.PI/2, tangent1MinifiedLine[1]);
         const tangent2PerpendicularMinifiedLine = geoRotate(tangent2MinifiedLine, Math.PI/2, tangent2MinifiedLine[1]);
@@ -108,7 +102,7 @@ export function actionCurverize(selectedIds, projection) {
             console.log('could not find a circle center to draw arc')
             return graph;
         }
-        /*const nodeTangentIntersection = osmNode({ loc: projection.invert(tangentsIntersection) });
+        /*const nodeTangentIntersection = osmNode({ loc: projection.invert(tangentsIntersection), tags: { entrance: 'shop' } });
         graph = graph.replace(nodeTangentIntersection);
 
         const nodeCircleCenter = osmNode({ loc: projection.invert(circleCenter) });
@@ -116,46 +110,64 @@ export function actionCurverize(selectedIds, projection) {
 
         const radiusLineStart = [circleCenter, tangent1MinifiedLine[1]];
         const radiusLineEnd = [circleCenter, tangent2MinifiedLine[1]];
-
+        const radius = geoVecLength(radiusLineStart[0], radiusLineStart[1]);
         let angleRadiusLineStart = geoVecAngle(radiusLineStart[0], radiusLineStart[1]);
         let angleRadiusLineEnd = geoVecAngle(radiusLineEnd[0], radiusLineEnd[1]);
-        console.log('angleRadiusLineStart', angleRadiusLineStart*180/Math.PI, 'angleRadiusLineEnd', angleRadiusLineEnd*180/Math.PI);
-
-        let arcAngleRad = geoVecAngleBetween(angleRadiusLineStart, angleRadiusLineEnd);
+        //console.log('radius', radius);
+        let arcAngleRad = Math.abs(geoVecAngleBetween(angleRadiusLineStart, angleRadiusLineEnd));
+        //console.log('arcLength', arcAngleRad * radius);
+        
         const arcAngleDeg = arcAngleRad * 180.0 / Math.PI;
-        console.log('arcAngle', arcAngleRad, arcAngleDeg);
+        //console.log('arcAngle', arcAngleRad, arcAngleDeg);
 
-        const numberOfSegments = Math.max(2, Math.ceil(arcAngleDeg / 5.0));
+        let numberOfSegments = Math.max(2, Math.ceil(arcAngleDeg / 10.0));
+        //console.log('arcLength', arcAngleRad * radius);
+        const arcSegmentLength = arcAngleRad * radius / numberOfSegments;
+        if (arcSegmentLength > 20) {
+            numberOfSegments = Math.max(2, arcAngleRad * radius / 20);
+        }
 
         const radiusNodes = [];
 
+        const directionCheckAngle = angleRadiusLineStart - geoVecAngle(tangent1MinifiedLine[0], tangent1MinifiedLine[1]);
+        let reverse = false;
+        // verify that we do go in the right direction. If angle is not -90 degrees, we need to switch direction for arc creation
+        if (Math.round(1000.0 * directionCheckAngle) !== -1571.0) {
+            reverse = true;
+        }
+        //console.log(reverse, 'angleBetweenTangeantAndRadiusLine', Math.round(1000.0 * directionCheckAngle), (180/Math.PI) * directionCheckAngle);
+        //if (angleRadiusLineStart - geoVecAngle(tangent1MinifiedLine[0], tangent1MinifiedLine[1]))
+        //const secondRadiusSegment = geoRotate(radiusLineStart, - 1 * arcAngleRad / numberOfSegments, circleCenter);
+        //const secondArcPoint = secondRadiusSegment[1];
+        //const angleWithTangeant = 
+
         for (let i = 0; i < numberOfSegments; i++) {
-            console.log('angle ' + i, (180.0 / Math.PI) * i * arcAngleRad / numberOfSegments);
-            const radiusSegment = geoRotate(radiusLineStart, - i * arcAngleRad / numberOfSegments, circleCenter);
+            //console.log('angle ' + i, (180.0 / Math.PI) * i * arcAngleRad / numberOfSegments);
+            const radiusSegment = geoRotate(radiusLineStart, (reverse ? i : -i) * arcAngleRad / numberOfSegments, circleCenter);
             const arcPoint = radiusSegment[1];
             radiusNodes.push(osmNode({ loc: projection.invert(arcPoint) }));
         }
-        console.log('radiusNodes', radiusNodes);
+        //console.log('radiusNodes', radiusNodes);
         for (let i = 0; i < radiusNodes.length; i++) {
             graph = graph.replace(radiusNodes[i]);
         }
 
-        console.log('graph', graph);
+        //console.log('graph', graph);
 
         const radiusNodesIds = radiusNodes.map(function(node) { return node.id });
         const wayNodes = [...(way.nodes)];
-        console.log('radiusNodesIds', radiusNodesIds);
+        //console.log('radiusNodesIds', radiusNodesIds);
         if (direction === 'forward') {
             wayNodes.splice(2, 0, ...(radiusNodesIds.reverse()));
-            console.log('way forward', way);
+            //console.log('way forward', way);
         } else if (direction === 'backward') {
             wayNodes.splice(wayNodes.length - 2, 0, ...(radiusNodesIds.reverse()));
-            console.log('way backward', way);
+            //console.log('way backward', way);
         }
         way = way.update({nodes: wayNodes});
         graph = graph.replace(way);
 
-        console.log('end curverize');
+        //console.log('end curverize');
 
         return graph;
     };
