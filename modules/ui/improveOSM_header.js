bd1836fd0cf53254ed88352eb69a78da51092692
Martin Raifer <martin@raifer.tech>
2022-02-14 17:37:15 +0100

    fix maki icons, tweak rendering of icons
    * maki v7+ doesn't have provide "11px" icons anymore
    * use 12px for icons on points & vertices on map (instead of 11px)
    * use 12px for icons on QA tool (improveOSM, osmose) markers (instead of 13px)
    * drop some unused code

--- modules/ui/improveOSM_header.js
+++ modules/ui/improveOSM_header.js
@@ -46,18 +46,10 @@ export function uiImproveOsmHeader() {
     svgEnter
       .append('use')
         .attr('class', 'icon-annotation')
-        .attr('width', '13px')
-        .attr('height', '13px')
-        .attr('transform', 'translate(3.5, 5)')
-        .attr('xlink:href', d => {
-          const picon = d.icon;
-          if (!picon) {
-            return '';
-          } else {
-            const isMaki = /^maki-/.test(picon);
-            return `#${picon}${isMaki ? '-11' : ''}`;
-          }
-        });
+        .attr('width', '12px')
+        .attr('height', '12px')
+        .attr('transform', 'translate(4, 5.5)')
+        .attr('xlink:href', d => d.icon ? '#' + d.icon : '');
 
     headerEnter
       .append('div')
