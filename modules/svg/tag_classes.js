688c27a0542a7af915f8a1ca0519beb5d9541c8e
emmlabro <emmanuel.labrousse@deutschepost.de>
2022-06-13 16:06:05 +0200

    feat: add yellow background to elements with tag key postal_object

--- modules/svg/tag_classes.js
+++ modules/svg/tag_classes.js
@@ -7,7 +7,7 @@ export function svgTagClasses() {
         'building', 'highway', 'railway', 'waterway', 'aeroway', 'aerialway',
         'piste:type', 'boundary', 'power', 'amenity', 'natural', 'landuse',
         'leisure', 'military', 'place', 'man_made', 'route', 'attraction',
-        'building:part', 'indoor'
+        'building:part', 'indoor', 'postal_object'
     ];
     var statuses = [
         // nonexistent, might be built
