openstreetmap/iD

commit 8bb1d39431f2f8a076c56e6686d45f32b31506fb

refactor validation modules to not inject html

modules/core/validation/models.js
@@ -66,7 +66,7 @@ export function validationIssue(attrs) {
         if (issue.severity === 'warning') {
             // allow ignoring any issue that's not an error
             fixes.push(new validationIssueFix({
-                title: t.html('issues.fix.ignore_issue.title'),
+                title: t.append('issues.fix.ignore_issue.title'),
                 icon: 'iD-icon-close',
                 onClick: function() {
                     context.validator().ignoreIssue(this.issue.id);

modules/ui/commit_warnings.js
@@ -1,3 +1,5 @@
+import { select as d3_select } from 'd3-selection';
+
 import { t } from '../core/localizer';
 import { svgIcon } from '../svg/icon';
 import { uiTooltip } from './tooltip';
@@ -89,8 +91,9 @@ export function uiCommitWarnings(context) {
                 .merge(items);
 
             items.selectAll('.issue-message')
-                .html(function(d) {
-                    return d.message(context);
+                .text('')
+                .each(function(d) {
+                    return d.message(context)(d3_select(this));
                 });
         }
     }

modules/ui/conflicts.js
@@ -205,7 +205,6 @@ export function uiConflicts(context) {
             .data(['previous', 'next'])
             .enter()
             .append('button')
-            .html(function(d) { return t.html('save.conflict.' + d); })
             .attr('class', 'conflict-nav-button action col6')
             .attr('disabled', function(d, i) {
                 return (i === 0 && index === 0) ||
@@ -223,7 +222,8 @@ export function uiConflicts(context) {
 
                 container
                     .call(showConflict, index + sign);
-            });
+            })
+            .call(function(d) { t.append('save.conflict.' + d)(d3_select(this)); });
 
     }
 

modules/ui/sections/entity_issues.js
@@ -180,8 +180,9 @@ export function uiSectionEntityIssues(context) {
             .classed('active', function(d) { return d.id === _activeIssueID; });
 
         containers.selectAll('.issue-message')
-            .html(function(d) {
-                return d.message(context);
+            .text('')
+            .each(function(d) {
+                return d.message(context)(d3_select(this));
             });
 
         // fixes
@@ -242,7 +243,7 @@ export function uiSectionEntityIssues(context) {
         buttons
             .append('span')
             .attr('class', 'fix-message')
-            .html(function(d) { return d.title; });
+            .each(function(d) { return d.title(d3_select(this)); });
 
         fixesEnter.merge(fixes)
             .selectAll('button')

modules/ui/sections/validation_issues.js
@@ -146,8 +146,9 @@ export function uiSectionValidationIssues(id, severity, context) {
             .order();
 
         items.selectAll('.issue-message')
-            .html(function(d) {
-                return d.message(context);
+            .text('')
+            .each(function(d) {
+                return d.message(context)(d3_select(this));
             });
 
         /*

modules/validations/almost_junction.js
@@ -55,12 +55,12 @@ export function validationAlmostJunction(context) {
         message: function(context) {
           const entity1 = context.hasEntity(this.entityIds[0]);
           if (this.entityIds[0] === this.entityIds[2]) {
-            return entity1 ? t.html('issues.almost_junction.self.message', {
+            return entity1 ? t.append('issues.almost_junction.self.message', {
               feature: utilDisplayLabel(entity1, context.graph())
             }) : '';
           } else {
             const entity2 = context.hasEntity(this.entityIds[2]);
-            return (entity1 && entity2) ? t.html('issues.almost_junction.message', {
+            return (entity1 && entity2) ? t.append('issues.almost_junction.message', {
               feature: utilDisplayLabel(entity1, context.graph()),
               feature2: utilDisplayLabel(entity2, context.graph())
             }) : '';
@@ -88,7 +88,7 @@ export function validationAlmostJunction(context) {
     function makeFixes(context) {
       let fixes = [new validationIssueFix({
         icon: 'iD-icon-abutment',
-        title: t.html('issues.fix.connect_features.title'),
+        title: t.append('issues.fix.connect_features.title'),
         onClick: function(context) {
           const annotation = t('issues.fix.connect_almost_junction.annotation');
           const [, endNodeId, crossWayId] = this.issue.entityIds;

modules/validations/close_nodes.js
@@ -172,7 +172,7 @@ export function validationCloseNodes(context) {
                         message: function(context) {
                             var entity = context.hasEntity(this.entityIds[0]),
                                 entity2 = context.hasEntity(this.entityIds[1]);
-                            return (entity && entity2) ? t.html('issues.close_nodes.detached.message', {
+                            return (entity && entity2) ? t.append('issues.close_nodes.detached.message', {
                                 feature: utilDisplayLabel(entity, context.graph()),
                                 feature2: utilDisplayLabel(entity2, context.graph())
                             }) : '';
@@ -183,11 +183,11 @@ export function validationCloseNodes(context) {
                             return [
                                 new validationIssueFix({
                                     icon: 'iD-operation-disconnect',
-                                    title: t.html('issues.fix.move_points_apart.title')
+                                    title: t.append('issues.fix.move_points_apart.title')
                                 }),
                                 new validationIssueFix({
                                     icon: 'iD-icon-layers',
-                                    title: t.html('issues.fix.use_different_layers_or_levels.title')
+                                    title: t.append('issues.fix.use_different_layers_or_levels.title')
                                 })
                             ];
                         }
@@ -237,7 +237,7 @@ export function validationCloseNodes(context) {
                 severity: 'warning',
                 message: function(context) {
                     var entity = context.hasEntity(this.entityIds[0]);
-                    return entity ? t.html('issues.close_nodes.message', { way: utilDisplayLabel(entity, context.graph()) }) : '';
+                    return entity ? t.append('issues.close_nodes.message', { way: utilDisplayLabel(entity, context.graph()) }) : '';
                 },
                 reference: showReference,
                 entityIds: [way.id, node1.id, node2.id],
@@ -246,7 +246,7 @@ export function validationCloseNodes(context) {
                     return [
                         new validationIssueFix({
                             icon: 'iD-icon-plus',
-                            title: t.html('issues.fix.merge_points.title'),
+                            title: t.append('issues.fix.merge_points.title'),
                             onClick: function(context) {
                                 var entityIds = this.issue.entityIds;
                                 var action = actionMergeNodes([entityIds[1], entityIds[2]]);
@@ -255,7 +255,7 @@ export function validationCloseNodes(context) {
                         }),
                         new validationIssueFix({
                             icon: 'iD-operation-disconnect',
-                            title: t.html('issues.fix.move_points_apart.title')
+                            title: t.append('issues.fix.move_points_apart.title')
                         })
                     ];
                 }

modules/validations/crossing_ways.js
@@ -407,7 +407,7 @@ export function validationCrossingWays(context) {
                 var graph = context.graph();
                 var entity1 = graph.hasEntity(this.entityIds[0]),
                     entity2 = graph.hasEntity(this.entityIds[1]);
-                return (entity1 && entity2) ? t.html('issues.crossing_ways.message', {
+                return (entity1 && entity2) ? t.append('issues.crossing_ways.message', {
                     feature: utilDisplayLabel(entity1, graph),
                     feature2: utilDisplayLabel(entity2, graph)
                 }) : '';
@@ -440,7 +440,7 @@ export function validationCrossingWays(context) {
                 if (isCrossingIndoors) {
                     fixes.push(new validationIssueFix({
                         icon: 'iD-icon-layers',
-                        title: t.html('issues.fix.use_different_levels.title')
+                        title: t.append('issues.fix.use_different_levels.title')
                     }));
                 } else if (isCrossingTunnels ||
                     isCrossingBridges ||
@@ -469,7 +469,7 @@ export function validationCrossingWays(context) {
                 // repositioning the features is always an option
                 fixes.push(new validationIssueFix({
                     icon: 'iD-operation-move',
-                    title: t.html('issues.fix.reposition_features.title')
+                    title: t.append('issues.fix.reposition_features.title')
                 }));
 
                 return fixes;
@@ -489,7 +489,7 @@ export function validationCrossingWays(context) {
     function makeAddBridgeOrTunnelFix(fixTitleID, iconName, bridgeOrTunnel){
         return new validationIssueFix({
             icon: iconName,
-            title: t.html('issues.fix.' + fixTitleID + '.title'),
+            title: t.append('issues.fix.' + fixTitleID + '.title'),
             onClick: function(context) {
                 var mode = context.mode();
                 if (!mode || mode.id !== 'select') return;
@@ -698,7 +698,7 @@ export function validationCrossingWays(context) {
 
         return new validationIssueFix({
             icon: 'iD-icon-crossing',
-            title: t.html('issues.fix.' + fixTitleID + '.title'),
+            title: t.append('issues.fix.' + fixTitleID + '.title'),
             onClick: function(context) {
                 var loc = this.issue.loc;
                 var connectionTags = this.issue.data.connectionTags;
@@ -742,7 +742,7 @@ export function validationCrossingWays(context) {
     function makeChangeLayerFix(higherOrLower) {
         return new validationIssueFix({
             icon: 'iD-icon-' + (higherOrLower === 'higher' ? 'up' : 'down'),
-            title: t.html('issues.fix.tag_this_as_' + higherOrLower + '.title'),
+            title: t.append('issues.fix.tag_this_as_' + higherOrLower + '.title'),
             onClick: function(context) {
 
                 var mode = context.mode();

commit 9c951aa23500f8addf30b128f73b765f4d2d59d8

fix regression which caused fist validation fix to revert to "ignore"
regressed in 8bb1d39431f2f8a076c56e6686d45f32b31506fb

modules/core/validation/models.js
@@ -76,7 +76,7 @@ export function validationIssue(attrs) {
 
         fixes.forEach(function(fix) {
             // the id doesn't matter as long as it's unique to this issue/fix
-            fix.id = fix.title;
+            fix.id = fix.title.stringId;
             // add a reference to the issue for use in actions
             fix.issue = issue;
             if (fix.autoArgs) {