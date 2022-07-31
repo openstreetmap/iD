a26031fa735ff25e4db656ccfabee291198b6c3b
paulklie <73854742+paulklie@users.noreply.github.com>
2022-07-31 23:38:26 +0200

    #9225 create a railway_crossing when connecting two railways

--- modules/validations/crossing_ways.js
+++ modules/validations/crossing_ways.js
@@ -156,7 +156,9 @@ export function validationCrossingWays(context) {
                 return {};
             }
             if (featureType1 === 'waterway') return {};
-            if (featureType1 === 'railway') return {};
+            if (featureType1 === 'railway') {
+                return {railway: 'railway_crossing'};
+            }
 
         } else {
             var featureTypes = [featureType1, featureType2];
