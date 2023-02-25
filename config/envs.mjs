050c164a608fd37f6c48fe26ffd7c88ab2a0ed38
Martin Raifer <martin@raifer.tech>
2023-02-25 18:03:09 +0100

    make OSM API connection details configurable via env vars

--- config/envs.mjs
+++ config/envs.mjs
@@ -2,7 +2,11 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 const envs = {
-  ENV__ID_PRESETS_CDN_URL: JSON.stringify(process.env.ID_PRESETS_CDN_URL || null)
+  ENV__ID_PRESETS_CDN_URL: JSON.stringify(process.env.ID_PRESETS_CDN_URL || null),
+  ENV__ID_API_CONNECTION_URL: JSON.stringify(process.env.ID_API_CONNECTION_URL || null),
+  ENV__ID_API_CONNECTION_CLIENT_ID: JSON.stringify(process.env.ID_API_CONNECTION_CLIENT_ID || null),
+  ENV__ID_API_CONNECTION_CLIENT_SECRET: JSON.stringify(process.env.ID_API_CONNECTION_CLIENT_SECRET || null),
+  ENV__ID_API_CONNECTION: JSON.stringify(process.env.ID_API_CONNECTION || null)
 }
 
 export default envs;
