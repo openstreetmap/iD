574cba9ed97354c555533d6010ce12ca735b9c03
Martin Raifer <martin@raifer.tech>
2023-02-25 12:19:27 +0100

    fix variable name in previous commit

--- config/envs.mjs
+++ config/envs.mjs
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 const envs = {
-  ID_PRESETS_CDN_URL: JSON.stringify(process.env.ID_PRESETS_CDN_URL || null)
+  ENV__ID_PRESETS_CDN_URL: JSON.stringify(process.env.ID_PRESETS_CDN_URL || null)
 }
 
 export default envs;
