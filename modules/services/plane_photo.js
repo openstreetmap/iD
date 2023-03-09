e33279bb4a1df755e56b68fd6a9ad66399218842
Noenandre <5470915+noenandre@users.noreply.github.com>
2023-03-09 16:38:02 +0100

    Create a new zoom behaviour when the image size potentially change.

--- modules/services/plane_photo.js
+++ modules/services/plane_photo.js
@@ -5,7 +5,35 @@ import { utilSetTransform, utilRebind } from '../util';
 const dispatch = d3_dispatch('viewerChanged');
 
 let _photo;
+let _wrapper;
 let imgZoom;
+
+function zoomPan (d3_event) {
+  let t = d3_event.transform;
+  _photo.call(utilSetTransform, t.x, t.y, t.k);
+  }
+
+function zoomBeahvior () {
+  const {width: wrapperWidth, height: wrapperHeight} = _wrapper.node().getBoundingClientRect();
+  const {naturalHeight, naturalWidth} = _photo.node();
+  const intrinsicRatio = naturalWidth / naturalHeight;
+  const widthOverflow = wrapperHeight * intrinsicRatio - wrapperWidth;
+  return d3_zoom()
+        .extent([[widthOverflow / 2, 0], [wrapperWidth + widthOverflow / 2, wrapperHeight]])
+        .translateExtent([[0, 0], [wrapperWidth + widthOverflow, wrapperHeight]])
+        .scaleExtent([1, 15])
+        .on('zoom', zoomPan);
+}
+
+function loadImage (selection, path) {
+  return new Promise((resolve) => {
+    selection.attr('src', path);
+    selection.on('load', () => {
+      resolve(selection);
+    });
+  });
+}
+
 
 export default {
 
@@ -18,25 +46,18 @@ export default {
                 .scaleExtent([1, 15])
                 .on('zoom', this.zoomPan);
 
-    const wrapper = selection
+    _wrapper = selection
       .append('div')
       .attr('class', 'photo-frame plane-frame')
       .classed('hide', true);
 
-    _photo = wrapper
+    _photo = _wrapper
       .append('img')
       .attr('class', 'plane-photo');
 
     context.ui().photoviewer.on('resize.plane', () => {
-      const {width: wrapperWidth} = wrapper.node().getBoundingClientRect();
-      const {width, height} = _photo.node().getBoundingClientRect();
-      const widthOverflow = width - wrapperWidth;
-      imgZoom = d3_zoom()
-        .extent([[widthOverflow / 2, 0], [wrapperWidth + widthOverflow / 2, height]])
-        .translateExtent([[0, 0], [width, height]])
-        .scaleExtent([1, 15])
-        .on('zoom', this.zoomPan);
-      wrapper.call(imgZoom);
+      imgZoom = zoomBeahvior();
+      _wrapper.call(imgZoom);
     });
 
     await Promise.resolve();
@@ -70,16 +91,15 @@ export default {
     },
 
   selectPhoto: function (data, keepOrientation) {
-    _photo.attr('src', data.image_path);
-    if (!keepOrientation) {
-      _photo.call(imgZoom.transform, d3_zoomIdentity);
-    }
+    loadImage(_photo, data.image_path)
+      .then(() => {
+        if (!keepOrientation) {
+          imgZoom = zoomBeahvior();
+          _wrapper.call(imgZoom);
+          _wrapper.call(imgZoom.transform, d3_zoomIdentity);
+        }
+      });
     return this;
-  },
-
-  zoomPan: function (d3_event) {
-    let t = d3_event.transform;
-    _photo.call(utilSetTransform, t.x, t.y, t.k);
   },
 
   getYaw: function() {
