3172f8c9560dea59592030b47027f5b55580a928
Kyle Hensel <k.y.l.e@outlook.co.nz>
2023-05-13 13:35:04 +1200

    remove fixme=continue when you continue a line

--- modules/operations/continue.js
+++ modules/operations/continue.js
@@ -2,6 +2,7 @@ import { t } from '../core/localizer';
 import { modeDrawLine } from '../modes/draw_line';
 import { behaviorOperation } from '../behavior/operation';
 import { utilArrayGroupBy } from '../util';
+import { actionChangeTags } from '../actions';
 
 
 export function operationContinue(context, selectedIDs) {
@@ -28,6 +29,17 @@ export function operationContinue(context, selectedIDs) {
 
     var operation = function() {
         var candidate = _candidates[0];
+
+        // remove fixme=continue from the vertex
+        if (_vertex.tags.fixme === 'continue') {
+            context.perform((graph) => {
+                const newTags = { ..._vertex.tags };
+                delete newTags.fixme;
+
+                return actionChangeTags(_vertex.id, newTags)(graph);
+            }, operation.annotation());
+        }
+
         context.enter(
             modeDrawLine(context, candidate.id, context.graph(), 'line', candidate.affix(_vertex.id), true)
         );
