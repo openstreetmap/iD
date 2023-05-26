1a8ba8e3564789b7a0a1d7edc66bae11fd1312f7
sezerbozbiyik <sezer@visiosoft.com.tr>
2023-05-27 01:11:34 +0300

    fixed test error

--- modules/svg/layers.js
+++ modules/svg/layers.js
@@ -37,10 +37,10 @@ export function svgLayers(projection, context) {
         { id: 'mapillary-map-features',  layer: svgMapillaryMapFeatures(projection, context, dispatch) },
         { id: 'mapillary-signs',  layer: svgMapillarySigns(projection, context, dispatch) },
         { id: 'kartaview', layer: svgKartaviewImages(projection, context, dispatch) },
+        { id: 'mapilio', layer: svgMapilioImages(projection, context, dispatch) },
         { id: 'debug', layer: svgDebug(projection, context, dispatch) },
         { id: 'geolocate', layer: svgGeolocate(projection, context, dispatch) },
         { id: 'touch', layer: svgTouch(projection, context, dispatch) },
-        { id: 'mapilio', layer: svgMapilioImages(projection, context, dispatch) }
     ];
 
 
