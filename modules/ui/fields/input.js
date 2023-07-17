openstreetmap/iD

commit 7015fae91c3578f875fbb914e894bbe926156bbf

only allow to click on valid website URLs
closes #8827

modules/ui/fields/input.js
@@ -182,11 +182,17 @@ export function uiFieldText(field, context) {
 
 
     function validIdentifierValueForLink() {
-        const value = utilGetSetValue(input).trim().split(';')[0];
+        const value = utilGetSetValue(input).trim();
 
-        if (field.type === 'url' && value) return value;
+        if (field.type === 'url' && value) {
+            try {
+                return (new URL(value)).href;
+            } catch (e) {
+                return null;
+            }
+        }
         if (field.type === 'identifier' && field.pattern) {
-            return value && value.match(new RegExp(field.pattern));
+            return value && value.match(new RegExp(field.pattern))[0];
         }
         return null;
     }

commit 9bd5817ecc89ea9acb658c5b6ae04ee3c57706d2

don't crash on invalid identifiers
fixes a bug where the link button would not be correctly disabled on invalid identifier values

(regression in 7015fae91c3578f875fbb914e894bbe926156bbf)

modules/ui/fields/input.js
@@ -195,6 +195,7 @@ export function uiFieldText(field, context) {
                         window.open(url, '_blank');
                     }
                 })
+                .classed('disabled', () => !validIdentifierValueForLink())
                 .merge(outlinkButton);
         } else if (field.type === 'url') {
             input.attr('type', 'text');
@@ -375,7 +376,7 @@ export function uiFieldText(field, context) {
             }
         }
         if (field.type === 'identifier' && field.pattern) {
-            return value && value.match(new RegExp(field.pattern))[0];
+            return value && value.match(new RegExp(field.pattern))?.[0];
         }
         return null;
     }