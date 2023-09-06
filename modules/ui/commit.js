f075219203941e4c255818dc5a2fd1739220932a
Kyle Hensel <k.y.l.e@outlook.co.nz>
2023-09-06 20:50:21 +1200

    remove leading semicolon from changeset `source`

--- modules/ui/commit.js
+++ modules/ui/commit.js
@@ -124,7 +124,7 @@ export function uiCommit(context) {
                 }
             });
 
-            tags.source = context.cleanTagValue(sources.join(';'));
+            tags.source = context.cleanTagValue(sources.filter(Boolean).join(';'));
         }
 
         context.changeset = new osmChangeset({ tags: tags });
