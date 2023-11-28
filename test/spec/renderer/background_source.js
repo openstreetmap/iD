b0b06fe55287fe363894e831939bb28bd5f47c37
Excitable Snowball <excitablesnowball@gmail.com>
2023-11-27 19:12:18 -0800

    Sanitize Amazon CloudFront signature in imagery_used

--- test/spec/renderer/background_source.js
+++ test/spec/renderer/background_source.js
@@ -92,6 +92,10 @@ describe('iD.rendererBackgroundSource.Custom', function() {
             var source = iD.rendererBackgroundSource.Custom('http://example.com?token=MYTOKEN');
             expect(source.imageryUsed()).to.eql('Custom (http://example.com?token={apikey} )');
         });
+        it('sanitizes `Signature` for CloudFront', function() {
+            var source = iD.rendererBackgroundSource.Custom('https://example.com/?Key-Pair-Id=foo&Policy=bar&Signature=baz');
+            expect(source.imageryUsed()).to.eql('Custom (https://example.com/?Key-Pair-Id=foo&Policy=bar&Signature={apikey} )');
+        });
         it('sanitizes wms path `token`', function() {
             var source = iD.rendererBackgroundSource.Custom('http://example.com/wms/v1/token/MYTOKEN/1.0.0/layer');
             expect(source.imageryUsed()).to.eql('Custom (http://example.com/wms/v1/token/{apikey}/1.0.0/layer )');
