e89121b550513f86418bc57d10b2cb5c15d977ee
Martin Raifer <martin@raifer.tech>
2024-07-31 17:53:33 +0200

    fix vegbilder overlay from being overwritten by panoramax layer

--- modules/ui/sections/photo_overlays.js
+++ modules/ui/sections/photo_overlays.js
@@ -67,7 +67,7 @@ export function uiSectionPhotoOverlays(context) {
             .merge(ul);
 
         var li = ul.selectAll('.list-item-photos')
-            .data(data);
+            .data(data, d => d.id);
 
         li.exit()
             .remove();
