f9e5bdb0f6f1d8d2cc8e8d0d14ca8d492f21c8fe
krishna <krishnasharma111104@gmail.com>
2025-02-05 14:39:28 +0530

    Use-distinct-placeholder-texts-for-search-input-boxes

--- modules/ui/preset_list.js
+++ modules/ui/preset_list.js
@@ -123,7 +123,7 @@ export function uiPresetList(context) {
         var search = searchWrap
             .append('input')
             .attr('class', 'preset-search-input')
-            .attr('placeholder', t('inspector.search'))
+            .attr('placeholder', 'Search feature type')
             .attr('type', 'search')
             .call(utilNoAuto)
             .on('keydown', initialKeydown)
