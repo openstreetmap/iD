ff9fd811baa7f2b52d195bdba49879a9bdbb8243
Martin Raifer <martin@raifer.tech>
2025-02-11 19:25:44 +0100

    var -> const/let

--- modules/validations/osm_api_limits.js
+++ modules/validations/osm_api_limits.js
@@ -3,13 +3,13 @@ import { validationIssue, validationIssueFix } from '../core/validation';
 import { operationSplit } from '../operations/split';
 
 export function validationOsmApiLimits(context) {
-    var type = 'osm_api_limits';
+    const type = 'osm_api_limits';
 
-    var validation = function checkOsmApiLimits(entity) {
-        var issues = [];
-        var osm = context.connection();
+    const validation = function checkOsmApiLimits(entity) {
+        const issues = [];
+        const osm = context.connection();
         if (!osm) return issues; // cannot check if there is no connection to the osm api, e.g. during unit tests
-        var maxWayNodes = osm.maxWayNodes();
+        const maxWayNodes = osm.maxWayNodes();
 
         if (entity.type === 'way') {
             if (entity.nodes.length > maxWayNodes) {
@@ -38,37 +38,39 @@ export function validationOsmApiLimits(context) {
     };
 
     function splitWayIntoSmallChunks() {
-        var fix = new validationIssueFix({
+        const fix = new validationIssueFix({
             icon: 'iD-operation-split',
             title: t.html('issues.fix.split_way.title'),
             entityIds: this.entityIds,
             onClick: function(context) {
-                var maxWayNodes = context.connection().maxWayNodes();
-                var g = context.graph();
+                const maxWayNodes = context.connection().maxWayNodes();
+                const g = context.graph();
 
-                var entityId = this.entityIds[0];
-                var entity = context.graph().entities[entityId];
-                var numberOfParts = Math.ceil(entity.nodes.length / maxWayNodes);
-                var splitVertices;
+                const entityId = this.entityIds[0];
+                const entity = context.graph().entities[entityId];
+                const numberOfParts = Math.ceil(entity.nodes.length / maxWayNodes);
+                let splitVertices;
 
                 if (numberOfParts === 2) {
                     // simple case: try to split at the an intersection vertex
-                    var splitIntersection = entity.nodes
+                    const splitIntersections = entity.nodes
                         .map(nid => g.entity(nid))
                         .filter(n => g.parentWays(n).length > 1)
                         .map(n => n.id)
-                        .find(nid => {
-                            var splitIndex = entity.nodes.indexOf(nid);
+                        .filter(nid => {
+                            const splitIndex = entity.nodes.indexOf(nid);
                             return splitIndex < maxWayNodes &&
                                 entity.nodes.length - splitIndex < maxWayNodes;
                         });
-                    if (splitIntersection !== undefined) {
-                        splitVertices = [splitIntersection];
+                    if (splitIntersections.length > 0) {
+                        splitVertices = [
+                            splitIntersections[Math.floor(splitIntersections.length / 2)]
+                        ];
                     }
                 }
 
                 if (splitVertices === undefined) {
-                    // generial case: either more than one split is needed or no possible
+                    // general case: either more than one split is needed or no possible
                     // intersection split point was found -> just split at regular intervals
                     splitVertices = [...Array(numberOfParts - 1)].map((_, i) =>
                         entity.nodes[Math.floor(entity.nodes.length * (i + 1) / numberOfParts)]);
@@ -79,7 +81,7 @@ export function validationOsmApiLimits(context) {
                     splitVertices.push(entity.nodes[0]);
                 }
 
-                var operation = operationSplit(context, splitVertices.concat(entityId));
+                const operation = operationSplit(context, splitVertices.concat(entityId));
                 if (!operation.disabled()) {
                     operation();
                 }
