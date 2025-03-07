7b76baddba969b5f5925b31194f9d9f557b3fb0b
Youssef Elzedy <youssefelzedy90@gmail.com>
2025-03-07 04:49:54 +0200

    Change railway line marker color from light-blue to light-pink

--- modules/svg/lines.js
+++ modules/svg/lines.js
@@ -15,7 +15,7 @@ function onewayArrowColour(tags) {
     // the return value must be defined in ./defs.js
     if (tags.highway === 'construction' && tags.bridge) return 'white';
     if (tags.highway === 'pedestrian' && tags.bridge) return 'pink';
-    if (tags.railway) return 'light-blue';
+    if (tags.railway) return 'light-pink';
     if (tags.aeroway === 'runway') return 'pink';
 
     return 'black';
