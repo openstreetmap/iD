9f39b3a4c1f0ba045578bd0a59a273e70c246c67
MOHAMED ALI FAWZI <76850143+MohamedAli00949@users.noreply.github.com>
2025-03-14 01:08:27 +0200

    fix: replace eye-dropper-icon instead of add-icon at set photo service tag btn

--- modules/ui/photoviewer.js
+++ modules/ui/photoviewer.js
@@ -134,7 +134,7 @@ export function uiPhotoviewer(context) {
                 const buttonEnter = button.enter()
                     .append('button')
                     .attr('class', 'set-photo-from-viewer')
-                    .call(svgIcon('#iD-icon-plus'))
+                    .call(svgIcon('#fas-eye-dropper'))
                     .call(uiTooltip()
                         .title(() => t.append('inspector.set_photo_from_viewer.enable'))
                         .placement('right')
