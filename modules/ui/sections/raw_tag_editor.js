3daee189c1862ae45f3866945c5315a9e87cc934
Martin Raifer <martin@raifer.tech>
2025-09-01 18:54:15 +0200

    make use of nestable t.append method

--- modules/ui/sections/raw_tag_editor.js
+++ modules/ui/sections/raw_tag_editor.js
@@ -21,7 +21,7 @@ export function uiSectionRawTagEditor(id, context) {
         .classes('raw-tag-editor')
         .label(function() {
             var count = Object.keys(_tags).filter(function(d) { return d; }).length;
-            return t.append('inspector.title_count', { title: t('inspector.tags'), count: count });
+            return t.append('inspector.title_count', { title: t.append('inspector.tags'), count: count });
         })
         .expandedByDefault(false)
         .disclosureContent(renderDisclosureContent);
