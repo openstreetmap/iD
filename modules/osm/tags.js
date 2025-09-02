2e3eda419c585ddd68b8302bfa346eda35885289
Martin Raifer <martin@raifer.tech>
2025-09-02 12:27:15 +0200

    add small circles as alternate style for sidedness markers for guard rails

--- modules/osm/tags.js
+++ modules/osm/tags.js
@@ -262,7 +262,7 @@ export var osmRightSideIsInsideTags = {
     'barrier': {
         'retaining_wall': true,
         'kerb': true,
-        'guard_rail': true,
+        'guard_rail': 'guard_rail',
         'city_wall': true,
     },
     'man_made': {
