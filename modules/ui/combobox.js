9e58a65b47ebcedf3e2139e979f4535bd1f2ba30
Sem Bauke <sem@freecodecamp.org>
2026-02-19 15:15:48 +0100

    feat: apply solution one stash and update max height

--- modules/ui/combobox.js
+++ modules/ui/combobox.js
@@ -1,6 +1,5 @@
 import { dispatch as d3_dispatch } from 'd3-dispatch';
 import { select as d3_select } from 'd3-selection';
-import { svgIcon } from '../svg/icon';
 import { utilGetSetValue, utilRebind, utilTriggerEvent } from '../util';
 
 
@@ -387,43 +386,30 @@ export function uiCombobox(context, klass) {
                 .remove();
 
             // enter/update
-            var entered = options.enter()
+            const enter = options.enter()
                 .append('a')
                 .attr('class', function(d) {
-                    return 'combobox-option ' + (d.klass || '');
+                    return 'combobox-option ' + (d.klass || '') + (d.description ? ' has-description' : '');
                 })
                 .attr('title', function(d) { return d.title; });
 
-            entered.each(function(d) {
-                var sel = d3_select(this);
-                var row = sel.append('span')
-                    .attr('class', 'combobox-option-row');
-                var labelSpan = row.append('span')
-                    .attr('class', 'combobox-option-label');
-                if (d.display) {
-                    d.display(labelSpan);
-                } else {
-                    labelSpan.text(d.value);
-                }
-                if (d.description) {
-                    row.append('button')
-                        .attr('class', 'combobox-option-info')
-                        .call(svgIcon('#iD-icon-info'))
-                        .on('click', function(d3_event) {
-                            d3_event.stopPropagation();
-                            d3_event.preventDefault();
-                            var descEl = sel.select('.combobox-option-description');
-                            var isExpanded = descEl.classed('expanded');
-                            descEl.classed('expanded', !isExpanded);
-                            d3_select(this).classed('active', !isExpanded);
-                        });
-                    sel.append('span')
-                        .attr('class', 'combobox-option-description')
-                        .text(d.description);
-                }
-            });
+            enter.each(function(d) {
+                    const sel = d3_select(this);
+                    const labelSpan = sel.append('span')
+                        .attr('class', 'combobox-option-label');
+                    if (d.display) {
+                        d.display(labelSpan);
+                    } else {
+                        labelSpan.text(d.value);
+                    }
+                    if (d.description) {
+                        sel.append('span')
+                            .attr('class', 'combobox-option-description')
+                            .text(d.description);
+                    }
+                });
 
-            entered
+            enter
                 .on('mouseenter', _mouseEnterHandler)
                 .on('mouseleave', _mouseLeaveHandler)
                 .merge(options)
