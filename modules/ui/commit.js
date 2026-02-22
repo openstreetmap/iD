fa3c838d0e8328434e623ac237bbc5973c92ab14
Shivang Jaiswal <jaiswalshivang01@gmail.com>
2026-02-23 00:37:24 +0530

    fix readonlyttags

--- modules/ui/commit.js
+++ modules/ui/commit.js
@@ -553,7 +553,7 @@ export function uiCommit(context) {
         Object.keys(changed).forEach(function(k) {
             var v = changed[k];
             k = context.cleanTagKey(k);
-            if (readOnlyTags.indexOf(k) !== -1) return;
+            if (readOnlyTags.some(tag => k.match(tag))) return;
 
             if (v === undefined) {
                 delete tags[k];
