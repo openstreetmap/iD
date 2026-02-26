db657e4f2d16baa156faf4456afe76676aea2dae
Shivang Jaiswal <jaiswalshivang01@gmail.com>
2026-02-26 22:31:12 +0530

    Fix formatting of conditional return statement

--- modules/osm/tags.js
+++ modules/osm/tags.js
@@ -30,7 +30,7 @@ const uninterestingKeyRegex = /^(source(_ref)?|tiger):/;
  */
 export function osmIsInterestingTag(key) {
     if (uninterestingKeys.has(key)) return false;
-    if (uninterestingKeyRegex.test(key)) return false;
+    if (uninterestingKeyRegex.test(key))  return false;
     return true;
 }
 
