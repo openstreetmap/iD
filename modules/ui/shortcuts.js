cc087224d81ed27e0fd436f16a94f381de38ef7b
Kyle Hensel <k.y.l.e@outlook.co.nz>
2026-03-20 14:15:24 +0100

    refactor `uiCmdSequence` into a separate function
    ----
    Co-authored-by: Martin Raifer <martin@raifer.tech>

--- modules/ui/shortcuts.js
+++ modules/ui/shortcuts.js
@@ -1,16 +1,11 @@
 import { select as d3_select } from 'd3-selection';
-
 import { fileFetcher } from '../core/file_fetcher';
 import { t } from '../core/localizer';
-import { svgIcon } from '../svg/icon';
-import { uiCmd } from './cmd';
 import { uiModal } from './modal';
-import { utilArrayUniq } from '../util';
-import { utilDetect } from '../util/detect';
+import { uiCmdSequence } from './cmd_sequence';
 
 
 export function uiShortcuts(context) {
-    var detected = utilDetect();
     var _activeTab = 0;
     var _modalSelection;
     var _selection = d3_select(null);
@@ -131,118 +126,12 @@ export function uiShortcuts(context) {
         var shortcutRows = rowsEnter
             .filter(function (d) { return d.shortcuts; });
 
-        var shortcutKeys = shortcutRows
+        shortcutRows
             .append('td')
-            .attr('class', 'shortcut-keys');
-
-        var modifierKeys = shortcutKeys
-            .filter(function (d) { return d.modifiers; });
-
-        modifierKeys
-            .selectAll('kbd.modifier')
-            .data(function (d) {
-                if (detected.os === 'win' && d.text === 'shortcuts.editing.commands.redo') {
-                    return ['⌘'];
-                } else if (detected.os !== 'mac' && d.text === 'shortcuts.browsing.display_options.fullscreen') {
-                    return [];
-                } else {
-                    return d.modifiers;
-                }
-            })
-            .enter()
-            .each(function () {
-                var selection = d3_select(this);
-
-                selection
-                    .append('kbd')
-                    .attr('class', 'modifier')
-                    .text(function (d) { return uiCmd.display(d); });
-
-                selection
-                    .append('span')
-                    .text('+');
+            .attr('class', 'shortcut-keys')
+            .each(function (d) {
+                uiCmdSequence(d)(d3_select(this));
             });
-
-
-        shortcutKeys
-            .selectAll('kbd.shortcut')
-            .data(function (d) {
-                var arr = d.shortcuts;
-                if (detected.os === 'win' && d.text === 'shortcuts.editing.commands.redo') {
-                    arr = ['Y'];
-                } else if (detected.os !== 'mac' && d.text === 'shortcuts.browsing.display_options.fullscreen') {
-                    arr = ['F11'];
-                }
-
-                // replace translations
-                arr = arr.map(function(s) {
-                    return uiCmd.display(s.indexOf('.') !== -1 ? t(s) : s);
-                });
-
-                return utilArrayUniq(arr).map(function(s) {
-                    return {
-                        shortcut: s,
-                        separator: d.separator,
-                        suffix: d.suffix
-                    };
-                });
-            })
-            .enter()
-            .each(function (d, i, nodes) {
-                var selection = d3_select(this);
-                var click = d.shortcut.toLowerCase().match(/(.*).click/);
-
-                if (click && click[1]) {   // replace "left_click", "right_click" with mouse icon
-                    selection
-                        .call(svgIcon('#iD-walkthrough-mouse-' + click[1], 'operation'));
-                } else if (d.shortcut.toLowerCase() === 'long-press') {
-                    selection
-                        .call(svgIcon('#iD-walkthrough-longpress', 'longpress operation'));
-                } else if (d.shortcut.toLowerCase() === 'tap') {
-                    selection
-                        .call(svgIcon('#iD-walkthrough-tap', 'tap operation'));
-                } else {
-                    selection
-                        .append('kbd')
-                        .attr('class', 'shortcut')
-                        .text(function (d) { return d.shortcut; });
-                }
-
-                if (i < nodes.length - 1) {
-                    if (d.separator) {
-                        selection
-                            .append('span')
-                            .text(d.separator);
-                    } else {
-                        selection.append('span').text('\u00a0');
-                        selection.append('span').call(t.append('shortcuts.or'));
-                        selection.append('span').text('\u00a0');
-                    }
-                } else if (i === nodes.length - 1 && d.suffix) {
-                    selection
-                        .append('span')
-                        .text(d.suffix);
-                }
-            });
-
-
-        shortcutKeys
-            .filter(function(d) { return d.gesture; })
-            .each(function () {
-                var selection = d3_select(this);
-
-                selection
-                    .append('span')
-                    .text('+');
-
-                selection
-                    .append('span')
-                    .attr('class', 'gesture')
-                    .each(function (d) {
-                        d3_select(this).call(t.addOrUpdate(d.gesture));
-                    });
-            });
-
 
         shortcutRows
             .append('td')
