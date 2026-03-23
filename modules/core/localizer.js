openstreetmap/iD

commit ce093c43622c7b97805107c9967b4ebc6cc68b1b

fix duplicate strings/user images in upload dialog, fixes #12063

modules/core/localizer.js
@@ -434,9 +434,7 @@ export function coreLocalizer() {
               .attr('lang', info.locale || 'und')
               .text(replacements?._trim ? text.trim() : text);
           } else {
-            selection.each(function(d) {
-              d3_select(this).call(text, d);
-            });
+            d3_select(this).call(text);
           }
         });
       };
@@ -454,23 +452,20 @@ export function coreLocalizer() {
           replacements?.suffix
         ].filter(Boolean);
 
-        const span = selection.selectAll('span.localized-text')
+        const span = selection.selectAll('span')
           .data(texts.map((_, i) => i), d => stringId + d);
         span.exit().remove();
         const enter = span.enter()
-          .append('span')
-          .classed('localized-text', true);
+          .append('span');
         span.merge(enter).each(function(d) {
           const text = texts[d];
           if (typeof text === 'string') {
-            selection.append('span')
-              .attr('class', 'localized-text')
+            d3_select(this)
+              .classed('localized-text', true)
               .attr('lang', info.locale || 'und')
               .text(replacements?._trim ? text.trim() : text);
           } else {
-            selection.each(function(d) {
-              d3_select(this).call(text, d);
-            });
+            d3_select(this).call(text);
           }
         });
       };

modules/ui/commit.js
@@ -273,7 +273,7 @@ export function uiCommit(context) {
         prose = prose.enter()
             .append('p')
             .attr('class', 'commit-info')
-            .call(t.append('commit.upload_explanation'))
+            .call(t.addOrUpdate('commit.upload_explanation'))
             .merge(prose);
 
         // always check if this has changed, but only update prose.html()
@@ -284,15 +284,20 @@ export function uiCommit(context) {
             if (_userDetails === user) return;  // no change
             _userDetails = user;
 
-            const userLink = selection => {
+            const userLink = function(selection) {
+                selection = selection.selectAll('span.user-link').data([user.id], d => d);
+                selection.exit().remove();
+                const enter = selection.enter()
+                    .append('span')
+                    .classed('user-link', true);
                 if (user.image_url) {
-                    selection
+                    enter
                         .append('img')
                         .attr('src', user.image_url)
                         .attr('class', 'icon pre-text user-icon');
                 }
 
-                selection
+                enter
                     .append('a')
                     .attr('class', 'user-info')
                     .text(user.display_name)

commit b65ad1d73f99f9529f8cb3ae779916f8c3df4394

fix regression causing strings with replacement strings not
this regression was introudced in https://github.com/openstreetmap/iD/commit/ce093c43622c7b97805107c9967b4ebc6cc68b1b#diff-849c38d4d2f9a07e9301d615da477fc56d407fca11d991fcccd50ba96a620fd5R437

modules/core/localizer.js
@@ -434,7 +434,7 @@ export function coreLocalizer() {
               .attr('lang', info.locale || 'und')
               .text(replacements?._trim ? text.trim() : text);
           } else {
-            d3_select(this).call(text);
+            selection.call(text);
           }
         });
       };