openstreetmap/iD

commit 768f52f48c75ed93f50f63b3e2430f4874cb9c26

render shared bicycle/foot paths with a dedicated colour (#10256)

modules/svg/tag_classes.js
@@ -14,7 +14,7 @@ export function svgTagClasses() {
         'oneway', 'bridge', 'tunnel', 'embankment', 'cutting', 'barrier',
         'surface', 'tracktype', 'footway', 'crossing', 'service', 'sport',
         'public_transport', 'location', 'parking', 'golf', 'type', 'leisure',
-        'man_made', 'indoor', 'construction', 'proposed'
+        'man_made', 'indoor', 'construction', 'proposed', 'bicycle', 'foot'
     ];
     var _tags = function(entity) { return entity.tags; };
 
@@ -61,15 +61,9 @@ export function svgTagClasses() {
 
         // pick at most one primary classification tag..
         for (i = 0; i < primaries.length; i++) {
-            k = primaries[i];
+            k = primaries[i].replace(':', '_');
             v = t[k];
             if (!v || v === 'no') continue;
-
-            if (k === 'piste:type') {  // avoid a ':' in the class name
-                k = 'piste';
-            } else if (k === 'building:part') {  // avoid a ':' in the class name
-                k = 'building_part';
-            }
 
             primary = k;
             if (statuses.indexOf(v) !== -1) {   // e.g. `railway=abandoned`

css/30_highways.css
@@ -588,6 +588,27 @@ path.line.stroke.tag-highway-cycleway,
     stroke: #fff;
 }
 
+/* shared paths */
+path.line.casing.tag-highway-cycleway.tag-foot-designated:not(.tag-crossing),
+path.line.casing.tag-highway-path.tag-bicycle-designated.tag-foot-designated:not(.tag-crossing) {
+    stroke: #fff;
+}
+.preset-icon .icon.tag-highway-cycleway.tag-foot-designated,
+.preset-icon .icon.tag-highway-path.tag-bicycle-designated.tag-foot-designated {
+    color: #b458ed;
+    fill: #fff;
+}
+path.line.stroke.tag-highway-cycleway.tag-foot-designated,
+path.line.stroke.tag-highway-path.tag-bicycle-designated.tag-foot-designated,
+.preset-icon-container path.casing.tag-highway-cycleway.tag-foot-designated,
+.preset-icon-container path.casing.tag-highway-path.tag-bicycle-designated.tag-foot-designated {
+    stroke: #b458ed;
+}
+.preset-icon-container path.stroke.tag-highway-cycleway.tag-foot-designated:not(.tag-crossing),
+.preset-icon-container path.stroke.tag-highway-path.tag-bicycle-designated.tag-foot-designated:not(.tag-crossing) {
+    stroke: #fff;
+}
+
 /* bridleways */
 .preset-icon .icon.tag-route-horse,
 .preset-icon .icon.tag-highway-bridleway {

commit 4de9b77776d20e1c2c3035d6fafd4b046c10ad3e

fix regression of piste:type rendering (#12301)
fixes #12297, regressed in #10256:

* fix css styling of piste:type rendering
* fix bug in normalization of primary keys with colons
* refactor to more modern js (var to const/let, for of loops, arrow functions)

---------

Co-authored-by: Martin Raifer <martin@raifer.tech>

css/50_misc.css
@@ -8,7 +8,7 @@ path.line.shadow.tag-man_made-pipeline,
 path.line.shadow.tag-natural-tree_row,
 path.line.shadow.tag-roller_coaster-track,
 path.line.shadow.tag-roller_coaster-support,
-path.line.shadow.tag-piste {
+path.line.shadow.tag-piste_type {
     stroke-width: 16;
 }
 path.line.casing.tag-aerialway,
@@ -19,7 +19,7 @@ path.line.casing.tag-man_made-pipeline,
 path.line.casing.tag-natural-tree_row,
 path.line.casing.tag-roller_coaster-track,
 path.line.casing.tag-roller_coaster-support,
-path.line.casing.tag-piste {
+path.line.casing.tag-piste_type {
     stroke-width: 7;
 }
 path.line.stroke.tag-aerialway,
@@ -30,7 +30,7 @@ path.line.stroke.tag-man_made-pipeline,
 path.line.stroke.tag-natural-tree_row,
 path.line.stroke.tag-roller_coaster-track,
 path.line.stroke.tag-roller_coaster-support,
-path.line.stroke.tag-piste {
+path.line.stroke.tag-piste_type {
     stroke-width: 5;
 }
 
@@ -42,7 +42,7 @@ path.line.stroke.tag-piste {
 .low-zoom path.line.shadow.tag-natural-tree_row,
 .low-zoom path.line.shadow.tag-roller_coaster-track,
 .low-zoom path.line.shadow.tag-roller_coaster-support,
-.low-zoom path.line.shadow.tag-piste {
+.low-zoom path.line.shadow.tag-piste_type {
     stroke-width: 12;
 }
 .low-zoom path.line.casing.tag-aerialway,
@@ -53,7 +53,7 @@ path.line.stroke.tag-piste {
 .low-zoom path.line.casing.tag-natural-tree_row,
 .low-zoom path.line.casing.tag-roller_coaster-track,
 .low-zoom path.line.casing.tag-roller_coaster-support,
-.low-zoom path.line.casing.tag-piste {
+.low-zoom path.line.casing.tag-piste_type {
     stroke-width: 5;
 }
 .low-zoom path.line.stroke.tag-aerialway,
@@ -64,7 +64,7 @@ path.line.stroke.tag-piste {
 .low-zoom path.line.stroke.tag-natural-tree_row,
 .low-zoom path.line.stroke.tag-roller_coaster-track,
 .low-zoom path.line.stroke.tag-roller_coaster-support,
-.low-zoom path.line.stroke.tag-piste {
+.low-zoom path.line.stroke.tag-piste_type {
     stroke-width: 3;
 }
 
@@ -106,10 +106,10 @@ path.line.casing.tag-aerialway {
 
 
 /* pistes */
-path.line.stroke.tag-piste {
+path.line.stroke.tag-piste_type {
     stroke: #a9d;
 }
-path.line.casing.tag-piste {
+path.line.casing.tag-piste_type {
     stroke: #444;
 }
 

modules/svg/tag_classes.js
@@ -3,14 +3,14 @@ import { osmPathHighwayTagValues, osmPavedTags, osmSemipavedTags, osmLifecyclePr
 
 
 export function svgTagClasses() {
-    var primaries = [
+    const primaries = [
         'building', 'highway', 'railway', 'waterway', 'aeroway', 'aerialway',
         'piste:type', 'boundary', 'power', 'amenity', 'natural', 'landuse',
         'leisure', 'military', 'place', 'man_made', 'route', 'attraction',
         'roller_coaster', 'building:part', 'indoor', 'climbing'
     ];
-    var statuses = Object.keys(osmLifecyclePrefixes);
-    var secondaries = [
+    const statuses = Object.keys(osmLifecyclePrefixes);
+    const secondaries = [
         'oneway', 'bridge', 'tunnel', 'embankment', 'cutting', 'barrier',
         'surface', 'tracktype', 'footway', 'crossing', 'service', 'sport',
         'public_transport', 'location', 'parking', 'golf', 'type', 'leisure',
@@ -39,11 +39,10 @@ export function svgTagClasses() {
 
 
     tagClasses.getClassesString = function(t, value) {
-        var primary, status;
-        var i, j, k, v;
+        let primary, status;
 
         // in some situations we want to render perimeter strokes a certain way
-        var overrideGeometry;
+        let overrideGeometry;
         if (/\bstroke\b/.test(value)) {
             if (!!t.barrier && t.barrier !== 'no') {
                 overrideGeometry = 'line';
@@ -51,18 +50,17 @@ export function svgTagClasses() {
         }
 
         // preserve base classes (nothing with `tag-`)
-        var classes = value.trim().split(/\s+/)
-            .filter(function(klass) {
-                return klass.length && !/^tag-/.test(klass);
-            })
-            .map(function(klass) {  // special overrides for some perimeter strokes
-                return (klass === 'line' || klass === 'area') ? (overrideGeometry || klass) : klass;
-            });
+        const classes = value.trim().split(/\s+/)
+            .filter(klass => klass.length && !/^tag-/.test(klass))
+            .map(klass => (klass === 'line' || klass === 'area')
+                // special overrides for some perimeter strokes
+                ? (overrideGeometry || klass)
+                : klass);
 
         // pick at most one primary classification tag..
-        for (i = 0; i < primaries.length; i++) {
-            k = primaries[i].replace(':', '_');
-            v = t[k];
+        for (let k of primaries) {
+            const v = t[k];
+            k = k.replace(':', '_');
             if (!v || v === 'no') continue;
 
             primary = k;
@@ -78,13 +76,13 @@ export function svgTagClasses() {
         }
 
         if (!primary) {
-            for (i = 0; i < statuses.length; i++) {
-                for (j = 0; j < primaries.length; j++) {
-                    k = statuses[i] + ':' + primaries[j];  // e.g. `demolished:building=yes`
-                    v = t[k];
+            for (const prefix of statuses) {
+                for (const key of primaries) {
+                    const k = prefix + ':' + key;  // e.g. `demolished:building=yes`
+                    const v = t[k];
                     if (!v || v === 'no') continue;
 
-                    status = statuses[i];
+                    status = prefix;
                     break;
                 }
             }
@@ -92,9 +90,8 @@ export function svgTagClasses() {
 
         // add at most one status tag, only if relates to primary tag..
         if (!status) {
-            for (i = 0; i < statuses.length; i++) {
-                k = statuses[i];
-                v = t[k];
+            for (const k of statuses) {
+                const v = t[k];
                 if (!v || v === 'no') continue;
 
                 if (v === 'yes') {   // e.g. `railway=rail + abandoned=yes`
@@ -117,9 +114,8 @@ export function svgTagClasses() {
         }
 
         // add any secondary tags
-        for (i = 0; i < secondaries.length; i++) {
-            k = secondaries[i];
-            v = t[k];
+        for (const k of secondaries) {
+            const v = t[k];
             if (!v || v === 'no' || k === primary) continue;
             classes.push('tag-' + k);
             classes.push('tag-' + k + '-' + v);
@@ -127,9 +123,9 @@ export function svgTagClasses() {
 
         // For highways, look for surface tagging..
         if ((primary === 'highway' && !osmPathHighwayTagValues[t.highway]) || primary === 'aeroway') {
-            var surface = t.highway === 'track' ? 'unpaved' : 'paved';
-            for (k in t) {
-                v = t[k];
+            let surface = t.highway === 'track' ? 'unpaved' : 'paved';
+            for (const k in t) {
+                const v = t[k];
                 if (k in osmPavedTags) {
                     surface = osmPavedTags[k][v] ? 'paved' : 'unpaved';
                 }
@@ -141,7 +137,7 @@ export function svgTagClasses() {
         }
 
         // If this is a wikidata-tagged item, add a class for that..
-        var qid = (
+        const qid = (
             t.wikidata ||
             t['flag:wikidata'] ||
             t['brand:wikidata'] ||

test/spec/svg/tag_classes.js
@@ -274,4 +274,11 @@ describe('iD.svgTagClasses', function () {
             .call(iD.svgTagClasses());
         expect(selection.attr('class')).to.equal(null);
     });
+
+    it('normalizes colons in primary tag key to underlines', function() {
+        selection
+            .datum(new iD.osmWay({tags: {'piste:type': 'nordic'}}))
+            .call(iD.svgTagClasses());
+        expect(selection.attr('class')).to.equal('tag-piste_type tag-piste_type-nordic');
+    });
 });