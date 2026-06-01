a23d5107e5759ad2a6204839e92bd5bd4c7ec9d8
Brian Strauch <brian@brianstrauch.com>
2026-06-01 09:17:15 -0700

    revert vitest config and prefer polyfill

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,15 +1,9 @@
-import os from 'node:os';
-import path from 'node:path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
     css: true,
     environment: 'jsdom',
-    execArgv: [
-        '--localstorage-file',
-        path.resolve(os.tmpdir(), `vitest-${process.pid}.localstorage`),
-    ],
     globals: true,
     include: ['test/spec/**/*.{js,ts}'],
     setupFiles: ['./test/spec_helpers.ts'],
