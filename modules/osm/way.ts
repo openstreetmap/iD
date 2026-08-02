ab14f67d7ad9c481f458ed4b1e1c9a3f6f5dd625
bhavyaKhatri2703 <khatribhavya2703@gmail.com>
2026-08-02 18:04:18 +0530

    fix: remove whitespaces
    Signed-off-by: bhavyaKhatri2703 <khatribhavya2703@gmail.com>

--- modules/osm/way.ts
+++ modules/osm/way.ts
@@ -184,8 +184,7 @@ export class osmWay extends OsmAbstractEntity {
             if (key in osmSidednessTags && (value in osmSidednessTags[key])) {
                 if (osmSidednessTags[key][value] === true) {
                     return key;
-                } 
-                
+                }
                 // embankment=yes/dyke/both and cutting=yes/both are both-sided
                 // by default, but a separate `embankment:side=*` or
                 // `cutting:side=*` tag can narrow it to just one side.
@@ -195,7 +194,6 @@ export class osmWay extends OsmAbstractEntity {
                         return `${key}-${side}`;
                     }
                 }
-               
                 // if the map's value is something other than a
                 // literal true, we should use it so we can
                 // special case some keys (e.g. natural=coastline
