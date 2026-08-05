4ff276202ae819e3435924e96ad835c95bf33d54
Kyle Hensel <k.y.l.e@outlook.co.nz>
2026-08-05 19:09:36 +1000

    convert presetManager to typescript

--- modules/core/context.ts
+++ modules/core/context.ts
@@ -720,7 +720,6 @@ export function coreContext(this: object): coreContext {
     function initializeDependents() {
 
       if (context.initialHashParams.presets) {
-        // @ts-expect-error -- will be fixed in a different PR
         presetManager.addablePresetIDs(new Set(context.initialHashParams.presets.split(',')));
       }
 
@@ -734,7 +733,6 @@ export function coreContext(this: object): coreContext {
 
       // kick off some async work
       localizer.ensureLoaded();
-      // @ts-expect-error -- will be fixed in a different PR
       presetManager.ensureLoaded();
       _background.ensureLoaded();
 
