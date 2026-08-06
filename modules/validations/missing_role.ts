f31b93676442f8a6c15431b9d9d0f34ed6ae3983
Kyle Hensel <k.y.l.e@outlook.co.nz>
2026-08-06 22:37:06 +1000

    migrate validators to typescript (part 2)

--- modules/validations/missing_role.ts
+++ modules/validations/missing_role.ts
@@ -3,13 +3,17 @@ import { actionDeleteMember } from '../actions/delete_member';
 import { t } from '../core/localizer';
 import { utilDisplayLabel } from '../util/utilDisplayLabel';
 import { validationIssue, validationIssueFix } from '../core/validation';
+import type { osmRelation, RelationMemberWithIndex } from '../osm/relation';
+import type { osmWay, RelationId } from '../osm';
+import type { CreateValidator, Validator } from '../core/validation/models';
 
+interface Data { member: RelationMemberWithIndex }
 
-export function validationMissingRole() {
+export const validationMissingRole: CreateValidator = () => {
     var type = 'missing_role';
 
-    var validation = function checkMissingRole(entity, graph) {
-        var issues = [];
+    const validation: Validator = function checkMissingRole(entity, graph) {
+        var issues: validationIssue<Data>[] = [];
         if (entity.type === 'way') {
             graph.parentRelations(entity).forEach(function(relation) {
                 if (!relation.isMultipolygon()) return;
@@ -21,7 +25,7 @@ export function validationMissingRole() {
             });
         } else if (entity.type === 'relation' && entity.isMultipolygon()) {
             entity.indexedMembers().forEach(function(member) {
-                var way = graph.hasEntity(member.id);
+                var way = graph.hasEntity<osmWay>(member.id);
                 if (way && isMissingRole(member)) {
                     issues.push(makeIssue(way, entity, member));
                 }
@@ -32,13 +36,13 @@ export function validationMissingRole() {
     };
 
 
-    function isMissingRole(member) {
+    function isMissingRole(member: RelationMemberWithIndex) {
         return !member.role || !member.role.trim().length;
     }
 
 
-    function makeIssue(way, relation, member) {
-        return new validationIssue({
+    function makeIssue(way: osmWay, relation: osmRelation, member: RelationMemberWithIndex) {
+        return new validationIssue<Data>({
             type: type,
             severity: 'warning',
             message: function(context) {
@@ -62,12 +66,12 @@ export function validationMissingRole() {
                 return [
                     makeAddRoleFix('inner'),
                     makeAddRoleFix('outer'),
-                    new validationIssueFix({
+                    new validationIssueFix<Data>({
                         icon: 'iD-operation-delete',
                         title: t.append('issues.fix.remove_from_relation.title'),
                         onClick: function(context) {
                             context.perform(
-                                actionDeleteMember(this.issue.entityIds[1], this.issue.data.member.index),
+                                actionDeleteMember(relation.id, member.index),
                                 t('operations.delete_member.annotation', {
                                     n: 1
                                 })
@@ -79,7 +83,7 @@ export function validationMissingRole() {
         });
 
 
-        function showReference(selection) {
+        function showReference(selection: d3.Selection) {
             selection.selectAll('.issue-reference')
                 .data([0])
                 .enter()
@@ -90,14 +94,14 @@ export function validationMissingRole() {
     }
 
 
-    function makeAddRoleFix(role) {
-        return new validationIssueFix({
+    function makeAddRoleFix(role: string) {
+        return new validationIssueFix<Data>({
             title: t.append('issues.fix.set_as_' + role + '.title'),
             onClick: function(context) {
-                var oldMember = this.issue.data.member;
-                var member = { id: this.issue.entityIds[0], type: oldMember.type, role: role };
+                var oldMember = this.issue!.data!.member;
+                var member = { id: this.issue!.entityIds[0], type: oldMember.type, role: role };
                 context.perform(
-                    actionChangeMember(this.issue.entityIds[1], member, oldMember.index),
+                    actionChangeMember(this.issue!.entityIds[1] as RelationId, member, oldMember.index),
                     t('operations.change_role.annotation', {
                         n: 1
                     })
@@ -109,4 +113,4 @@ export function validationMissingRole() {
     validation.type = type;
 
     return validation;
-}
+};
